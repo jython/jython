e1acfdde636e1ec5cdbb5cce5674e9cc5cb19df0
Jim Hugunin <jim@hugunin.net>
1997-12-05 10:30:18 +0000

    Final files to initial JPython repository

---
+++ Misc/make_cmp.py
@@ -0,0 +1,12 @@
+code = """	static Integer %(name)s;
+	public Object %(name)s_cmp(SimpleNode node) throws Exception {
+		if (mrefs.%(name)s == null) %(name)s = new Integer(make_binop("%(name)s"));
+		return mrefs.%(name)s;
+	}
+"""
+
+for name in ['less', 'greater', 'equal', 'less_equal', 
+			'greater_equal', 'notequal', 'in', 'not_in',
+			'is', 'is_not']:
+	print code % {'name':name}
+
