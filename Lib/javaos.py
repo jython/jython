6c3d7820bb0ffff36206bbffd8d329b4eb110e0a
Jim Hugunin <jim@hugunin.net>
1998-02-26 15:36:59 +0000

    Moving many modules to builtins Will let JPython interact better with standard Python Lib's

---
+++ Lib/javaos.py
@@ -0,0 +1,65 @@
+import java
+import javapath
+path = javapath
+
+error = 'os.error'
+
+name = 'jdk1.1'
+curdir = '.'
+pardir = '..' #This might not be right...
+#curdir, pardir??
+sep = java.io.File.separator
+pathsep = java.io.File.pathSeparator
+#defpath?
+
+#I can do better than this...
+environ = {}
+
+
+def _exit(n=0):
+	java.lang.System.exit(n)
+	
+def getcwd():
+	dir = java.io.File("foo")
+	file = java.io.File(dir.getAbsolutePath())
+	return file.getParent()
+
+def listdir(path):
+	dir = java.io.File(path)
+	l = dir.list()
+	if l is None:
+		raise error, 'No such directory'
+	return list(l)
+
+def mkdir(path):
+	dir = java.io.File(path)
+	if not dir.mkdir():
+		raise error, "couldn't make directory"
+
+def remove(path):
+	file = java.io.File(path)
+	if not file.delete():
+		raise error, "couldn't delete file"
+
+def rename(path, newpath):
+	file = java.io.File(path)
+	if not file.renameTo(newpath):
+	    raise error, "couldn't rename file"
+
+def rmdir(path):
+	dir = java.io.File(path)
+	if not dir.delete():
+		raise error, "couldn't delete directory"
+				
+unlink = remove
+
+
+def stat(path):
+	"""The Java stat implementation only returns a small subset of 
+	the standard fields"""
+	
+	file = java.io.File(path)
+	mtime = file.lastModified()
+	size = file.length()
+	
+	return (0,0,0,0,0,0, size, mtime, mtime, 0)
---
+++ Lib/javapath.py
@@ -0,0 +1,179 @@
+"""Common pathname manipulations, JDK version.
+
+Instead of importing this module directly, import os and refer to this
+module as os.path.
+
+"""
+
+# Incompletely implemented:
+# islink -- How?
+# ismount -- How?
+# splitdrive -- How?
+# normcase -- How?
+
+# Missing:
+# expandvars -- can't be bothered right now
+# normpath -- How?  getCanonicalPath() does too much...
+# sameopenfile -- Java doesn't have fstat nor file descriptors?
+# samestat -- How?
+
+import java
+from java.io import File
+from java.lang import System
+import os
+
+def dirname(path):
+    """Return the directory component of a pathname"""
+    result = File(path).getParent()
+    if not result:
+	if isabs(path):
+	    result = path # Must be root
+	else:
+	    result = ""
+    return result
+
+def basename(path):
+    """Return the final component of a pathname"""
+    return File(path).getName()
+
+def split(path):
+    """Split a pathname.
+
+    Return tuple "(head, tail)" where "tail" is everything after the
+    final slash.  Either part may be empty.
+
+    """
+    return (dirname(path), basename(path))
+
+def splitext(path):
+    """Split the extension from a pathname.
+
+    Extension is everything from the last dot to the end.  Return
+    "(root, ext)", either part may be empty.
+
+    """
+    i = 0
+    n = -1
+    for c in path:
+        if c == '.': n = i
+        i = i+1
+    if n < 0:
+        return (path, "")
+    else:
+        return (path[:n], path[n:])
+
+def splitdrive(path):
+    """Split a pathname into drive and path.
+
+    On JDK, drive is always empty.
+    XXX This isn't correct for JDK on DOS/Windows!
+
+    """
+    return ("", path)
+
+def exists(path):
+    """Test whether a path exists.
+
+    Returns false for broken symbolic links.
+
+    """
+    return File(path).exists()
+
+def isabs(path):
+    """Test whether a path is absolute"""
+    return File(path).isAbsolute()
+
+def isfile(path):
+    """Test whether a path is a regular file"""
+    return File(path).isFile()
+
+def isdir(path):
+    """Test whether a path is a directory"""
+    return File(path).isDirectory()
+
+def join(path, *args):
+    """Join two or more pathname components, inserting os.sep as needed"""
+    f = File(path)
+    for a in args:
+	g = File(a)
+	if g.isAbsolute():
+	    f = g
+	else:
+	    f = File(f, a)
+    return f.getPath()
+
+def normcase(path):
+    """Normalize case of pathname.
+
+    XXX Not done right under JDK.
+
+    """
+    return File(path).getPath()
+
+def commonprefix(m):
+    "Given a list of pathnames, return the longest common leading component"
+    if not m: return ''
+    prefix = m[0]
+    for item in m:
+        for i in range(len(prefix)):
+            if prefix[:i+1] <> item[:i+1]:
+                prefix = prefix[:i]
+                if i == 0: return ''
+                break
+    return prefix
+
+def islink(path):
+    """Test whether a path is a symbolic link.
+
+    XXX This incorrectly always returns false under JDK.
+
+    """
+    return 0
+
+def samefile(path, path2):
+    """Test whether two pathnames reference the same actual file"""
+    f = File(path)
+    f2 = File(path2)
+    return f.getCanonicalPath() == f2.getCanonicalPath()
+
+def ismount(path):
+    """Test whether a path is a mount point.
+
+    XXX This incorrectly always returns false under JDK.
+
+    """
+    return 0
+
+
+def walk(top, func, arg):
+    """Walk a directory tree.
+
+    walk(top,func,args) calls func(arg, d, files) for each directory
+    "d" in the tree rooted at "top" (including "top" itself).  "files"
+    is a list of all the files and subdirs in directory "d".
+
+    """
+    try:
+        names = os.listdir(top)
+    except os.error:
+        return
+    func(arg, top, names)
+    for name in names:
+	name = join(top, name)
+	if isdir(name) and not islink(name):
+	    walk(name, func, arg)
+
+def expanduser(path):
+    if path[:1] == "~":
+	c = path[1:2]
+	if not c:
+	    return gethome()
+	if c == os.sep:
+	    return File(gethome(), path[2:]).getPath()
+    return path
+
+def getuser():
+    return System.getProperty("user.name")
+
+def gethome():
+    return System.getProperty("user.home")
