1abd7f4b7b22267da14aae0c2e56f491c7224779
Jim Hugunin <jim@hugunin.net>
1998-05-14 13:31:46 +0000

    Adding to source control

---
+++ org/python/rmi/UnicastRemoteObject.java
@@ -0,0 +1,16 @@
+package org.python.rmi;
+import java.rmi.*;
+
+public class UnicastRemoteObject extends java.rmi.server.RemoteServer {
+    Remote remote;
+
+    public UnicastRemoteObject() throws RemoteException {
+        this.remote = remote;
+        java.rmi.server.UnicastRemoteObject.exportObject(remote);
+    }
+
+    private void readObject(java.io.ObjectInputStream in)
+    throws java.io.IOException, java.lang.ClassNotFoundException {
+	    java.rmi.server.UnicastRemoteObject.exportObject(remote);
+    }
+}
