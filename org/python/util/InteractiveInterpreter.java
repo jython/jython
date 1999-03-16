844e9ca350f1ee35c23a6975a2bed55c219b95c9
Barry Warsaw <barry@python.org>
1999-03-16 18:21:53 +0000

    intermediate

--- org/python/util/InteractiveInterpreter.java
+++ org/python/util/InteractiveInterpreter.java
@@ -3,32 +3,35 @@ import org.python.core.*;
 // Based on CPython-1.5.2's code module
 
 public class InteractiveInterpreter extends PythonInterpreter {
-    public InteractiveInterpreter() { super(); }
+    public InteractiveInterpreter() {
+	super();
+    }
     public InteractiveInterpreter(PyObject locals) {
         super(locals);
     }
 
-    /**Compile and run some source in the interpreter.
-
-        Arguments are as for compile_command().
-
-        One several things can happen:
-
-        1) The input is incorrect; compile_command() raised an
-        exception (SyntaxError or OverflowError).  A syntax traceback
-        will be printed by calling the showsyntaxerror() method.
-
-        2) The input is incomplete, and more input is required;
-        compile_command() returned None.  Nothing happens.
-
-        3) The input is complete; compile_command() returned a code
-        object.  The code is executed by calling self.runcode() (which
-        also handles run-time exceptions, except for SystemExit).
-
-        The return value is 1 in case 2, 0 in the other cases (unless
-        an exception is raised).  The return value can be used to
-        decide whether to use sys.ps1 or sys.ps2 to prompt the next
-        line.**/
+    /**
+     * Compile and run some source in the interpreter.
+     *
+     * Arguments are as for compile_command().
+     *
+     * One several things can happen:
+     *
+     * 1) The input is incorrect; compile_command() raised an exception
+     * (SyntaxError or OverflowError).  A syntax traceback will be printed
+     * by calling the showsyntaxerror() method.
+     *
+     * 2) The input is incomplete, and more input is required;
+     * compile_command() returned None.  Nothing happens.
+     *
+     * 3) The input is complete; compile_command() returned a code object.
+     * The code is executed by calling self.runcode() (which also handles
+     * run-time exceptions, except for SystemExit).
+     *
+     * The return value is 1 in case 2, 0 in the other cases (unless an
+     * exception is raised).  The return value can be used to decide
+     * whether to use sys.ps1 or sys.ps2 to prompt the next line.
+     **/
     public boolean runsource(String source) {
         return runsource(source, "<input>", "single");
     }
@@ -36,10 +39,12 @@ public class InteractiveInterpreter extends PythonInterpreter {
     public boolean runsource(String source, String filename) {
         return runsource(source, filename, "single");
     }
+
     public boolean runsource(String source, String filename, String symbol) {
         PyObject code;
         try {
-            code = org.python.modules.codeop.compile_command(source, filename, symbol);
+            code = org.python.modules.codeop.compile_command(
+		source, filename, symbol);
         } catch (PyException exc) {
             if (Py.matchException(exc, Py.SyntaxError)) {
                 // Case 1
@@ -49,26 +54,26 @@ public class InteractiveInterpreter extends PythonInterpreter {
                 throw exc;
             }
         }
-
         // Case 2
-        if (code == Py.None) return true;
-
+        if (code == Py.None)
+	    return true;
         // Case 3
         runcode(code);
         return false;
     }
 
     /**
-    execute a code object.
+     * execute a code object.
+     *
+     * When an exception occurs, self.showtraceback() is called to display
+     * a traceback.  All exceptions are caught except SystemExit, which is
+     * reraised.
+     *
+     * A note about KeyboardInterrupt: this exception may occur elsewhere
+     * in this code, and may not always be caught.  The caller should be
+     * prepared to deal with it.
+     **/
 
-        When an exception occurs, self.showtraceback() is called to
-        display a traceback.  All exceptions are caught except
-        SystemExit, which is reraised.
-
-        A note about KeyboardInterrupt: this exception may occur
-        elsewhere in this code, and may not always be caught.  The
-        caller should be prepared to deal with it.
-    **/
     // Make this run in another thread somehow????
     public void runcode(PyObject code) {
         try {
@@ -80,7 +85,8 @@ public class InteractiveInterpreter extends PythonInterpreter {
     }
 
     public void showexception(PyException exc) {
-        // Should probably add code to handle skipping top stack frames somehow...
+        // Should probably add code to handle skipping top stack frames
+        // somehow...
         write(exc.toString());
     }
 
@@ -95,9 +101,10 @@ public class InteractiveInterpreter extends PythonInterpreter {
         buffer.setLength(0);
     }
     
-    /** Pause the current code, sneak an exception raiser into sys.trace_func,
-    and then continue the code hoping that JPython will get control to do the break;
-    **/
+    /** Pause the current code, sneak an exception raiser into
+     * sys.trace_func, and then continue the code hoping that JPython will
+     * get control to do the break;
+     **/
     public void interrupt(ThreadState ts) throws InterruptedException {
         TraceFunction breaker = new BreakTraceFunction();
         TraceFunction oldTrace = ts.systemState.tracefunc;
@@ -115,8 +122,23 @@ class BreakTraceFunction extends TraceFunction {
         //Thread.currentThread().interrupt();
     }
 
-    public TraceFunction traceCall(PyFrame frame) { doBreak(); return null; }
-    public TraceFunction traceReturn(PyFrame frame, PyObject ret) { doBreak(); return null;}
-    public TraceFunction traceLine(PyFrame frame, int line) { doBreak(); return null;}
-    public TraceFunction traceException(PyFrame frame, PyException exc) { doBreak(); return null;}
+    public TraceFunction traceCall(PyFrame frame) {
+	doBreak();
+	return null;
+    }
+
+    public TraceFunction traceReturn(PyFrame frame, PyObject ret) {
+	doBreak();
+	return null;
+    }
+
+    public TraceFunction traceLine(PyFrame frame, int line) {
+	doBreak();
+	return null;
+    }
+
+    public TraceFunction traceException(PyFrame frame, PyException exc) {
+	doBreak();
+	return null;
+    }
 }
