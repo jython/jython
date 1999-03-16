53bb64291d5190a602ad7376c0924a1772fbd37d
Barry Warsaw <barry@python.org>
1999-03-16 17:02:02 +0000

    intermediate

--- org/python/core/PyReflectedField.java
+++ org/python/core/PyReflectedField.java
@@ -3,54 +3,56 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
 public class PyReflectedField extends PyObject {
-	public Field field;
-
+    public Field field;
     public static PyClass __class__;
+
     public PyReflectedField(PyClass c) {
         super(c);
     }
     
-	public PyReflectedField(Field field) {
-	    super(__class__);
-		this.field = field;
-	}
+    public PyReflectedField(Field field) {
+	super(__class__);
+	this.field = field;
+    }
 
-	public PyObject _doget(PyObject self) {
-	    Object iself = null;
-	    if (!Modifier.isStatic(field.getModifiers())) {
-	        if (self == null) return this;
-	        iself = Py.tojava(self, field.getDeclaringClass());
-	    }
-	    Object value;
+    public PyObject _doget(PyObject self) {
+	Object iself = null;
+	if (!Modifier.isStatic(field.getModifiers())) {
+	    if (self == null)
+		return this;
+	    iself = Py.tojava(self, field.getDeclaringClass());
+	}
+	Object value;
 	    
-	    try {
-	        value = field.get(iself);
-	    } catch (IllegalAccessException exc) {
-		    throw Py.JavaError(exc);
-		}
-
-		return Py.java2py(value);
+	try {
+	    value = field.get(iself);
+	} catch (IllegalAccessException exc) {
+	    throw Py.JavaError(exc);
 	}
 
-	public boolean _doset(PyObject self, PyObject value) {
-	    Object iself = null;
-	    if (!Modifier.isStatic(field.getModifiers())) {
-	        if (self == null) {
-	            throw Py.AttributeError("set instance variable as static: "+field.toString());
-	        }
-	        iself = Py.tojava(self, field.getDeclaringClass());
+	return Py.java2py(value);
+    }
+
+    public boolean _doset(PyObject self, PyObject value) {
+	Object iself = null;
+	if (!Modifier.isStatic(field.getModifiers())) {
+	    if (self == null) {
+		throw Py.AttributeError("set instance variable as static: "+
+					field.toString());
 	    }
+	    iself = Py.tojava(self, field.getDeclaringClass());
+	}
         Object fvalue = Py.tojava(value, field.getType());
         
         try {
-		    field.set(iself, fvalue);
-		} catch (IllegalAccessException exc) {
-		    throw Py.JavaError(exc);
-		}
-		return true;
+	    field.set(iself, fvalue);
+	} catch (IllegalAccessException exc) {
+	    throw Py.JavaError(exc);
 	}
+	return true;
+    }
 
-	public String toString() {
-		return "<reflected field "+field.toString()+" at "+Py.id(this)+">";
-	}
+    public String toString() {
+	return "<reflected field "+field.toString()+" at "+Py.id(this)+">";
+    }
 }
