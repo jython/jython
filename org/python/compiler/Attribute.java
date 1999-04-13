abe4e78fbc7069ef3a1ab51a52cb3333ca9ad679
Barry Warsaw <barry@python.org>
1999-04-13 21:33:37 +0000

    intermediate

--- org/python/compiler/Attribute.java
+++ org/python/compiler/Attribute.java
@@ -1,5 +1,9 @@
+// Copyright © Corporation for National Research Initiatives
+
 package org.python.compiler;
 
-public abstract class Attribute {
-	public abstract void write(java.io.DataOutputStream s ) throws java.io.IOException;
+public abstract class Attribute
+{
+    public abstract void write(java.io.DataOutputStream s)
+	throws java.io.IOException;
 }
