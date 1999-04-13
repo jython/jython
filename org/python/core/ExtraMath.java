d2cde2da7368f7fa780521040c2003b7f139ff36
Barry Warsaw <barry@python.org>
1999-04-13 22:27:47 +0000

    intermediate

--- org/python/core/ExtraMath.java
+++ org/python/core/ExtraMath.java
@@ -1,13 +1,13 @@
+// Copyright © Corporation for National Research Initiatives
+
 package org.python.core;
 
 public class ExtraMath {
-
     static double LOG10 = Math.log(10.0);
 
     public static double log10(double v) {
         return Math.log(v) / LOG10;
     }
-
 
     public static double hypot(double v, double w) {
         v = Math.abs(v);
@@ -18,7 +18,7 @@ public class ExtraMath {
             w = temp;
 	}
         if (v == 0.0)
-                return 0.0;
+	    return 0.0;
         else {
             double wv = w/v;
             return v * Math.sqrt(1.0 + wv*wv);
