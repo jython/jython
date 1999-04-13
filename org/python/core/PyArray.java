b40f4230666e7bbe41467d8a8a2901ae8eb299b8
Barry Warsaw <barry@python.org>
1999-04-13 22:37:37 +0000

    intermediate

--- org/python/core/PyArray.java
+++ org/python/core/PyArray.java
@@ -1,145 +1,150 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.core;
 import java.lang.reflect.Array;
 
 public class PyArray extends PySequence {
-	Object data;
-	Class type;
-	public char typecode;
+    Object data;
+    Class type;
+    public char typecode;
 
     public static PyClass __class__;
-	public PyArray(Class type, Object data) {
-	    super(__class__);
-		this.type = type;
-		this.data = data;
-	}
+    public PyArray(Class type, Object data) {
+	super(__class__);
+	this.type = type;
+	this.data = data;
+    }
 
-	public PyArray(Class type, int n) {
-		this(type, Array.newInstance(type, n));
-	}
+    public PyArray(Class type, int n) {
+	this(type, Array.newInstance(type, n));
+    }
 
-	public static PyArray zeros(int n, Class ctype) {
-		return new PyArray(ctype, n);
-	}
+    public static PyArray zeros(int n, Class ctype) {
+	return new PyArray(ctype, n);
+    }
 
-	public static PyArray array(PyObject seq, Class ctype) {
-		PyArray array = new PyArray(ctype, seq.__len__());
-		PyObject o;
-		for(int i=0; (o=seq.__finditem__(i)) != null; i++) {
-			array.set(i, o);
-		}
-		return array;
+    public static PyArray array(PyObject seq, Class ctype) {
+	PyArray array = new PyArray(ctype, seq.__len__());
+	PyObject o;
+	for(int i=0; (o=seq.__finditem__(i)) != null; i++) {
+	    array.set(i, o);
 	}
+	return array;
+    }
 
-	public static Class char2class(char type) {
-		switch (type) {
-		case 'z':
-			return Boolean.TYPE;
-		case 'c':
-			return Character.TYPE;
-		case 'b':
-			return Byte.TYPE;
-		case 'h':
-			return Short.TYPE;
-		case 'i':
-			return Integer.TYPE;
-		case 'l':
-			return Long.TYPE;
-		case 'f':
-			return Float.TYPE;
-		case 'd':
-			return Double.TYPE;
-		default:
-			throw Py.ValueError("typecode must be in [zcbhilfd]");
-		}
+    public static Class char2class(char type) {
+	switch (type) {
+	case 'z':
+	    return Boolean.TYPE;
+	case 'c':
+	    return Character.TYPE;
+	case 'b':
+	    return Byte.TYPE;
+	case 'h':
+	    return Short.TYPE;
+	case 'i':
+	    return Integer.TYPE;
+	case 'l':
+	    return Long.TYPE;
+	case 'f':
+	    return Float.TYPE;
+	case 'd':
+	    return Double.TYPE;
+	default:
+	    throw Py.ValueError("typecode must be in [zcbhilfd]");
 	}
+    }
 
-	public Object __tojava__(Class c) {
-		if (c == Object.class || (c.isArray() && c.getComponentType().isAssignableFrom(type))) {
-			return data;
-		}
-		return super.__tojava__(c);
+    public Object __tojava__(Class c) {
+	if (c == Object.class ||
+	    (c.isArray() && c.getComponentType().isAssignableFrom(type)))
+	{
+	    return data;
 	}
+	return super.__tojava__(c);
+    }
 
-	public int __len__() { 
-	    return Array.getLength(data); 
-	}
+    public int __len__() { 
+	return Array.getLength(data); 
+    }
 	
-	protected PyObject get(int i) {
-		return Py.java2py(Array.get(data, i));
-	}
+    protected PyObject get(int i) {
+	return Py.java2py(Array.get(data, i));
+    }
 	
-	protected PyObject getslice(int start, int stop, int step) {
-	    if (step != 1)
-		    throw Py.TypeError("step != 1 not implemented yet");
-		int n = stop-start;
-		PyArray ret = new PyArray(type, n);
-		System.arraycopy(data, start, ret.data, 0, n);
-		return ret;
-	}
+    protected PyObject getslice(int start, int stop, int step) {
+	if (step != 1)
+	    throw Py.TypeError("step != 1 not implemented yet");
+	int n = stop-start;
+	PyArray ret = new PyArray(type, n);
+	System.arraycopy(data, start, ret.data, 0, n);
+	return ret;
+    }
 
-	protected PyObject repeat(int count) {
-		throw Py.TypeError("can't apply '*' to arrays");
-	}
+    protected PyObject repeat(int count) {
+	throw Py.TypeError("can't apply '*' to arrays");
+    }
 
-	protected void del(int i) {
-		throw Py.TypeError("can't remove from array");
-	}
-	protected void delRange(int start, int stop, int step) {
-		throw Py.TypeError("can't remove from array");
-	}
+    protected void del(int i) {
+	throw Py.TypeError("can't remove from array");
+    }
+    protected void delRange(int start, int stop, int step) {
+	throw Py.TypeError("can't remove from array");
+    }
 
-	protected void set(int i, PyObject value) {
-		Object o = Py.tojava(value, type);
-		Array.set(data, i, o);
-	}
+    protected void set(int i, PyObject value) {
+	Object o = Py.tojava(value, type);
+	Array.set(data, i, o);
+    }
 
-	protected void setslice(int start, int stop, int step, PyObject value) {
-		if (value instanceof PyString && type == Character.TYPE) {
-		    char[] chars = value.toString().toCharArray();
-		    if (chars.length == stop-start && step == 1) {
-		        System.arraycopy(chars, 0, data, start, chars.length);
-		    } else {
-		        throw Py.ValueError("invalid bounds for setting from string");
-		    }
+    protected void setslice(int start, int stop, int step, PyObject value) {
+	if (value instanceof PyString && type == Character.TYPE) {
+	    char[] chars = value.toString().toCharArray();
+	    if (chars.length == stop-start && step == 1) {
+		System.arraycopy(chars, 0, data, start, chars.length);
+	    } else {
+		throw Py.ValueError("invalid bounds for setting from string");
+	    }
 
+	} else {
+	    if (value instanceof PyString && type == Byte.TYPE) {
+		byte[] chars = value.toString().getBytes();
+		if (chars.length == stop-start && step == 1) {
+		    System.arraycopy(chars, 0, data, start, chars.length);
 		} else {
-    		if (value instanceof PyString && type == Byte.TYPE) {
-    		    byte[] chars = value.toString().getBytes();
-    		    if (chars.length == stop-start && step == 1) {
-    		        System.arraycopy(chars, 0, data, start, chars.length);
-    		    } else {
-    		        throw Py.ValueError("invalid bounds for setting from string");
-    		    }
-    		} else {
-		        throw Py.TypeError("can't set slice in array (except from string)");
-    		}
+		    throw Py.ValueError(
+			"invalid bounds for setting from string");
+		}
+	    } else {
+		throw Py.TypeError(
+		    "can't set slice in array (except from string)");
+	    }
     	}
 
+    }
+
+    public String tostring() {
+	if (type == Character.TYPE) {
+	    return new String((char[])data);
 	}
-
-	public String tostring() {
-	    if (type == Character.TYPE) {
-	        return new String((char[])data);
-	    }
-	    if (type == Byte.TYPE) {
-	        return new String((byte[])data, 0);
-	    }
-	    throw Py.TypeError("can only convert arrays of byte and char to string");
+	if (type == Byte.TYPE) {
+	    return new String((byte[])data, 0);
 	}
+	throw Py.TypeError(
+	    "can only convert arrays of byte and char to string");
+    }
 
-
-
-	public PyString __repr__() {
-		StringBuffer buf = new StringBuffer("array([");
-		for(int i=0; i<__len__()-1; i++) {
-			buf.append(get(i).__repr__().toString());
-			buf.append(", ");
-		}
-		if (__len__() > 0) buf.append(get(__len__()-1).__repr__().toString());
-		buf.append("], ");
-		buf.append(type.getName());
-		buf.append(")");
-
-		return new PyString(buf.toString());
+    public PyString __repr__() {
+	StringBuffer buf = new StringBuffer("array([");
+	for (int i=0; i<__len__()-1; i++) {
+	    buf.append(get(i).__repr__().toString());
+	    buf.append(", ");
 	}
+	if (__len__() > 0)
+	    buf.append(get(__len__()-1).__repr__().toString());
+	buf.append("], ");
+	buf.append(type.getName());
+	buf.append(")");
+
+	return new PyString(buf.toString());
+    }
 }
