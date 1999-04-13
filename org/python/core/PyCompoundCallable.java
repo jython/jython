d93b5a801e0c83201f3c349857be91819aac9081
Barry Warsaw <barry@python.org>
1999-04-13 23:00:49 +0000

    intermediate

--- org/python/core/PyCompoundCallable.java
+++ org/python/core/PyCompoundCallable.java
@@ -1,3 +1,4 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.core;
 import java.util.Vector;
 
@@ -18,11 +19,11 @@ public class PyCompoundCallable extends PyObject {
         callables.removeAllElements();
     }
 
-	public PyObject __call__(PyObject[] args, String[] keywords) {
-	    // Set the system state to handle callbacks from java threads
-	    Py.setSystemState(systemState);
-	    int n = callables.size();
-	    //System.out.println("callable: "+n);
+    public PyObject __call__(PyObject[] args, String[] keywords) {
+	// Set the system state to handle callbacks from java threads
+	Py.setSystemState(systemState);
+	int n = callables.size();
+	//System.out.println("callable: "+n);
         for (int i=0; i<n; i++) {
             ((PyObject)callables.elementAt(i)).__call__(args, keywords);
         }
