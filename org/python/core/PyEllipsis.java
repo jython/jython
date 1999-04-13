e56bed9e094000df0aa9b1e3d07b082ba44eb556
Barry Warsaw <barry@python.org>
1999-04-13 23:03:10 +0000

    reformatted
    added CNRI copyright notice as per REK 05-Mar-1999

--- org/python/core/PyEllipsis.java
+++ org/python/core/PyEllipsis.java
@@ -1,7 +1,8 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.core;
 
 public class PyEllipsis extends PySingleton {
-	public PyEllipsis() {
-		super("Ellipsis");
-	}
+    public PyEllipsis() {
+	super("Ellipsis");
+    }
 }
