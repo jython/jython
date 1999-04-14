a511a5a1fc269731208e8014c1fd88abff6936dd
Barry Warsaw <barry@python.org>
1999-04-14 19:56:45 +0000

    intermediate

--- org/python/modules/jarray.java
+++ org/python/modules/jarray.java
@@ -1,19 +1,20 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.modules;
 import org.python.core.*;
 
 public class jarray {
-	public static PyArray array(PyObject seq, char typecode) {
-		return PyArray.array(seq, PyArray.char2class(typecode));
-	}
+    public static PyArray array(PyObject seq, char typecode) {
+	return PyArray.array(seq, PyArray.char2class(typecode));
+    }
 
-	public static PyArray array(PyObject seq, Class type) {
-		return PyArray.array(seq, type);
-	}
-	public static PyArray zeros(int n, char typecode) {
-		return PyArray.zeros(n, PyArray.char2class(typecode));
-	}
+    public static PyArray array(PyObject seq, Class type) {
+	return PyArray.array(seq, type);
+    }
+    public static PyArray zeros(int n, char typecode) {
+	return PyArray.zeros(n, PyArray.char2class(typecode));
+    }
 
-	public static PyArray zeros(int n, Class type) {
-		return PyArray.zeros(n, type);
-	}
+    public static PyArray zeros(int n, Class type) {
+	return PyArray.zeros(n, type);
+    }
 }
