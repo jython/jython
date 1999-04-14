22dfe487bdd565abd0b1c230ff4f79abd7de75e7
Barry Warsaw <barry@python.org>
1999-04-14 18:43:33 +0000

    intermediate

--- org/python/core/PySlice.java
+++ org/python/core/PySlice.java
@@ -1,23 +1,26 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.core;
 
 public class PySlice extends PyObject {
-	public PyObject start, stop, step;
+    public PyObject start, stop, step;
 
     public static PyClass __class__;
-	public PySlice(PyObject start, PyObject stop, PyObject step) {
-	    super(__class__);
-		this.start = start;
-		this.stop = stop;
-		this.step = step;
-	}
 
-	public PyString __str__() throws PyException {
-		return new PyString(start.__repr__()+":"+stop.__repr__()+":"+step.__repr__());
-	}
+    public PySlice(PyObject start, PyObject stop, PyObject step) {
+	super(__class__);
+	this.start = start;
+	this.stop = stop;
+	this.step = step;
+    }
 
-	public PyString __repr__() throws PyException {
-		return new PyString("slice("+start.__repr__()+", "+
-			stop.__repr__()+", "+
-			step.__repr__()+")");
-	}
+    public PyString __str__() throws PyException {
+	return new PyString(start.__repr__()+":"+stop.__repr__()+":"+
+			    step.__repr__());
+    }
+
+    public PyString __repr__() throws PyException {
+	return new PyString("slice("+start.__repr__()+", "+
+			    stop.__repr__()+", "+
+			    step.__repr__()+")");
+    }
 }
