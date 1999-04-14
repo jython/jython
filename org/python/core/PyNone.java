7a4ae337196e9a907d29a84b83dfb8337b4f0c8b
Barry Warsaw <barry@python.org>
1999-04-14 18:05:57 +0000

    intermediate

--- org/python/core/PyNone.java
+++ org/python/core/PyNone.java
@@ -1,16 +1,21 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.core;
 
 public class PyNone extends PySingleton {
-	public PyNone() {
-		super("None");
-	}
+    public PyNone() {
+	super("None");
+    }
 
-    public boolean __nonzero__() { return false; }
+    public boolean __nonzero__() {
+	return false;
+    }
 
-	public Object __tojava__(Class c) {
-		//Danger here.  java.lang.Object gets null not None
-		if (c == PyObject.class) return this;
-		if (c.isPrimitive()) return Py.NoConversion;
-		return null;
-	}
+    public Object __tojava__(Class c) {
+	//Danger here.  java.lang.Object gets null not None
+	if (c == PyObject.class)
+	    return this;
+	if (c.isPrimitive())
+	    return Py.NoConversion;
+	return null;
+    }
 }
