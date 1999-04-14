9e121685e59d9dc046211ec87db075f4c9db6165
Barry Warsaw <barry@python.org>
1999-04-14 19:55:45 +0000

    intermediate

--- org/python/modules/code.java
+++ org/python/modules/code.java
@@ -1,4 +1,6 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.modules;
+
 import org.python.core.*;
 
 public class code {
@@ -10,7 +12,9 @@ public class code {
         return codeop.compile_command(string, filename, "single");
     }
     
-    public static PyObject compile_command(String string, String filename, String kind) {
+    public static PyObject compile_command(String string, String filename,
+					   String kind)
+    {
         return codeop.compile_command(string, filename, kind);
     }
 }
