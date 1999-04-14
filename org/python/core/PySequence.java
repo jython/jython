d0be320cc2050492a0020909b5f510d04ab5b450
Barry Warsaw <barry@python.org>
1999-04-14 18:41:02 +0000

    intermediate

--- org/python/core/PySequence.java
+++ org/python/core/PySequence.java
@@ -1,3 +1,4 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.core;
 
 /**
@@ -15,101 +16,108 @@ set, setslice, del, and delRange.
 
 abstract public class PySequence extends PyObject {
     /**
-    This constructor is used to pass on an __class__ attribute.
+       This constructor is used to pass on an __class__ attribute.
     **/
-    public PySequence(PyClass c) { super(c); }
-    public PySequence() { ; }
+    public PySequence(PyClass c) {
+	super(c);
+    }
+    public PySequence() {}
 
-	/*These methods must be defined for any sequence*/
+    /*These methods must be defined for any sequence*/
 	
-	/**
-	@param index index of element to return.
-	@return the element at the given position in the list.
-	**/
-	abstract protected PyObject get(int index);
+    /**
+       @param index index of element to return.
+       @return the element at the given position in the list.
+    **/
+    abstract protected PyObject get(int index);
 	
-	/**
-	Returns a range of elements from the sequence.
+    /**
+       Returns a range of elements from the sequence.
 	
-	@param start the position of the first element.
-	@param stop one more than the position of the last element.
-	@param step the step size.
-	@return a sequence corresponding the the given range of elements.
-	**/
-	abstract protected PyObject getslice(int start, int stop, int step);
+       @param start the position of the first element.
+       @param stop one more than the position of the last element.
+       @param step the step size.
+       @return a sequence corresponding the the given range of elements.
+    **/
+    abstract protected PyObject getslice(int start, int stop, int step);
 	
-	/**
-	Repeats the given sequence.
+    /**
+       Repeats the given sequence.
 	
-	@param count the number of times to repeat the sequence.
-	@return this sequence repeated count times.
-	**/
-	abstract protected PyObject repeat(int count);
+       @param count the number of times to repeat the sequence.
+       @return this sequence repeated count times.
+    **/
+    abstract protected PyObject repeat(int count);
 
-	/* These methods only apply to writeable sequences */
-	/**
-	Sets the given element of the sequence.
+    /* These methods only apply to writeable sequences */
+    /**
+       Sets the given element of the sequence.
 	
-	@param index index of the element to set.
-	@param value the value to set this element to.
-	**/
-	protected void set(int index, PyObject value) {
-		throw Py.TypeError("can't assign to immutable object");
-	}
+       @param index index of the element to set.
+       @param value the value to set this element to.
+    **/
+    protected void set(int index, PyObject value) {
+	throw Py.TypeError("can't assign to immutable object");
+    }
 	
-	/**
-	Sets the given range of elements.
-	**/
-	protected void setslice(int start, int stop, int step, PyObject value) {
-		throw Py.TypeError("can't assign to immutable object");
-	}
+    /**
+       Sets the given range of elements.
+    **/
+    protected void setslice(int start, int stop, int step, PyObject value) {
+	throw Py.TypeError("can't assign to immutable object");
+    }
 	
-	protected void del(int i) throws PyException {
-		throw Py.TypeError("can't remove from immutable object");
-	}
-	protected void delRange(int start, int stop, int step) {
-		throw Py.TypeError("can't remove from immutable object");
-	}	
+    protected void del(int i) throws PyException {
+	throw Py.TypeError("can't remove from immutable object");
+    }
+    protected void delRange(int start, int stop, int step) {
+	throw Py.TypeError("can't remove from immutable object");
+    }	
 
-	public boolean __nonzero__() {
-		return __len__() != 0;
-	}
+    public boolean __nonzero__() {
+	return __len__() != 0;
+    }
 
-	public int __cmp__(PyObject ob_other) {
-		if (ob_other.__class__ != __class__) return -2;
+    public int __cmp__(PyObject ob_other) {
+	if (ob_other.__class__ != __class__)
+	    return -2;
 		
-		PySequence other = (PySequence)ob_other;
-		int s1 = __len__();
-		int s2 = other.__len__();
-		int len = (s1 > s2) ? s2 : s1;
+	PySequence other = (PySequence)ob_other;
+	int s1 = __len__();
+	int s2 = other.__len__();
+	int len = (s1 > s2) ? s2 : s1;
 
-		for(int i=0; i<len; i++) {
-			int c = get(i)._cmp(other.get(i));
-			if (c != 0) return c;
-		}
-		return s1 < s2 ? -1 : (s1 > s2 ? 1: 0);
+	for (int i=0; i<len; i++) {
+	    int c = get(i)._cmp(other.get(i));
+	    if (c != 0)
+		return c;
 	}
+	return s1 < s2 ? -1 : (s1 > s2 ? 1: 0);
+    }
 
     protected static final int sliceLength(int start, int stop, int step) {
         //System.err.println("slice: "+start+", "+stop+", "+step);
         if (step > 0) {
             return (stop-start+step-1)/step;
         } else {
-		    return (stop-start+step+1)/step;
-		}
+	    return (stop-start+step+1)/step;
 	}
+    }
 
     private static final int getIndex(PyObject index, int defaultValue) {
-        if (index == Py.None || index == null) return defaultValue;
-        if (!(index instanceof PyInteger)) throw Py.TypeError("slice index must be int");
+        if (index == Py.None || index == null)
+	    return defaultValue;
+        if (!(index instanceof PyInteger))
+	    throw Py.TypeError("slice index must be int");
         return ((PyInteger)index).getValue();
     }
 
-	private static final int[] slice_indices(PySlice s, int length) {
-		int start, stop, step;
+    private static final int[] slice_indices(PySlice s, int length) {
+	int start, stop, step;
 
         step = getIndex(s.step, 1);
-        if (step == 0) throw Py.TypeError("slice step of zero not allowed");
+        if (step == 0)
+	    throw Py.TypeError("slice step of zero not allowed");
         
         if (step > 0) {
             start = getIndex(s.start, 0);
@@ -119,69 +127,80 @@ abstract public class PySequence extends PyObject {
             stop = getIndex(s.start, length);
         }
 		
-		if (start < 0) {
-		    start = length+start;
-		    if (start < 0) start = 0;
-		} else if (start > length) {
-		    start = length;
-		}
-		if (stop < 0) {
-		    stop = length+stop;
-		    if (stop < 0) stop = 0;
-		} else if (stop > length) {
-		    stop = length;
-		}
+	if (start < 0) {
+	    start = length+start;
+	    if (start < 0)
+		start = 0;
+	} else if (start > length) {
+	    start = length;
+	}
+	if (stop < 0) {
+	    stop = length+stop;
+	    if (stop < 0)
+		stop = 0;
+	} else if (stop > length) {
+	    stop = length;
+	}
 		
-		if (stop < start) stop = start;
+	if (stop < start)
+	    stop = start;
 		
-		if (step > 0) {
-		    return new int[] {start, stop, step};
-		} else {
-		    return new int[] {stop, start, step};
-		}
+	if (step > 0) {
+	    return new int[] {start, stop, step};
+	} else {
+	    return new int[] {stop, start, step};
 	}
+    }
 
-	protected int fixindex(int index) {
-		int l = __len__();
-		if (index < 0) index += l;
-		if (index < 0 || index >= l) return -1;
-		//throw Py.IndexError("index out of range");
-		else return index;
-	}
+    protected int fixindex(int index) {
+	int l = __len__();
+	if (index < 0)
+	    index += l;
+	if (index < 0 || index >= l)
+	    return -1;
+	//throw Py.IndexError("index out of range");
+	else return index;
+    }
 
 
-	public PyObject __finditem__(int index) {
-	    index = fixindex(index);
-	    if (index == -1) return null;
-	    else return get(index);
-	}
+    public PyObject __finditem__(int index) {
+	index = fixindex(index);
+	if (index == -1)
+	    return null;
+	else
+	    return get(index);
+    }
 
-	public PyObject __finditem__(PyObject index) {
-		if (index instanceof PyInteger) {
-			return __finditem__(((PyInteger)index).getValue());
-		} else {
-			if (index instanceof PySlice) {
-				int[] s = slice_indices((PySlice)index, __len__());
-				return getslice(s[0], s[1], s[2]);
-			} else {
-				throw Py.TypeError("sequence subscript must be integer or slice");
-			}
-		}
-	}
-
-	public PyObject __getitem__(PyObject index) {
-	    PyObject ret = __finditem__(index);
-	    if (ret == null) {
-	        throw Py.IndexError("index out of range: "+index);
+    public PyObject __finditem__(PyObject index) {
+	if (index instanceof PyInteger) {
+	    return __finditem__(((PyInteger)index).getValue());
+	} else {
+	    if (index instanceof PySlice) {
+		int[] s = slice_indices((PySlice)index, __len__());
+		return getslice(s[0], s[1], s[2]);
+	    } else {
+		throw Py.TypeError(
+		    "sequence subscript must be integer or slice");
 	    }
-	    return ret;
 	}
+    }
+
+    public PyObject __getitem__(PyObject index) {
+	PyObject ret = __finditem__(index);
+	if (ret == null) {
+	    throw Py.IndexError("index out of range: "+index);
+	}
+	return ret;
+    }
 	
-	public PyObject __getslice__(PyObject s_start, PyObject s_stop, PyObject s_step) {
-		int start, stop, step;
+    public PyObject __getslice__(PyObject s_start, PyObject s_stop,
+				 PyObject s_step)
+    {
+	int start, stop, step;
         int length = __len__();
         step = getIndex(s_step, 1);
-        if (step == 0) throw Py.TypeError("slice step of zero not allowed");
+        if (step == 0)
+	    throw Py.TypeError("slice step of zero not allowed");
         
         if (step > 0) {
             start = getIndex(s_start, 0);
@@ -191,31 +210,40 @@ abstract public class PySequence extends PyObject {
             stop = getIndex(s_start, length);
         }
 		
-		if (start < 0) {
-		    start = length+start;
-		    if (start < 0) start = 0;
-		} else if (start > length) {
-		    start = length;
-		}
-		if (stop < 0) {
-		    stop = length+stop;
-		    if (stop < 0) stop = 0;
-		} else if (stop > length) {
-		    stop = length;
-		}
-		
-		if (stop < start) stop = start;
-		
-		if (step <= 0) {
-		    int tmp = start; start = stop; stop = tmp;
-		}
-		return getslice(start, stop, step);
+	if (start < 0) {
+	    start = length+start;
+	    if (start < 0)
+		start = 0;
+	} else if (start > length) {
+	    start = length;
 	}
-	public void __setslice__(PyObject s_start, PyObject s_stop, PyObject s_step, PyObject value) {
-		int start, stop, step;
+	if (stop < 0) {
+	    stop = length+stop;
+	    if (stop < 0)
+		stop = 0;
+	} else if (stop > length) {
+	    stop = length;
+	}
+		
+	if (stop < start)
+	    stop = start;
+		
+	if (step <= 0) {
+	    int tmp = start;
+	    start = stop;
+	    stop = tmp;
+	}
+	return getslice(start, stop, step);
+    }
+    
+    public void __setslice__(PyObject s_start, PyObject s_stop,
+			     PyObject s_step, PyObject value)
+    {
+	int start, stop, step;
         int length = __len__();
         step = getIndex(s_step, 1);
-        if (step == 0) throw Py.TypeError("slice step of zero not allowed");
+        if (step == 0)
+	    throw Py.TypeError("slice step of zero not allowed");
         
         if (step > 0) {
             start = getIndex(s_start, 0);
@@ -225,31 +253,40 @@ abstract public class PySequence extends PyObject {
             stop = getIndex(s_start, length);
         }
 		
-		if (start < 0) {
-		    start = length+start;
-		    if (start < 0) start = 0;
-		} else if (start > length) {
-		    start = length;
-		}
-		if (stop < 0) {
-		    stop = length+stop;
-		    if (stop < 0) stop = 0;
-		} else if (stop > length) {
-		    stop = length;
-		}
+	if (start < 0) {
+	    start = length+start;
+	    if (start < 0)
+		start = 0;
+	} else if (start > length) {
+	    start = length;
+	}
+	if (stop < 0) {
+	    stop = length+stop;
+	    if (stop < 0)
+		stop = 0;
+	} else if (stop > length) {
+	    stop = length;
+	}
 		
-		if (stop < start) stop = start;
+	if (stop < start)
+	    stop = start;
 		
-		if (step <= 0) {
-		    int tmp = start; start = stop; stop = tmp;
-		}
+	if (step <= 0) {
+	    int tmp = start;
+	    start = stop;
+	    stop = tmp;
+	}
         setslice(start, stop, step, value);
-	}
-	public void __delslice__(PyObject s_start, PyObject s_stop, PyObject s_step) {
-		int start, stop, step;
+    }
+    
+    public void __delslice__(PyObject s_start, PyObject s_stop,
+			     PyObject s_step)
+    {
+	int start, stop, step;
         int length = __len__();
         step = getIndex(s_step, 1);
-        if (step == 0) throw Py.TypeError("slice step of zero not allowed");
+        if (step == 0)
+	    throw Py.TypeError("slice step of zero not allowed");
         
         if (step > 0) {
             start = getIndex(s_start, 0);
@@ -259,89 +296,97 @@ abstract public class PySequence extends PyObject {
             stop = getIndex(s_start, length);
         }
 		
-		if (start < 0) {
-		    start = length+start;
-		    if (start < 0) start = 0;
-		} else if (start > length) {
-		    start = length;
-		}
-		if (stop < 0) {
-		    stop = length+stop;
-		    if (stop < 0) stop = 0;
-		} else if (stop > length) {
-		    stop = length;
-		}
+	if (start < 0) {
+	    start = length+start;
+	    if (start < 0)
+		start = 0;
+	} else if (start > length) {
+	    start = length;
+	}
+	if (stop < 0) {
+	    stop = length+stop;
+	    if (stop < 0)
+		stop = 0;
+	} else if (stop > length) {
+	    stop = length;
+	}
 		
-		if (stop < start) stop = start;
+	if (stop < start)
+	    stop = start;
 		
-		if (step <= 0) {
-		    int tmp = start; start = stop; stop = tmp;
-		}
+	if (step <= 0) {
+	    int tmp = start;
+	    start = stop;
+	    stop = tmp;
+	}
         delRange(start, stop, step);
-	}		
+    }		
 
-	public void __setitem__(int index, PyObject value) {
-		int i = fixindex(index);
-		if (i == -1) throw Py.IndexError("index out of range: "+i);
-	    set(i, value);
+    public void __setitem__(int index, PyObject value) {
+	int i = fixindex(index);
+	if (i == -1)
+	    throw Py.IndexError("index out of range: "+i);
+	set(i, value);
+    }
+
+    public void __setitem__(PyObject index, PyObject value) {
+	if (index instanceof PyInteger) {
+	    __setitem__(((PyInteger)index).getValue(), value);
+
+	} else {
+	    if (index instanceof PySlice) {
+		int[] s = slice_indices((PySlice)index, __len__());
+		setslice(s[0], s[1], s[2], value);
+	    } else {
+		throw Py.TypeError(
+		    "sequence subscript must be integer or slice");
+	    }
 	}
+    }
 
-	public void __setitem__(PyObject index, PyObject value) {
-		if (index instanceof PyInteger) {
-		    __setitem__(((PyInteger)index).getValue(), value);
+    public void __delitem__(PyObject index) {
+	if (index instanceof PyInteger) {
+	    int i = fixindex(((PyInteger)index).getValue());
+	    if (i == -1)
+		throw Py.IndexError("index out of range: "+i);
+	    del(i);
+	} else {
+	    if (index instanceof PySlice) {
+		int[] s = slice_indices((PySlice)index, __len__());
+		delRange(s[0], s[1], s[2]);
+	    } else {
+		throw Py.TypeError(
+		    "sequence subscript must be integer or slice");
+	    }
+	}
+    }
 
-		} else {
-			if (index instanceof PySlice) {
-				int[] s = slice_indices((PySlice)index, __len__());
-				setslice(s[0], s[1], s[2], value);
-			} else {
-				throw Py.TypeError("sequence subscript must be integer or slice");
-			}
+    public PyObject __mul__(PyObject count) {
+	int repeats = Py.py2int(count, "can't multiply sequence with non-int");
+	return repeat(repeats >= 0 ? repeats : 0);
+    }
+
+    public PyObject __rmul__(PyObject count) {
+	return __mul__(count);
+    }
+
+    public Object __tojava__(Class c) {
+	if (c.isArray()) {
+	    Class component = c.getComponentType();
+	    //System.out.println("getting: "+component);
+	    try {
+		int n = __len__();
+		PyArray array = new PyArray(component, n);
+		for (int i=0; i<n; i++) {
+		    PyObject o = get(i);
+		    array.set(i, o);
 		}
+		//System.out.println("getting: "+component+", "+array.data);
+		return array.data;
+	    } catch (Throwable t) {
+		;//System.out.println("failed to get: "+component.getName());
+	    }
 	}
-
-	public void __delitem__(PyObject index) {
-		if (index instanceof PyInteger) {
-		    int i = fixindex(((PyInteger)index).getValue());
-		    if (i == -1) throw Py.IndexError("index out of range: "+i);
-			del(i);
-		} else {
-			if (index instanceof PySlice) {
-				int[] s = slice_indices((PySlice)index, __len__());
-				delRange(s[0], s[1], s[2]);
-			} else {
-				throw Py.TypeError("sequence subscript must be integer or slice");
-			}
-		}
-	}
-
-	public PyObject __mul__(PyObject count) {
-	    int repeats = Py.py2int(count, "can't multiply sequence with non-int");
-	    return repeat(repeats >= 0 ? repeats : 0);
-	}
-
-	public PyObject __rmul__(PyObject count) {
-		return __mul__(count);
-	}
-
-	public Object __tojava__(Class c) {
-		if (c.isArray()) {
-			Class component = c.getComponentType();
-			//System.out.println("getting: "+component);
-			try {
-				int n = __len__();
-				PyArray array = new PyArray(component, n);
-				for(int i=0; i<n; i++) {
-					PyObject o = get(i);
-					array.set(i, o);
-				}
-		        //System.out.println("getting: "+component+", "+array.data);
-				return array.data;
-			} catch (Throwable t) {
-				;//System.out.println("failed to get: "+component.getName());
-			}
-		}
-		return super.__tojava__(c);
-	}
-
+	return super.__tojava__(c);
+    }
 }
