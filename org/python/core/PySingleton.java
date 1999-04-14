91c99fa7e5848f1cdf9082e06f5d9c4988f32939
Barry Warsaw <barry@python.org>
1999-04-14 18:42:15 +0000

    intermediate

--- org/python/core/PySingleton.java
+++ org/python/core/PySingleton.java
@@ -1,11 +1,12 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.core;
 
 public class PySingleton extends PyObject {
-	private String name;
+    private String name;
 
-	public PySingleton(String name) {
-		this.name = name;
-	}
+    public PySingleton(String name) {
+	this.name = name;
+    }
 
     public String toString() {
         return name;
