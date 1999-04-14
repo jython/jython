cc507cd5866b2e928140292bcaa871e1d232ee51
Barry Warsaw <barry@python.org>
1999-04-14 19:29:28 +0000

    intermediate

--- org/python/core/PyTraceback.java
+++ org/python/core/PyTraceback.java
@@ -1,52 +1,55 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.core;
 
 public class PyTraceback extends PyObject {
-	public PyObject tb_next;
-	public PyFrame tb_frame;
-	public int tb_lineno;
+    public PyObject tb_next;
+    public PyFrame tb_frame;
+    public int tb_lineno;
 
-	public PyTraceback(PyFrame frame) {
-		tb_frame = frame;
-		if (tb_frame != null)
-			tb_lineno = tb_frame.getline();
-		tb_next = Py.None;
+    public PyTraceback(PyFrame frame) {
+	tb_frame = frame;
+	if (tb_frame != null)
+	    tb_lineno = tb_frame.getline();
+	tb_next = Py.None;
+    }
+
+    public PyTraceback(PyTraceback next) {
+	tb_next = next;
+	if (next != null) {
+	    tb_frame = next.tb_frame.f_back;
+	    tb_lineno = tb_frame.getline();
 	}
+    }
 
-	public PyTraceback(PyTraceback next) {
-		tb_next = next;
-		if (next != null) {
-			tb_frame = next.tb_frame.f_back;
-			tb_lineno = tb_frame.getline();
-		}
+    // filename, lineno, function_name
+    // "  File \"%.900s\", line %d, in %s\n"
+    private String line() {
+	if (tb_frame == null || tb_frame.f_code == null)
+	    return "  (no code object) at line "+tb_lineno+"\n";
+	return "  File \""+tb_frame.f_code.co_filename+
+	    "\", line "+tb_lineno+
+	    ", in "+tb_frame.f_code.co_name+"\n";
+    }
+
+    public void dumpStack(StringBuffer buf) {
+	//System.out.println("dumpStack: "+this+tb_next);
+	buf.append(line());
+	//System.out.println(line());
+	if (tb_next != Py.None && tb_next != this)
+	    ((PyTraceback)tb_next).dumpStack(buf);
+	else {
+	    if (tb_next == this) {
+		buf.append("circularity detected!"+this+tb_next);
+	    }
 	}
+    }
 
-	// filename, lineno, function_name
-	// "  File \"%.900s\", line %d, in %s\n"
-	private String line() {
-		if (tb_frame == null || tb_frame.f_code == null) return "  (no code object) at line "+tb_lineno+"\n";
-		return "  File \""+tb_frame.f_code.co_filename+
-					"\", line "+tb_lineno+
-					", in "+tb_frame.f_code.co_name+"\n";
-	}
+    public String dumpStack() {
+	StringBuffer buf = new StringBuffer();
 
-	public void dumpStack(StringBuffer buf) {
-		//System.out.println("dumpStack: "+this+tb_next);
-		buf.append(line());
-		//System.out.println(line());
-		if (tb_next != Py.None && tb_next != this) ((PyTraceback)tb_next).dumpStack(buf);
-		else {
-			if (tb_next == this) {
-				buf.append("circularity detected!"+this+tb_next);
-			}
-		}
-	}
+	buf.append("Traceback (innermost last):\n");
+	dumpStack(buf);
 
-	public String dumpStack() {
-		StringBuffer buf = new StringBuffer();
-
-		buf.append("Traceback (innermost last):\n");
-		dumpStack(buf);
-
-		return buf.toString();
-	}
+	return buf.toString();
+    }
 }
