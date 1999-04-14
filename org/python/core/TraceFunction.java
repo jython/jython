452c7fe384a173a21cd83641586e866e2af70368
Barry Warsaw <barry@python.org>
1999-04-14 19:37:09 +0000

    intermediate

--- org/python/core/TraceFunction.java
+++ org/python/core/TraceFunction.java
@@ -1,8 +1,10 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.core;
 
 public abstract class TraceFunction {
     public abstract TraceFunction traceCall(PyFrame frame);
     public abstract TraceFunction traceReturn(PyFrame frame, PyObject ret);
     public abstract TraceFunction traceLine(PyFrame frame, int line);
-    public abstract TraceFunction traceException(PyFrame frame, PyException exc);
+    public abstract TraceFunction traceException(PyFrame frame,
+						 PyException exc);
 }
