71de0c3bd674e575d5e5fb411a5cc9e31de8bb68
Barry Warsaw <barry@python.org>
1999-04-14 18:26:59 +0000

    intermediate

--- org/python/core/PyRunnable.java
+++ org/python/core/PyRunnable.java
@@ -1,5 +1,6 @@
+// Copyright © Corporation for National Research Initiatives
 package org.python.core;
 
 public interface PyRunnable {
-	abstract public PyCode getMain();
+    abstract public PyCode getMain();
 }
