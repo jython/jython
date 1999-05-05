9927f2284ba9b3c45d45e4ee243a60d102401ad9
Barry Warsaw <barry@python.org>
1999-05-05 19:21:07 +0000

    De-Windows-ify and generalize sys.path hacking
    Fix indentation

--- Lib/test/bugs/bugs101.py
+++ Lib/test/bugs/bugs101.py
@@ -1,42 +1,42 @@
 import sys
-sys.path.append('c:\\jpython\\cpd\\jpython\\scripts')
+import os
+dir = os.path.dirname(sys.argv[0])
+scriptsdir = os.path.normpath(os.path.join(dir, os.pardir, 'scripts'))
+sys.path.append(scriptsdir)
 from test_support import *
 
 def test931():
-	'exec and eval are not thread safe'
-	
-	from java.lang import Thread
+    '''exec and eval are not thread safe'''
 
-	class TestThread(Thread):
-	         def run(self):
-	                 for i in range(30):
-	                         exec("x=2+2")
-	
-	testers = []
-	for i in range(10):
-	         testers.append(TestThread())
-	
-	for tester in testers:
-	         tester.start()
-	   
+    from java.lang import Thread
 
-	for tester in testers:
-		tester.join()
-		
+    class TestThread(Thread):
+	def run(self):
+	    for i in range(30):
+		exec("x=2+2")
+
+    testers = []
+    for i in range(10):
+	testers.append(TestThread())
+
+    for tester in testers:
+	tester.start()
+
+
+    for tester in testers:
+	tester.join()
+
 code32 = """\
 def foo():
-	try:
-		2+2
-	finally:
-		return 4
+    try:
+	2+2
+    finally:
+	return 4
 """
 
 def test32():
-	'return in finally clause causes java.lang.VerifyError at compile time'
-	exec code32
-
-def test
-
+    'return in finally clause causes java.lang.VerifyError at compile time'
+    exec code32
 
 print_test('Bug Fixes', 0)
 print_test('From 1.0.1 to 1.0.2', 1)
@@ -46,12 +46,12 @@ items.sort()
 
 errors = 0
 for name, value in items:
-	if name[:4] == 'test':
-		print_test(value.__doc__+' #'+name[4:], 2)
-		try:
-			value()
-		except:
-			print 'Error!', sys.exc_type, sys.exc_value
-			errors = errors+1
+    if name[:4] == 'test':
+	print_test(value.__doc__+' #'+name[4:], 2)
+	try:
+	    value()
+	except:
+	    print 'Error!', sys.exc_type, sys.exc_value
+	    errors = errors+1
 
 print errors, 'errors'
