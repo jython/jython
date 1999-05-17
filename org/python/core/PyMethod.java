1cab5f0b765b90dcf7223cce097f7802099ceba5
Barry Warsaw <barry@python.org>
1999-05-17 19:59:39 +0000

    untabify
    __call__(PyObject[], String[]): im_self can be an instance of PyClass which provides a more CPython-ish toString() and a __cmp__() that works for bound methods.  I am not sure this change is correct, but it snuck in with the untabify change

--- org/python/core/PyMethod.java
+++ org/python/core/PyMethod.java
@@ -9,41 +9,60 @@ public class PyMethod extends PyObject {
 
     public static PyClass __class__;
     public PyMethod(PyObject self, PyObject f) {
-	super(__class__);
-	im_func = f;
-	im_self = self;
+        super(__class__);
+        im_func = f;
+        im_self = self;
     }
-	
+        
     public PyMethod(PyObject self, PyFunction f) {
-	this(self, (PyObject)f);
-	__name__ = f.__name__;
-	__doc__ = f.__doc__;
+        this(self, (PyObject)f);
+        __name__ = f.__name__;
+        __doc__ = f.__doc__;
     }
-	
+        
     public PyMethod(PyObject self, PyReflectedFunction f) {
-	this(self, (PyObject)f);
-	__name__ = f.__name__;
-	__doc__ = f.__doc__;
+        this(self, (PyObject)f);
+        __name__ = f.__name__;
+        __doc__ = f.__doc__;
     }
-	
+        
     /*private final boolean isBound() {
       return im_self != null && !(im_self instanceof PyClass);
       }*/
-	
+        
     public PyObject __call__(PyObject[] args, String[] keywords) {
-	if (im_self != null) {
-	    return im_func.__call__(im_self, args, keywords);
-	} else {
-	    // Verify that first arg is class instance?
-	    return im_func.__call__(args, keywords);
-	}
+        if (im_self instanceof PyClass)
+            // unbound method
+            // TBD: verify that first arg is class instance?
+            return im_func.__call__(args, keywords);
+        else
+            // bound method
+            return im_func.__call__(im_self, args, keywords);
+    }
+
+    public int __cmp__(PyObject other) {
+        if (other instanceof PyMethod) {
+            PyMethod mother = (PyMethod)other;
+            if (im_self == mother.im_self &&
+                im_func == mother.im_func)
+            {
+                return 0;
+            }
+        }
+        return -1;
     }
 
     public String toString() {
-	if (im_self != null) {
-	    return "<method "+__name__+" at "+Py.id(this)+">";
-	} else {
-	    return "<unbound method "+__name__+" at "+Py.id(this)+">";
-	}
+        if (im_self instanceof PyClass)
+            // this is an unbound method
+            return "<unbound method " + ((PyClass)im_self).__name__ + "." +
+                __name__ + ">";
+        else {
+            // this is a bound method
+            String classname = im_self.__class__.__name__;
+            return "<method " + classname + "." +
+                __name__ + " of " + classname + " instance at " +
+                Py.id(im_func) + ">";
+        }
     }
 }
