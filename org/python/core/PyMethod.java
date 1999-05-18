0c26a27a1c6ac045832dd93fae078a0aa443e2cd
Barry Warsaw <barry@python.org>
1999-05-18 18:01:44 +0000

    Implement im_class attribute for CPython compatibility.  im_class defaults to null, but usually gets set by a call to _setClass().
    _setClass(): New method, overrides from PyObject.
    
    __call__(): Slight rewrite (not important)
    
    __cmp__(): Model comparison after CPython's equivalent for instance
    methods.  One difference: compare im_func directly instead of calling
    im_func.__cmp__().
    
    toString(): CPython compatibility uses im_class attribute (but watches
    out for im_class == null... when can that happen?)

--- org/python/core/PyMethod.java
+++ org/python/core/PyMethod.java
@@ -4,6 +4,7 @@ package org.python.core;
 public class PyMethod extends PyObject {
     public PyObject im_self;
     public PyObject im_func;
+    public PyObject im_class;
     public String __name__;
     public PyObject __doc__;
 
@@ -26,43 +27,46 @@ public class PyMethod extends PyObject {
         __doc__ = f.__doc__;
     }
         
+    public void _setClass(PyObject myclass) {
+        im_class = myclass;
+    }
+
     /*private final boolean isBound() {
       return im_self != null && !(im_self instanceof PyClass);
       }*/
         
     public PyObject __call__(PyObject[] args, String[] keywords) {
-        if (im_self instanceof PyClass)
-            // unbound method
-            // TBD: verify that first arg is class instance?
-            return im_func.__call__(args, keywords);
-        else
+        if (im_self != null)
             // bound method
             return im_func.__call__(im_self, args, keywords);
+        // unbound method.  TBD: Verify that the first argument is a class
+        // instance.
+        System.err.println("unbound check");
+        return im_func.__call__(args, keywords);
     }
 
     public int __cmp__(PyObject other) {
         if (other instanceof PyMethod) {
             PyMethod mother = (PyMethod)other;
-            if (im_self == mother.im_self &&
-                im_func == mother.im_func)
-            {
-                return 0;
-            }
+            if (im_self != mother.im_self)
+                return Py.id(im_self) < Py.id(mother.im_self) ? -1 : 1;
+            if (im_func != mother.im_func)
+                return Py.id(im_func) < Py.id(mother.im_func) ? -1 : 1;
+            return 0;
         }
-        return -1;
+        return -2;
     }
 
     public String toString() {
-        if (im_self instanceof PyClass)
+        String classname = "?";
+        if (im_class != null && im_class instanceof PyClass)
+            classname = ((PyClass)im_class).__name__;
+        if (im_self == null)
             // this is an unbound method
-            return "<unbound method " + ((PyClass)im_self).__name__ + "." +
-                __name__ + ">";
-        else {
-            // this is a bound method
-            String classname = im_self.__class__.__name__;
+            return "<unbound method " + classname + "." + __name__ + ">";
+        else
             return "<method " + classname + "." +
                 __name__ + " of " + classname + " instance at " +
                 Py.id(im_func) + ">";
-        }
     }
 }
