efc601a4ff4e6ae078713e3546c23c99f8e7917c
Barry Warsaw <barry@python.org>
1999-05-18 14:39:22 +0000

    Integrate Jim Hugunin's patch to fix PR#104:
        This bug is caused by the fact that imp.java is not properly
        checking for ParseException's that might occur during compilation
        in order to turn them into standard SyntaxError's.  Add the same
        check here as is used in Py.compile.

--- org/python/core/imp.java
+++ org/python/core/imp.java
@@ -125,8 +125,8 @@ public class imp {
             }
 
             return ofp.toByteArray();
-        } catch (Exception e) {
-            throw Py.JavaError(e);
+        } catch (Throwable t) {
+            throw parser.fixParseError(null, t, filename);
         }
     }
 
