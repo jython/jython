2b2992e737493ca30399b5bc74fd4490da7dc066
Barry Warsaw <barry@python.org>
1999-05-19 20:12:48 +0000

    intermediate

--- Tools/jythonc/main.py
+++ Tools/jythonc/main.py
@@ -26,19 +26,19 @@ where options include:
 from compile import Compiler
 import sys, string, os
 from Options import *
-		
+                
 def addCore(extraPackages):
     skiplist = ['org.python.core.parser',
-		'org.python.core.BytecodeLoader',
-		'org.python.core.jpython',
-		]
+                'org.python.core.BytecodeLoader',
+                'org.python.core.jpython',
+                ]
     extraPackages.append( ('org.python.core', skiplist) )
 
 
 def addAll(extraPackages):
     for name in ['core', 'compiler', 'parser']:
-	extraPackages.append(('org.python.'+name, []))
-		
+        extraPackages.append(('org.python.'+name, []))
+                
                       
 ## XXX: This seems unused! -baw
 ##options_to_add = """
@@ -48,45 +48,45 @@ def addAll(extraPackages):
 def usage(errcode, msg=''):
     print __doc__ % globals()
     if msg:
-	print msg
+        print msg
     sys.exit(errcode)
 
 
 def getOptions():
     try:
-	opts = Options({'jar'        : None,
-			'workdir'    :"jpywork",
-			'core'       : YesNo,
-			'all'        : YesNo,
-			'deep'       : YesNo,
-			'bean'       : None,
-			'skip'       : "",
-			'package'    : None,
-			'addfiles'   : "",
-			'falsenames' : "",
-			'compiler'   : None,
-			'bean'       : None,
-			'addpackages': "",
-			})
+        opts = Options({'jar'        : None,
+                        'workdir'    :"jpywork",
+                        'core'       : YesNo,
+                        'all'        : YesNo,
+                        'deep'       : YesNo,
+                        'bean'       : None,
+                        'skip'       : "",
+                        'package'    : None,
+                        'addfiles'   : "",
+                        'falsenames' : "",
+                        'compiler'   : None,
+                        'bean'       : None,
+                        'addpackages': "",
+                        })
     except KeyError, value:
-	print 'illegal argument: -'+value
-	print
-	usage()
+        print 'illegal argument: -'+value
+        print
+        usage()
 
     if len(opts.args) == 0:
-	print 'nothing to compile'
-	print
-	usage()
+        print 'nothing to compile'
+        print
+        usage()
 
     if opts.jar is not None:
-	opts.deep = 1
+        opts.deep = 1
     elif opts.core or opts.all:
-	opts.deep = 1
+        opts.deep = 1
 
     opts.falsenames = opts.falsenames.split(",")
 
     if not os.path.isabs(opts.workdir):
-	opts.workdir = os.path.join(".", opts.workdir)	
+        opts.workdir = os.path.join(".", opts.workdir)  
 
     return opts
 
@@ -97,33 +97,33 @@ def doCompile(opts):
     skiplist = string.split(opts.skip, ",")
     optpkgs = string.split(opts.addpackages, ',')
     addpackages = ['org.python.modules',
-		   'com.oroinc.text.regex'] + optpkgs
+                   'com.oroinc.text.regex'] + optpkgs
 
     comp = Compiler(javapackage=opts.package,
-		    deep=opts.deep,
-		    include=addpackages,
-		    skip=skiplist,
-		    options=opts)
+                    deep=opts.deep,
+                    include=addpackages,
+                    skip=skiplist,
+                    options=opts)
     global mainclass
     global basepath
 
     for target in opts.args:
-	if target.endswith('.py'):
-	    classname = os.path.splitext(os.path.basename(target))[0]
-	    filename = target
-	    if basepath is None:
-		basepath = os.path.split(target)[0]
-		sys.path.insert(0, basepath)
-	else:
-	    classname = target
-	    import ImportName
-	    m = ImportName.lookupName(classname)
-	    filename = m.file
-	if mainclass is None:
-	    mainclass = classname
-	    if opts.package is not None:
-		mainclass = opts.package+'.'+mainclass
-	comp.compilefile(filename, classname)
+        if target.endswith('.py'):
+            classname = os.path.splitext(os.path.basename(target))[0]
+            filename = target
+            if basepath is None:
+                basepath = os.path.split(target)[0]
+                sys.path.insert(0, basepath)
+        else:
+            classname = target
+            import ImportName
+            m = ImportName.lookupName(classname)
+            filename = m.file
+        if mainclass is None:
+            mainclass = classname
+            if opts.package is not None:
+                mainclass = opts.package+'.'+mainclass
+        comp.compilefile(filename, classname)
 
     comp.dump(opts.workdir)
     return comp
@@ -139,7 +139,7 @@ def copyclass(jc, fromdir, todir):
     toname = os.path.join(todir, name)
     tohead = os.path.split(toname)[0]
     if not os.path.exists(tohead):
-	os.makedirs(tohead)
+        os.makedirs(tohead)
     istream = FileInputStream(fromname)
     ostream = FileOutputStream(toname)
     jar.copy(istream, ostream)
@@ -155,19 +155,19 @@ def writeResults(comp, opts):
     javaclasses = comp.javaclasses
 
     if opts.bean is not None:
-	jarfile = opts.bean
+        jarfile = opts.bean
     else:
-	jarfile = opts.jar		
+        jarfile = opts.jar              
 
     if jarfile is None:
-	if not opts.deep and opts.package is not None:
-	    for jc in javaclasses:
-		if isinstance(jc, type( () )):
-		    jc = jc[0]
-		if basepath is None:
-		    basepath = '.'
-		copyclass(jc, opts.workdir, basepath)
-	sys.exit(0)
+        if not opts.deep and opts.package is not None:
+            for jc in javaclasses:
+                if isinstance(jc, type( () )):
+                    jc = jc[0]
+                if basepath is None:
+                    basepath = '.'
+                copyclass(jc, opts.workdir, basepath)
+        sys.exit(0)
 
     print 'Building archive:', jarfile
     from jar import JavaArchive
@@ -175,23 +175,23 @@ def writeResults(comp, opts):
     extraPackages = []
 
     if opts.core:
-	addCore(extraPackages)
+        addCore(extraPackages)
     if opts.all:
-	addAll(extraPackages)
+        addAll(extraPackages)
 
     ja = JavaArchive(extraPackages)
     ja.addToManifest({'Main-Class':mainclass})
     for jc in javaclasses:
-	if isinstance(jc, type( () )):
-	    ja.addClass(opts.workdir, jc[0], jc[1])
-	else:
-	    ja.addClass(opts.workdir, jc)
+        if isinstance(jc, type( () )):
+            ja.addClass(opts.workdir, jc[0], jc[1])
+        else:
+            ja.addClass(opts.workdir, jc)
 
     for dep in comp.trackJavaDependencies():
-	ja.addEntry(dep)
+        ja.addEntry(dep)
 
     ja.dump(jarfile)
-	
+        
 
 
 def main():
