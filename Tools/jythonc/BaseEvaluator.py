7fe4f33c21f370c615e72dd7416598a24641882d
Barry Warsaw <barry@python.org>
1999-05-19 20:06:54 +0000

    untabify

--- Tools/jythonc/BaseEvaluator.py
+++ Tools/jythonc/BaseEvaluator.py
@@ -6,204 +6,204 @@ import string
 
 class BaseEvaluator:
     def __init__(self):
-	self.globalnames = {}
-	self.lineno = -1
-	self.visitor = PythonVisitor(self)
+        self.globalnames = {}
+        self.lineno = -1
+        self.visitor = PythonVisitor(self)
 
     def parse(self, node):
-	try:
-	    return self.visit(node)
-	except:
-	    print 'Parsing line: %d' % self.lineno
-	    if hasattr(self, 'data') and self.lineno > 0:
-		print string.split(self.data, '\n')[self.lineno-1]
-	    raise
+        try:
+            return self.visit(node)
+        except:
+            print 'Parsing line: %d' % self.lineno
+            if hasattr(self, 'data') and self.lineno > 0:
+                print string.split(self.data, '\n')[self.lineno-1]
+            raise
 
     def setline(self, lineno):
-	self.lineno = lineno
+        self.lineno = lineno
 
     def getName(self, name):
-	return self.visitor.getName(name)
+        return self.visitor.getName(name)
 
     def visit(self, node):
-	return node.visit(self.visitor)
+        return node.visit(self.visitor)
 
     def suite(self, nodes):
-	ret = []
-	for node in nodes:
-	    ret.append(self.visit(node))
-	return ret
+        ret = []
+        for node in nodes:
+            ret.append(self.visit(node))
+        return ret
 
     def del_stmt(self, nodes):
-	stmts = []
-	for node in nodes:
-	    stmts.append(self.delete(node))
-	return stmts
+        stmts = []
+        for node in nodes:
+            stmts.append(self.delete(node))
+        return stmts
 
     def delete(self, node):
-	if node.id == JJTNAME:
-	    return self.del_name(node.getInfo())
-	elif node.id == JJTLIST or node.id == JJTTUPLE:
-	    return self.del_list(nodeToList(node))
-	elif node.id == JJTINDEX_OP:
-	    return self.del_item(node.getChild(0), 
-			    node.getChild(1))
-	elif node.id == JJTDOT_OP:
-	    return self.del_attribute(node.getChild(0),
-			    node.getChild(1).getInfo())	
-	else:
-	    raise TypeError, 'help, fancy lhs: %s' % node
+        if node.id == JJTNAME:
+            return self.del_name(node.getInfo())
+        elif node.id == JJTLIST or node.id == JJTTUPLE:
+            return self.del_list(nodeToList(node))
+        elif node.id == JJTINDEX_OP:
+            return self.del_item(node.getChild(0), 
+                            node.getChild(1))
+        elif node.id == JJTDOT_OP:
+            return self.del_attribute(node.getChild(0),
+                            node.getChild(1).getInfo()) 
+        else:
+            raise TypeError, 'help, fancy lhs: %s' % node
 
     def del_list(self, seq):
-	return self.del_stmt(seq)
+        return self.del_stmt(seq)
 
     def del_item(self, obj, index):
-	if index.id == JJTSLICE:
-	    start, stop, step = self.getSlice(index)
-	    return self.visit(obj).delslice(start, stop, step)
-	return self.visit(obj).delitem(self.visit(index))
+        if index.id == JJTSLICE:
+            start, stop, step = self.getSlice(index)
+            return self.visit(obj).delslice(start, stop, step)
+        return self.visit(obj).delitem(self.visit(index))
 
     def del_attribute(self, obj, name):
-	return self.visit(obj).delattr(name)
+        return self.visit(obj).delattr(name)
 
     def set(self, node, value):
-	if node.id == JJTNAME:
-	    return self.set_name(node.getInfo(), value)
-	elif node.id == JJTLIST or node.id == JJTTUPLE:
-	    return self.set_list(nodeToList(node), value)
-	elif node.id == JJTINDEX_OP:
-	    return self.set_item(node.getChild(0), 
-			    node.getChild(1), value)
-	elif node.id == JJTDOT_OP:
-	    return self.set_attribute(node.getChild(0),
-			    node.getChild(1).getInfo(), value)	
-	else:
-	    raise TypeError, 'help, fancy lhs: %s' % node
+        if node.id == JJTNAME:
+            return self.set_name(node.getInfo(), value)
+        elif node.id == JJTLIST or node.id == JJTTUPLE:
+            return self.set_list(nodeToList(node), value)
+        elif node.id == JJTINDEX_OP:
+            return self.set_item(node.getChild(0), 
+                            node.getChild(1), value)
+        elif node.id == JJTDOT_OP:
+            return self.set_attribute(node.getChild(0),
+                            node.getChild(1).getInfo(), value)  
+        else:
+            raise TypeError, 'help, fancy lhs: %s' % node
 
     def set_list(self, seq, value):
-	n = len(seq)
-	tmp, code = self.makeTemp(value)
-	stmts = [code]
+        n = len(seq)
+        tmp, code = self.makeTemp(value)
+        stmts = [code]
 
-	for i in range(n):
-	    stmts.append(self.set(seq[i], tmp.igetitem(i)))
-	self.freeTemp(tmp)
-	return stmts
+        for i in range(n):
+            stmts.append(self.set(seq[i], tmp.igetitem(i)))
+        self.freeTemp(tmp)
+        return stmts
 
     def set_item(self, obj, index, value):
-	if index.id == JJTSLICE:
-	    start, stop, step = self.getSlice(index)
-	    return self.visit(obj).setslice(start, stop, step, value)
-	return self.visit(obj).setitem(self.visit(index), value)
+        if index.id == JJTSLICE:
+            start, stop, step = self.getSlice(index)
+            return self.visit(obj).setslice(start, stop, step, value)
+        return self.visit(obj).setitem(self.visit(index), value)
 
     def set_attribute(self, obj, name, value):
-	return self.visit(obj).setattr(name, value)
+        return self.visit(obj).setattr(name, value)
 
     def get_item(self, obj, index):
-	if index.id == JJTSLICE:
-	    start, stop, step = self.getSlice(index)
-	    return self.visit(obj).getslice(start, stop, step)
-	return self.visit(obj).getitem(self.visit(index))
+        if index.id == JJTSLICE:
+            start, stop, step = self.getSlice(index)
+            return self.visit(obj).getslice(start, stop, step)
+        return self.visit(obj).getitem(self.visit(index))
 
     def get_attribute(self, obj, name):
-	return self.visit(obj).getattr(name)
+        return self.visit(obj).getattr(name)
 
     def makeTemp(self, value):
-	return value
+        return value
 
     def freeTemp(self, tmp): pass
 
     def expr_stmt(self, lhss, rhs):
-	if len(lhss) == 0:
-	    return self.visit(rhs).makeStatement()
-	if len(lhss) == 1:
-	    return self.set(lhss[0], self.visit(rhs))
+        if len(lhss) == 0:
+            return self.visit(rhs).makeStatement()
+        if len(lhss) == 1:
+            return self.set(lhss[0], self.visit(rhs))
 
-	tmp, code = self.makeTemp(self.visit(rhs))
-	stmts = [code]
-	for lhs in lhss:
-	    stmts.append(self.set(lhs, tmp))
-	self.freeTemp(tmp)
-	return stmts
+        tmp, code = self.makeTemp(self.visit(rhs))
+        stmts = [code]
+        for lhs in lhss:
+            stmts.append(self.set(lhs, tmp))
+        self.freeTemp(tmp)
+        return stmts
 
     def binary_op(self, name, x, y):
-	return self.visit(x).binop(name, self.visit(y))
+        return self.visit(x).binop(name, self.visit(y))
 
     def unary_op(self, name, x):
-	return self.visit(x).unop(name)
+        return self.visit(x).unop(name)
 
     def compare_op(self, start, compares):
-	x = self.visit(start)
-	for op, other in compares:
-	    y = self.visit(other)
-	    test = x.compop(op, y)
-	    if not test.nonzero():
-		return test
-	    x = y
-	return test
+        x = self.visit(start)
+        for op, other in compares:
+            y = self.visit(other)
+            test = x.compop(op, y)
+            if not test.nonzero():
+                return test
+            x = y
+        return test
 
     def print_line(self, value=None):
-	if value is None:
-	    print
-	else:
-	    return self.visit(value).print_line()
+        if value is None:
+            print
+        else:
+            return self.visit(value).print_line()
 
     def print_continued(self, value):
-	return self.visit(value).print_continued()
+        return self.visit(value).print_continued()
 
     def visitall(self, args):
-	ret = []
-	for arg in args:
-	    ret.append(self.visit(arg))
-	return ret
+        ret = []
+        for arg in args:
+            ret.append(self.visit(arg))
+        return ret
 
     def visitnames(self, kws):
-	ret = []
-	for name, value in kws:
-	    ret.append( (name, self.visit(value)) )
-	return ret
+        ret = []
+        for name, value in kws:
+            ret.append( (name, self.visit(value)) )
+        return ret
 
     def invoke(self, obj, name, args, kws):
-	return self.visit(obj).invoke(name, self.visitall(args),
-				      self.visitnames(kws))
+        return self.visit(obj).invoke(name, self.visitall(args),
+                                      self.visitnames(kws))
 
     def call(self, callee, args, kws):
-	return self.visit(callee).call(self.visitall(args),
-				       self.visitnames(kws))
+        return self.visit(callee).call(self.visitall(args),
+                                       self.visitnames(kws))
 
     def global_stmt(self, names):
-	for name in names:
-	    self.globalnames[name] = 1
+        for name in names:
+            self.globalnames[name] = 1
 
     def import_stmt(self, names):
-	ret = []
-	for name in names:
-	    ret.append(self.set_name(name[0], self.get_module(name)))
-	return ret
+        ret = []
+        for name in names:
+            ret.append(self.set_name(name[0], self.get_module(name)))
+        return ret
 
     def importall_stmt(self, top):
-	pass
+        pass
 
-    def importfrom_stmt(self, top, names):			
-	module = self.get_module(top, 1)
-	if names == '*':
-	    return self.importall_stmt(module)
-	    #print 'import * from', module
-	    #names = module.dir()
-	ret = []
-	for name in names:
-	    ret.append(self.set_name(name, module.getattr(name)))
-	return ret
+    def importfrom_stmt(self, top, names):                      
+        module = self.get_module(top, 1)
+        if names == '*':
+            return self.importall_stmt(module)
+            #print 'import * from', module
+            #names = module.dir()
+        ret = []
+        for name in names:
+            ret.append(self.set_name(name, module.getattr(name)))
+        return ret
 
     #external interfaces
     def execstring(self, data):
-	self.data = data
-	from org.python.core import parser
-	node = parser.parse(data, 'exec')
-	return self.parse(node)
+        self.data = data
+        from org.python.core import parser
+        node = parser.parse(data, 'exec')
+        return self.parse(node)
 
     def execfile(self, filename):
-	fp = open(filename, 'r')
-	data = fp.read()
-	fp.close()
-	return execstring(data)
+        fp = open(filename, 'r')
+        data = fp.read()
+        fp.close()
+        return execstring(data)
--- Tools/jythonc/JavaCall.py
+++ Tools/jythonc/JavaCall.py
@@ -7,120 +7,120 @@ import string
 funcs = {}
 def call(func, this, args=None):
     if not funcs.has_key(func):
-	jf = JavaFunction(func)
-	funcs[func] = jf
+        jf = JavaFunction(func)
+        funcs[func] = jf
     else:
-	jf = funcs[func]
+        jf = funcs[func]
     if args is None:
-	return jf.call(this)
+        return jf.call(this)
     else:
-	return jf.invoke(this, args)
+        return jf.invoke(this, args)
 
 
 
 class Signature:
     def __init__(self, name, inTypes, retType, isStatic=0, isPyArgs=0,
-		 isConstructor=0):
-	self.isPyArgs = isPyArgs
-	self.isStatic = isStatic
-	self.retType = retType
-	self.inTypes = inTypes
-	self.name = name
-	self.isConstructor = isConstructor
+                 isConstructor=0):
+        self.isPyArgs = isPyArgs
+        self.isStatic = isStatic
+        self.retType = retType
+        self.inTypes = inTypes
+        self.name = name
+        self.isConstructor = isConstructor
 
     def handle(self, callee, args, isa=1):
-##	if self.isPyArgs:
-##	    pyargs = []
-##	    for arg in args:
-##		pyargs.append(arg.asa(AnyType).getCode())
-##	    return self.mkcall(callee, [Statement.PyObjectArray(pyargs)] )
+##      if self.isPyArgs:
+##          pyargs = []
+##          for arg in args:
+##              pyargs.append(arg.asa(AnyType).getCode())
+##          return self.mkcall(callee, [Statement.PyObjectArray(pyargs)] )
 
-	if len(args) != len(self.inTypes):
-	    return None
+        if len(args) != len(self.inTypes):
+            return None
 
-	callargs = []
-	for i in range(len(args)):
-	    #print i, args[i]
-	    if isa:
-		callarg = args[i].isa(self.inTypes[i])
-	    else:
-		callarg = args[i].asa(self.inTypes[i])
-	    if callarg == None:
-		return None
-	    callargs.append(callarg)
-	return self.mkcall(callee, callargs)
+        callargs = []
+        for i in range(len(args)):
+            #print i, args[i]
+            if isa:
+                callarg = args[i].isa(self.inTypes[i])
+            else:
+                callarg = args[i].asa(self.inTypes[i])
+            if callarg == None:
+                return None
+            callargs.append(callarg)
+        return self.mkcall(callee, callargs)
 
     def mkcall(self, callee, args):
-	if self.isConstructor:
-	    op = jast.New(self.name, args)
-	elif self.isStatic:
-	    op = jast.InvokeStatic(callee, self.name, args)
-	else:
-	    #print callee, self.name, args
-	    op = jast.Invoke(callee, self.name, args)
-	return op, self.retType
+        if self.isConstructor:
+            op = jast.New(self.name, args)
+        elif self.isStatic:
+            op = jast.InvokeStatic(callee, self.name, args)
+        else:
+            #print callee, self.name, args
+            op = jast.Invoke(callee, self.name, args)
+        return op, self.retType
 
     def __repr__(self):
-	r = []
-	for arg in self.inTypes:
-	    r.append(arg.name)
-	return '('+string.join(r, ', ')+')->'+self.retType.name
+        r = []
+        for arg in self.inTypes:
+            r.append(arg.name)
+        return '('+string.join(r, ', ')+')->'+self.retType.name
 
 
 class JavaFunction:
     def __init__(self, reflectedFunction):
-	self.name = reflectedFunction.__name__
-	self.isConstructor = isinstance(reflectedFunction,
-					org.python.core.PyReflectedConstructor)
-	sigs = []
-	self.callee = None
-	for i in range(reflectedFunction.nargs):
-	    sigs.append(self.makeSignature(reflectedFunction.argslist[i]))
-	self.sigs = sigs
-	self.function = reflectedFunction
-	self.declaringClass = self.function.argslist[0].declaringClass
+        self.name = reflectedFunction.__name__
+        self.isConstructor = isinstance(reflectedFunction,
+                                        org.python.core.PyReflectedConstructor)
+        sigs = []
+        self.callee = None
+        for i in range(reflectedFunction.nargs):
+            sigs.append(self.makeSignature(reflectedFunction.argslist[i]))
+        self.sigs = sigs
+        self.function = reflectedFunction
+        self.declaringClass = self.function.argslist[0].declaringClass
 
     def makeSignature(self, rargs):
-	inTypes = list(rargs.args)
-	isPyArgs = rargs.flags == rargs.PyArgsCall
-	isStatic = rargs.isStatic
-	name = rargs.data.name
+        inTypes = list(rargs.args)
+        isPyArgs = rargs.flags == rargs.PyArgsCall
+        isStatic = rargs.isStatic
+        name = rargs.data.name
 
-	if self.isConstructor:
-	    outType = rargs.data.declaringClass			
-	else:
-	    outType = rargs.data.returnType
+        if self.isConstructor:
+            outType = rargs.data.declaringClass                 
+        else:
+            outType = rargs.data.returnType
 
-	if isStatic:
-	    self.callee = rargs.data.declaringClass.__name__
+        if isStatic:
+            self.callee = rargs.data.declaringClass.__name__
 
-	return Signature(name, inTypes, outType, isStatic, isPyArgs,
-			 self.isConstructor)
+        return Signature(name, inTypes, outType, isStatic, isPyArgs,
+                         self.isConstructor)
 
     def __repr__(self):
-	if len(self.sigs) == 1:
-	    return self.name+repr(self.sigs[0])
-	else:
-	    return self.name+repr(self.sigs)
+        if len(self.sigs) == 1:
+            return self.name+repr(self.sigs[0])
+        else:
+            return self.name+repr(self.sigs)
 
     def call(self, args):
-	for isa in [1, 0]:
-	    for sig in self.sigs:
-		ret = sig.handle(self.callee, args, isa=isa)
-		if ret is None:
-		    continue
-		return ret
-	raise TypeError, repr(args)+' args do not match: '+repr(self)
+        for isa in [1, 0]:
+            for sig in self.sigs:
+                ret = sig.handle(self.callee, args, isa=isa)
+                if ret is None:
+                    continue
+                return ret
+        raise TypeError, repr(args)+' args do not match: '+repr(self)
 
     def invoke(self, this, args):
-	this = this.asa(self.declaringClass)
-	for isa in [1, 0]:
-	    for sig in self.sigs:
-		ret = sig.handle(this, args, isa=isa)
-		if ret is None:
-		    continue
-		return ret
-	raise TypeError, repr(args)+' args do not match: '+repr(self)
+        this = this.asa(self.declaringClass)
+        for isa in [1, 0]:
+            for sig in self.sigs:
+                ret = sig.handle(this, args, isa=isa)
+                if ret is None:
+                    continue
+                return ret
+        raise TypeError, repr(args)+' args do not match: '+repr(self)
 
 
 
@@ -129,29 +129,29 @@ if __name__ == '__main__':
     print f
 
     class Object:
-	def __init__(self, code, istypes, astypes):
-	    self.code = code
-	    self.istypes = istypes
-	    self.astypes = astypes
+        def __init__(self, code, istypes, astypes):
+            self.code = code
+            self.istypes = istypes
+            self.astypes = astypes
 
-	def isa(self, type):
-	    if type in self.istypes:
-		return self.code
+        def isa(self, type):
+            if type in self.istypes:
+                return self.code
 
-	def asa(self, type):
-	    if type in self.istypes:
-		return self.code
+        def asa(self, type):
+            if type in self.istypes:
+                return self.code
 
-	    for astype, ascode in self.astypes:
-		if astype == type:
-		    return ascode
+            for astype, ascode in self.astypes:
+                if astype == type:
+                    return ascode
 
     one = Object(jast.IntegerConstant(1), [java.lang.Integer.TYPE], 
-		 [(org.python.core.PyObject, jast.Identifier("one"))])
+                 [(org.python.core.PyObject, jast.Identifier("one"))])
     foo = Object(jast.Identifier("foo"), [org.python.core.PyObject],
-		 [(java.lang.Integer.TYPE,
-		   jast.InvokeStatic("Py", "toint",
-				     [jast.Identifier("foo")]))])
+                 [(java.lang.Integer.TYPE,
+                   jast.InvokeStatic("Py", "toint",
+                                     [jast.Identifier("foo")]))])
 
     print f.invoke(foo, [one])
     print f.invoke(foo, [foo])
