b1a6fb0ef1064c5a9db695e100c7a79fdd0fd015
Barry Warsaw <barry@python.org>
1999-06-03 20:44:09 +0000

    Pulling in the current documentation from Website.  This will need some massaging though.

---
+++ Doc/links.h
@@ -0,0 +1,22 @@
+<!-- -*- html -*- -->
+<h3>Documentation</h3>
+<li><a href="./">Overview</a>
+<li><a href="whatis.html">Executive Summary</a>
+<li><a href="interpreter.html">Invoking JPython</a>
+<li><a href="registry.html">JPython Registry</a>
+<li><a href="embedding.html">Embedding</a>
+<li><a href="compile.html">Compiling JPython from source</a>
+<h3>Working with Java</h3>
+<li><a href="usejava.html">The Basics</a>
+<li><a href="properties.html">JavaBean Properties</a>
+<li><a href="jarray.html">Java arrays</a>
+<li><a href="subclassing.html">Subclassing</a>
+<li><a href="jpythonc.html">Building applets, servlets, beans...</a>
+<h3>Python Docs (exits)</h3>
+<li><a href="http://www.python.org/doc/tut/tut.html">Python Tutorial</a>
+<li><a href="http://www.python.org/doc/lib/lib.html">Library Reference</a>
+<h3>Other</h3>
+<li><a href="differences.html">JPython vs. CPython</a>
+<li><a href="faq.html">JPython FAQ</a>
+<li><a href="http://www.python.org/pipermail/jpython-interest/">List Archives</a>
+<LI><A HREF="http://www.python.org/workshops/1997-10/proceedings/hugunin.html">JPython paper</A>
