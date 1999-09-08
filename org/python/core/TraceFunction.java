a27fbbda618fcb71608d92f5039b3eda1f03be56
Barry Warsaw <barry@python.org>
1999-09-08 22:15:34 +0000

    class PythonTraceFunction moved to its own source file Minor formatting

--- org/python/core/TraceFunction.java
+++ org/python/core/TraceFunction.java
@@ -1,7 +1,8 @@
 // Copyright © Corporation for National Research Initiatives
 package org.python.core;
 
-public abstract class TraceFunction {
+public abstract class TraceFunction
+{
     public abstract TraceFunction traceCall(PyFrame frame);
     public abstract TraceFunction traceReturn(PyFrame frame, PyObject ret);
     public abstract TraceFunction traceLine(PyFrame frame, int line);
