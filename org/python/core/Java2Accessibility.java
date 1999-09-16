2efdb88ba2dfb56d6305c38b85db37ef7ef9591f
Barry Warsaw <barry@python.org>
1999-09-16 21:56:33 +0000

    intermediate

---
+++ org/python/core/Java2Accessibility.java
@@ -0,0 +1,26 @@
+package org.python.core;
+import java.lang.reflect.*;
+
+/**
+ * Provides the Java 2 {Field|Method|Constructor}.setAccessibility()
+ * methods when compiled with Java 2.
+ *
+ * This class should not be compilied (and it won't compile) under Java 1.
+ */
+
+public class Java2Accessibility extends JavaAccessibility
+{
+    void setAccess(Field field, boolean flag) throws SecurityException {
+	field.setAccessible(flag);
+    }
+
+    void setAccess(Method method, boolean flag) throws SecurityException {
+	method.setAccessible(flag);
+    }
+
+    void setAccess(Constructor constructor, boolean flag)
+        throws SecurityException
+    {
+ 	constructor.setAccessible(flag);
+    }
+}
