fb940d7fd5ddd3aa515d5b0e41e3bfff22dd9815
Barry Warsaw <barry@python.org>
1999-09-16 22:04:35 +0000

    respectJavaAccessibility: new boolean member which controls whether Java access rules should be respected or whether they can be overridden.  When false, and running under JDK 1.2, non-public members can be accessed from Python.
    getBooleanOption(), getStringOption(), setFromRegistry(): Moved here
    from PySystemState.java for better localization.
    
    setFromRegistry(): Add other initializations that must be performed
    after the Options members are completely initialized.
    JavaAccessibility.initialize() is the only current call.
    
    Started an attempt to document each option variable.  Didn't get very
    far...

--- org/python/core/Options.java
+++ org/python/core/Options.java
@@ -1,18 +1,118 @@
 // Copyright © Corporation for National Research Initiatives
 package org.python.core;
 
-public class Options {
-    public static boolean showJavaExceptions=false;
-    public static String proxyCacheDirectory=null;
-    public static boolean showPythonProxyExceptions=false;
-    public static boolean skipCompile=true;
-    public static boolean verbosePackageCache=false;
-    public static boolean pollStandardIn=false;
-    public static boolean classBasedExceptions=true;
-    public static boolean importSite=true;
-    public static int verbose=Py.MESSAGE;
+public class Options
+{
+    // JPython options.  Some of these can be set from the command line
+    // options, but all can be controlled through the JPython registry
+
+    // when an exception occurs in Java code, and it is not caught, should
+    // the interpreter print out the Java exception in the traceback?
+    public static boolean showJavaExceptions = false;
+
+    // if this is not null, it must be a string indicating the directory to 
+    // save proxy adapter class files to
+    public static String proxyCacheDirectory = null;
+
+    // TBD
+    public static boolean showPythonProxyExceptions = false;
+
+    // TBD
+    public static boolean skipCompile = true;
+
+    // TBD
+    public static boolean verbosePackageCache = false;
+
+    // TBD
+    public static boolean pollStandardIn = false;
+
+    // TBD
+    public static boolean classBasedExceptions = true;
+
+    // If true, JPython respects Java the accessibility flag for fields,
+    // methods, and constructors.  This means you can only access public
+    // members.  Set this to false to access all members by toggling the
+    // accessible flag on the member.
+    public static boolean respectJavaAccessibility = true;
+
+    // TBD
+    public static boolean importSite = true;
+
+    // TBD
+    public static int verbose = Py.MESSAGE;
     
-    public static boolean deprecatedKeywordMangling=true;
+    // TBD
+    public static boolean deprecatedKeywordMangling = true;
     
-    public static boolean parserVerboseExceptions=false;
+    // TBD
+    public static boolean parserVerboseExceptions = false;
+
+    //
+    // ####### END OF OPTIONS
+    //
+
+    private static boolean getBooleanOption(String name, boolean defaultValue)
+    {
+        String prop = PySystemState.registry.getProperty("python."+name);
+        if (prop == null)
+            return defaultValue;
+        return prop.equalsIgnoreCase("true") || prop.equalsIgnoreCase("yes");
+    }
+    
+    private static String getStringOption(String name, String defaultValue) {
+        String prop = PySystemState.registry.getProperty("python."+name);
+        if (prop == null)
+            return defaultValue;
+        return prop;
+    }
+
+    public static void setFromRegistry() {
+        // Set the more unusual options
+        Options.showJavaExceptions =
+            getBooleanOption("options.showJavaExceptions",
+                             Options.showJavaExceptions);
+
+        Options.showPythonProxyExceptions = 
+            getBooleanOption("options.showPythonProxyExceptions",
+                             Options.showPythonProxyExceptions);
+
+        Options.skipCompile = 
+            getBooleanOption("options.skipCompile", Options.skipCompile);
+
+        Options.deprecatedKeywordMangling = 
+            getBooleanOption("deprecated.keywordMangling",
+                             Options.deprecatedKeywordMangling);
+
+        Options.pollStandardIn =
+            getBooleanOption("console.poll", Options.pollStandardIn);
+
+        Options.classBasedExceptions =
+            getBooleanOption("options.classExceptions",
+                             Options.classBasedExceptions);
+
+        Options.respectJavaAccessibility =
+            getBooleanOption("security.respectJavaAccessibility",
+                             Options.respectJavaAccessibility);
+
+        // verbosity is more complicated:
+        String prop = PySystemState.registry.getProperty("python.verbose");
+        if (prop != null) {
+            if (prop.equalsIgnoreCase("error"))
+                Options.verbose = Py.ERROR;
+            else if (prop.equalsIgnoreCase("warning"))
+                Options.verbose = Py.WARNING;
+            else if (prop.equalsIgnoreCase("message"))
+                Options.verbose = Py.MESSAGE;
+            else if (prop.equalsIgnoreCase("comment"))
+                Options.verbose = Py.COMMENT;
+            else if (prop.equalsIgnoreCase("debug"))
+                Options.verbose = Py.DEBUG;
+            else
+                throw Py.ValueError("Illegal verbose option setting: '"+
+                                    prop+"'");
+        }
+        // additional initializations which must happen after the registry
+        // is guaranteed to be initialized.
+        JavaAccessibility.initialize();
+    }
 }
