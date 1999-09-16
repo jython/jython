2b63b392e0426740e313becab91a86ff45b342d0
Barry Warsaw <barry@python.org>
1999-09-16 22:21:07 +0000

    intermediate

--- Lib/javaos.py
+++ Lib/javaos.py
@@ -5,7 +5,7 @@ path = javapath
 
 error = 'os.error'
 
-name = 'jdk1.1'
+name = 'java' # descriminate based on JDK version?
 curdir = '.'
 pardir = '..' #This might not be right...
 #curdir, pardir??
@@ -18,46 +18,46 @@ environ = {}
 
 
 def _exit(n=0):
-	java.lang.System.exit(n)
+    java.lang.System.exit(n)
 
 def getcwd():
-	foo = File(File("foo").getAbsolutePath())
-	return foo.getParent()
+    foo = File(File("foo").getAbsolutePath())
+    return foo.getParent()
 
 def listdir(path):
-	l = File(path).list()
-	if l is None:
-		raise error, 'No such directory'
-	return list(l)
+    l = File(path).list()
+    if l is None:
+	raise error, 'No such directory'
+    return list(l)
 
 def mkdir(path):
-	if not File(path).mkdir():
-		raise error, "couldn't make directory"
-		
+    if not File(path).mkdir():
+	raise error, "couldn't make directory"
+
 def makedirs(path):
-	if not File(path).mkdirs():
-		raise error, "couldn't make directories"
+    if not File(path).mkdirs():
+	raise error, "couldn't make directories"
 
 def remove(path):
-	if not File(path).delete():
-		raise error, "couldn't delete file"
+    if not File(path).delete():
+	raise error, "couldn't delete file"
 
 def rename(path, newpath):
-	if not File(path).renameTo(File(newpath)):
-	    raise error, "couldn't rename file"
+    if not File(path).renameTo(File(newpath)):
+	raise error, "couldn't rename file"
 
 def rmdir(path):
-	if not File(path).delete():
-		raise error, "couldn't delete directory"
+    if not File(path).delete():
+	raise error, "couldn't delete directory"
 
 unlink = remove
 
 def stat(path):
-	"""The Java stat implementation only returns a small subset of
-	the standard fields"""
+    """The Java stat implementation only returns a small subset of
+    the standard fields"""
 
-	file = java.io.File(path)
-	mtime = file.lastModified()
-	size = file.length()
+    file = java.io.File(path)
+    mtime = file.lastModified()
+    size = file.length()
 
-	return (0,0,0,0,0,0, size, mtime, mtime, 0)
+    return (0,0,0,0,0,0, size, mtime, mtime, 0)
