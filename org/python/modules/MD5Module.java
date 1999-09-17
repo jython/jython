e1c4335403bafcecb5c5a5fb33b80e0b99544c16
Barry Warsaw <barry@python.org>
1999-09-17 21:18:25 +0000

    Document how to enable the md5 module.

--- org/python/modules/MD5Module.java
+++ org/python/modules/MD5Module.java
@@ -1,4 +1,29 @@
 // Copyright © Corporation for National Research Initiatives
+
+// This is a JPython module wrapper around Harry Mantakos' md.java class,
+// which provides the basic MD5 algorithm.  See also MD5Object.java which
+// is the implementation of the md5 object returned by new().
+
+// In order to use this, do the following:
+//
+// Download Harry's source code from
+// <http://www.cs.umd.edu/~harry/jotp/src.html>.  Hopefully, I'll
+// eventually be able to redistribute this by default with JPython.
+//
+// Drop md.java into org/python/modules (or otherwise put it on your
+// CLASSPATH) and edit the top of the file to include
+// "package org.python.modules;"
+//
+// Compile md.java
+//
+// That's it.  As long as JPython can find the class org.python.modules.md5 
+// you can, from JPython do something like the following:
+//
+// >>> import md5
+// >>> n = md5.new("hello")
+// >>> n.digest()
+// ']A@*\274K*v\271q\235\221\020\027\305\222'
+
 package org.python.modules;
 
 import org.python.core.*;
