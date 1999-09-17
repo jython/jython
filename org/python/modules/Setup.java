67fb2a0782515ee8e14f9d55362139cc89cd8422
Barry Warsaw <barry@python.org>
1999-09-17 21:10:20 +0000

    Document the new semantics

--- org/python/modules/Setup.java
+++ org/python/modules/Setup.java
@@ -6,11 +6,19 @@ package org.python.modules;
 
 public class Setup 
 {
-    // Each element of this array is a string naming the builtin module.
-    // The string can be of the form name:module in which case the class
-    // name of the builtin module is module.name.  The string can also be
-    // of the form name:null in which case name is removed from the list of 
-    // builtin modules.
+    // Each element of this array is a string naming a builtin module to
+    // add to the system.  The string has the following allowable forms:
+    //
+    // name
+    //     The module name is `name' and the class name is
+    //     org.python.modules.name
+    //
+    // name:class
+    //     The module name is `name' and the class name is `class' where
+    //     class must be a fully qualified Java class name
+    //
+    // name:null
+    //     The module `name' is removed from the list of builtin modules
     //
     // That isn't very useful here, but you can add additional builtin
     // modules by editing the JPython registry file.  See the property
