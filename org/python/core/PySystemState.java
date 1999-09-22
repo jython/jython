12ac87821bd1e25a7b1496e8ad11d1320684bd8d
Barry Warsaw <barry@python.org>
1999-09-22 17:44:30 +0000

    Bump version number to 1.1beta3

--- org/python/core/PySystemState.java
+++ org/python/core/PySystemState.java
@@ -13,7 +13,7 @@ public class PySystemState extends PyObject
     /**
      * The current version of JPython.
      */
-    public static String version = "1.1beta2+";
+    public static String version = "1.1beta3";
 
     /**
      * The copyright notice for this release.
