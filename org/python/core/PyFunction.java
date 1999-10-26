97e399705585e68364c420a8151d226db8a303ac
Barry Warsaw <barry@python.org>
1999-10-26 23:02:57 +0000

    Fixes for PR#192, specifically:
    __members__, __dir__(): Return list of members, specifically __doc__,
    func_doc, __name__, func_name, func_globals, func_defaults, func_code.
    
    ___setattr__(), __delattr__(): throw TypeError if trying to modify a
    readonly attribute, and AttributeError if modifying a non-existant
    attribute.  In __setattr__(), func_doc and __doc__ are writable.
    NOTE: In CPython, func_defaults and func_code are writable, but not in
    JPython.
    
    Also, get rid of __class__ and super(__class__) call in ctor.  That
    was the old way of initializing this ivar.

--- org/python/core/PyFunction.java
+++ org/python/core/PyFunction.java
@@ -1,19 +1,17 @@
 // Copyright © Corporation for National Research Initiatives
 package org.python.core;
 
-public class PyFunction extends PyObject {
+public class PyFunction extends PyObject
+{
     public String __name__;
     public PyObject __doc__;
     public PyObject func_globals;
     public PyObject[] func_defaults;
     public PyCode func_code;
 
-    public static PyClass __class__;
-
     public PyFunction(PyObject globals, PyObject[] defaults, PyCode code,
                       PyObject doc)
     {
-        super(__class__);
         func_globals = globals;
         __name__ = code.co_name;
         if (doc == null)
@@ -28,6 +26,57 @@ public class PyFunction extends PyObject {
         this(globals, defaults, code, null);
     }
         
+    private static final String[] __members__ = {
+        "__doc__", "func_doc",
+        "__name__", "func_name",
+        "func_globals", "func_defaults", "func_code"
+    };
+
+    public PyObject __dir__() {
+        PyString members[] = new PyString[__members__.length];
+        for (int i = 0; i < __members__.length; i++)
+            members[i] = new PyString(__members__[i]);
+        return new PyList(members);
+    }
+
+    private void throwReadonly(String name) {
+        for (int i = 0; i < __members__.length; i++)
+            if (__members__[i] == name)
+                throw Py.TypeError("readonly attribute");
+        throw Py.AttributeError(name);
+    }
+
+    public void __setattr__(String name, PyObject value) {
+        // TBD: in CPython, func_code, func_defaults, func_doc, __doc__ are
+        // writable.  For now, only func_doc, __doc__ are writable in
+        // JPython.
+        if (name == "func_doc" || name == "__doc__")
+            __doc__ = value;
+        // not yet implemented:
+        // func_code
+        // func_defaults
+        else throwReadonly(name);
+    }
+
+    public void __delattr__(String name) {
+        // TBD: should __doc__ be del'able?
+        throwReadonly(name);
+    }
+
+    public PyObject __findattr__(String name) {
+        // these are special, everything else is findable by reflection
+        if (name == "func_doc")
+            return __doc__;
+        if (name == "func_name")
+            return new PyString(__name__);
+        if (name == "func_defaults") {
+            if (func_defaults.length == 0)
+                return Py.None;
+            return new PyTuple(func_defaults);
+        }
+        return super.__findattr__(name);
+    }
+    
     public PyObject _doget(PyObject container) {
         return _doget(container, null);
     }
