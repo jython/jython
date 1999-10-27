adaac82732d4febab40ac650bf7ff099b6813aec
Barry Warsaw <barry@python.org>
1999-10-27 20:11:26 +0000

    A fix for PR#201.  This patch may have unintended consequences so it is currently controlled by the registry option python.options.extendedClassLoader.  When this option is true (default), the following semantics are in effect.  When this option is false, the old semantics are in effect.  This option will be removed before 1.0 final.
    Specifically,
    
    loadClass(): Insert a new class search algorithm in between the shot
    the Py state class loader has, and the shot the Java system class
    loader has.  This new algorithm searches sys.path for the named .class
    file and loads it if found.
    
    This fixes PR#201 by allowing the class loader (i.e. our current
    BytecodeLoader) that is loading an outer class have a shot at finding
    and loading the anonymous inner class.  Without this patch, a variety
    of errors can occur, depending on the state of your Java CLASSPATH;
    either a NoClassDefFoundError or IllegalAccessException will get
    thrown when trying to load the anonymous inner class.
    
    open(): helper method
    
    loadClass(String, byte[]) => loadClassFromBytes(String, byte[])

--- org/python/core/BytecodeLoader.java
+++ org/python/core/BytecodeLoader.java
@@ -1,20 +1,70 @@
 // Copyright © Corporation for National Research Initiatives
 
 package org.python.core;
-import java.util.Hashtable;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class BytecodeLoader extends ClassLoader
 {
-    protected Class loadClass(String s, boolean b)
+    // override from abstract base class
+    protected Class loadClass(String name, boolean resolve)
         throws ClassNotFoundException
     {
+//         System.err.println("loadClass("+name+", "+resolve+")");
+        // First, if the Python runtime system has a default class loader,
+        // defer to it.
         ClassLoader classLoader = Py.getSystemState().getClassLoader();
         if (classLoader != null)
-            return classLoader.loadClass(s);
-        return findSystemClass(s);
+            return classLoader.loadClass(name);
+        // Search the sys.path for a .class file matching the named class.
+        // TBD: This registry option is temporary.
+        if (Options.extendedClassLoader) {
+            PyList path = Py.getSystemState().path;
+            for (int i=0; i < path.__len__(); i++) {
+                String dir = path.get(i).__str__().toString();
+                FileInputStream fis = open(dir, name);
+                if (fis == null)
+                    continue;
+                try {
+                    int size = fis.available();
+                    byte[] buffer = new byte[size];
+                    fis.read(buffer);
+                    return loadClassFromBytes(name, buffer);
+                }
+                catch (IOException e) {
+                    continue;
+                }
+            }
+        }
+        // couldn't find the .class file on sys.path, so give the system
+        // class loader the final shot at it
+        return findSystemClass(name);
     }
 
-    public Class loadClass(String name, byte[] data) {
+    private FileInputStream open(String dir, String name) {
+        String accum = "";
+        boolean first = true;
+        for (StringTokenizer t = new StringTokenizer(name, ".");
+             t.hasMoreTokens();)
+        {
+            String token = t.nextToken();
+            if (!first)
+                accum += File.separator;
+            accum += token;
+            first = false;
+        }
+        try {
+            if (dir == "")
+                dir = ".";
+            return new FileInputStream(new File(dir, accum+".class"));
+        }
+        catch (FileNotFoundException e) {
+            return null;
+        }
+    }
+
+    private Class loadClassFromBytes(String name, byte[] data) {
+//         System.err.println("loadClassFromBytes("+name+", byte[])");
         Class c = defineClass(name, data, 0, data.length);
         resolveClass(c);
         // This method has caused much trouble.  Using it breaks jdk1.2rc1
@@ -22,7 +72,7 @@ public class BytecodeLoader extends ClassLoader
         // Don't use by default, but allow python.options.compileClass to
         // override
         if (!Options.skipCompile) {
-            //System.err.println("compile: "+name);
+//             System.err.println("compile: "+name);
             Compiler.compileClass(c);
         }
         return c;
@@ -31,12 +81,12 @@ public class BytecodeLoader extends ClassLoader
     public PyCode loadBytes(String name, byte[] data)
         throws InstantiationException, IllegalAccessException
     {
-        Class c = loadClass(name, data);
+        Class c = loadClassFromBytes(name, data);
         return ((PyRunnable)c.newInstance()).getMain();
     }
 
     public Class makeClass(String name, byte[] data) {
-        return loadClass(name, data);
+        return loadClassFromBytes(name, data);
     }
 
     public static PyCode makeCode(String name, byte[] data)
