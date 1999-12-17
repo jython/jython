448649b1ab5a1cd2b477aed215a9ed8231f9a7a2
Barry Warsaw <barry@python.org>
1999-12-17 19:48:32 +0000

    Bumping version to beta5

--- org/python/core/PySystemState.java
+++ org/python/core/PySystemState.java
@@ -13,7 +13,7 @@ public class PySystemState extends PyObject
     /**
      * The current version of JPython.
      */
-    public static String version = "1.1beta4";
+    public static String version = "1.1beta5";
 
     /**
      * The copyright notice for this release.
