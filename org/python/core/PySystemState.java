e24c8067d437a37253c96d2a550ec9970794013a
Barry Warsaw <barry@python.org>
2000-01-22 00:52:51 +0000

    Bump version to 1.1rc1

--- org/python/core/PySystemState.java
+++ org/python/core/PySystemState.java
@@ -13,7 +13,7 @@ public class PySystemState extends PyObject
     /**
      * The current version of JPython.
      */
-    public static String version = "1.1beta5";
+    public static String version = "1.1rc1";
 
     /**
      * The copyright notice for this release.
