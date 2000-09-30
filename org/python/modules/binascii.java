27cf9a6384a4f76a8c3b853055d8b44d6b8c0b8d
Finn Bock <bckfnn@worldonline.dk>
2000-09-30 10:06:28 +0000

    Added docstrings and improved CPython20 compatibility.

--- org/python/modules/binascii.java
+++ org/python/modules/binascii.java
@@ -128,9 +128,13 @@ import org.python.core.*;
 
  */
 public class binascii {
+
+    public static String __doc__ = "Conversion between binary data and ASCII";
+
     public static final PyString Error = new PyString("binascii.Error");
 
-    public static final PyString Incomplete = new PyString("binascii.Incomplete");
+    public static final PyString Incomplete = 
+                                     new PyString("binascii.Incomplete");
 
 
     // hqx lookup table, ascii->binary.
@@ -192,7 +196,8 @@ public class binascii {
     };
 
     private static byte[] table_b2a_hqx =
-	"!\"#$%&'()*+,-012345689@ABCDEFGHIJKLMNPQRSTUVXYZ[`abcdefhijklmpqr".getBytes();
+	"!\"#$%&'()*+,-012345689@ABCDEFGHIJKLMNPQRSTUVXYZ[`abcdefhijklmpqr".
+           getBytes();
 
 
 
@@ -209,10 +214,13 @@ public class binascii {
     };
 
     private static char BASE64_PAD = '=';
-    private static int BASE64_MAXBIN = 57; /* Max binary chunk size (76 char line) */
+
+    /* Max binary chunk size (76 char line) */
+    private static int BASE64_MAXBIN = 57; 
 
     private static byte[] table_b2a_base64 =
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".getBytes();
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".
+            getBytes();
 
 
 
@@ -250,6 +258,12 @@ public class binascii {
 	0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
 	0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0,
     };
+
+
+
+    public static PyString __doc__a2b_uu = new PyString(
+        "(ascii) -> bin. Decode a line of uuencoded data"
+    );
 
 
     /**
@@ -311,6 +325,12 @@ public class binascii {
     }
 
 
+
+    public static PyString __doc__b2a_uu = new PyString(
+        "(bin) -> ascii. Uuencode line of data"
+    );
+
+
     /**
      * Convert binary data to a line of ASCII characters, the return value
      * is the converted line, including a newline char. The length of
@@ -353,6 +373,36 @@ public class binascii {
     }
 
 
+    private static int binascii_find_valid(String s, int offset, int num) {
+        int slen = s.length() - offset;
+
+        /* Finds & returns the (num+1)th
+        ** valid character for base64, or -1 if none.
+        */
+
+        int ret = -1;
+
+        while ((slen > 0) && (ret == -1)) {
+            int c = (int)s.charAt(offset);
+            short b64val = table_a2b_base64[c & 0x7f];
+            if (((c <= 0x7f) && (b64val != -1)) ) {
+                if (num == 0)
+                     ret = c;
+                num--;
+            }
+
+            offset++;
+            slen--;
+        }
+        return ret;
+    }
+
+
+
+    public static PyString __doc__a2b_base64 = new PyString(
+         "(ascii) -> bin. Decode a line of base64 data"
+    );
+
     /** 
      * Convert a block of base64 data back to binary and return the
      * binary data. More than one line may be passed at a time.
@@ -361,7 +411,7 @@ public class binascii {
 	int leftbits = 0;
 	char this_ch;
 	int leftchar = 0;
-	int npad = 0;
+        int quad_pos = 0;
 
 	int ascii_len = ascii_data.length();
 
@@ -370,33 +420,50 @@ public class binascii {
 
 	for(int i = 0; ascii_len > 0 ; ascii_len--, i++) {
 	    // Skip some punctuation 
-	    this_ch = (char)(ascii_data.charAt(i) & 0x7f);
-	    if (this_ch == '\r' || this_ch == '\n' || this_ch == ' ')
+	    this_ch = ascii_data.charAt(i);
+	    if ((int) this_ch > 0x7F || this_ch == '\r' || this_ch == '\n' || this_ch == ' ')
 		continue;
 		
-	    if (this_ch == BASE64_PAD)
-		npad++;
-	    this_ch = (char)table_a2b_base64[(ascii_data.charAt(i)) & 0x7f];
-	    if (this_ch == -1) continue;
+	    if (this_ch == BASE64_PAD) {
+                if (quad_pos < 2 || (quad_pos == 2 && binascii_find_valid(ascii_data, i, 1) != BASE64_PAD))
+                    continue;
+                else {
+                    // A pad sequence means no more input.
+                    // We've already interpreted the data
+                    // from the quad at this point.
+                    leftbits = 0;
+                    break;
+                }
+            }
+
+	    short this_v = table_a2b_base64[(int) this_ch];
+	    if (this_v == -1) 
+                continue;
+
 	    // Shift it in on the low end, and see if there's
 	    // a byte ready for output.
-	    leftchar = (leftchar << 6) | (this_ch);
+            quad_pos = (quad_pos + 1) & 0x03;
+	    leftchar = (leftchar << 6) | (this_v);
 	    leftbits += 6;
 	    if (leftbits >= 8) {
 		leftbits -= 8;
 		bin_data.append((char)((leftchar >> leftbits) & 0xff));
-		leftchar &= ((1 << leftbits) - 1);
 		bin_len++;
+		leftchar &= ((1 << leftbits) - 1);
 	    }
 	}
 	// Check that no bits are left
 	if (leftbits != 0) {
 	    throw new PyException(Error, "Incorrect padding");
 	}
-	// and remove any padding
-	bin_len -= npad;
 	return bin_data.toString();
     }
+
+
+
+    public static PyString __doc__b2a_base64 = new PyString(
+        "(bin) -> ascii. Base64-code line of data"
+    );
 
 
     /**
@@ -442,6 +509,10 @@ public class binascii {
 	return ascii_data.toString();
     }
 
+
+    public static PyString __doc__a2b_hqx = new PyString(
+         "ascii -> bin, done. Decode .hqx coding"
+    );
 
     /**
      * Convert binhex4 formatted ASCII data to binary, without doing
@@ -493,6 +564,10 @@ public class binascii {
     }
 
 
+    public static PyString __doc__rlecode_hqx = new PyString(
+         "Binhex RLE-code binary data"
+    );
+
     /**
      * Perform binhex4 style RLE-compression on <i>data</i> and return the
      * result.
@@ -531,6 +606,10 @@ public class binascii {
     }
 
 
+    public static PyString __doc__b2a_hqx = new PyString( 
+         "Encode .hqx data"
+    );
+
     /**
      * Perform hexbin4 binary-to-ASCII translation and return the
      * resulting string. The argument should already be RLE-coded, and have a
@@ -562,6 +641,12 @@ public class binascii {
 	}
 	return ascii_data.toString();
     }
+
+
+
+    public static PyString __doc__rledecode_hqx = new PyString(
+        "Decode hexbin RLE-coded string"
+    );
 
 
     /**
@@ -629,6 +714,12 @@ public class binascii {
     }
 
 
+
+    public static PyString __doc__crc_hqx = new PyString(
+        "(data, oldcrc) -> newcrc. Compute hqx CRC incrementally"
+    );
+
+
     /**
      * Compute the binhex4 crc value of <i>data</i>, starting with an initial
      * <i>crc</i> and returning the result.
@@ -643,6 +734,151 @@ public class binascii {
 
 	return crc;
     }
+
+
+
+
+static long[] crc_32_tab = new long[] {
+0x00000000L, 0x77073096L, 0xee0e612cL, 0x990951baL, 0x076dc419L,
+0x706af48fL, 0xe963a535L, 0x9e6495a3L, 0x0edb8832L, 0x79dcb8a4L,
+0xe0d5e91eL, 0x97d2d988L, 0x09b64c2bL, 0x7eb17cbdL, 0xe7b82d07L,
+0x90bf1d91L, 0x1db71064L, 0x6ab020f2L, 0xf3b97148L, 0x84be41deL,
+0x1adad47dL, 0x6ddde4ebL, 0xf4d4b551L, 0x83d385c7L, 0x136c9856L,
+0x646ba8c0L, 0xfd62f97aL, 0x8a65c9ecL, 0x14015c4fL, 0x63066cd9L,
+0xfa0f3d63L, 0x8d080df5L, 0x3b6e20c8L, 0x4c69105eL, 0xd56041e4L,
+0xa2677172L, 0x3c03e4d1L, 0x4b04d447L, 0xd20d85fdL, 0xa50ab56bL,
+0x35b5a8faL, 0x42b2986cL, 0xdbbbc9d6L, 0xacbcf940L, 0x32d86ce3L,
+0x45df5c75L, 0xdcd60dcfL, 0xabd13d59L, 0x26d930acL, 0x51de003aL,
+0xc8d75180L, 0xbfd06116L, 0x21b4f4b5L, 0x56b3c423L, 0xcfba9599L,
+0xb8bda50fL, 0x2802b89eL, 0x5f058808L, 0xc60cd9b2L, 0xb10be924L,
+0x2f6f7c87L, 0x58684c11L, 0xc1611dabL, 0xb6662d3dL, 0x76dc4190L,
+0x01db7106L, 0x98d220bcL, 0xefd5102aL, 0x71b18589L, 0x06b6b51fL,
+0x9fbfe4a5L, 0xe8b8d433L, 0x7807c9a2L, 0x0f00f934L, 0x9609a88eL,
+0xe10e9818L, 0x7f6a0dbbL, 0x086d3d2dL, 0x91646c97L, 0xe6635c01L,
+0x6b6b51f4L, 0x1c6c6162L, 0x856530d8L, 0xf262004eL, 0x6c0695edL,
+0x1b01a57bL, 0x8208f4c1L, 0xf50fc457L, 0x65b0d9c6L, 0x12b7e950L,
+0x8bbeb8eaL, 0xfcb9887cL, 0x62dd1ddfL, 0x15da2d49L, 0x8cd37cf3L,
+0xfbd44c65L, 0x4db26158L, 0x3ab551ceL, 0xa3bc0074L, 0xd4bb30e2L,
+0x4adfa541L, 0x3dd895d7L, 0xa4d1c46dL, 0xd3d6f4fbL, 0x4369e96aL,
+0x346ed9fcL, 0xad678846L, 0xda60b8d0L, 0x44042d73L, 0x33031de5L,
+0xaa0a4c5fL, 0xdd0d7cc9L, 0x5005713cL, 0x270241aaL, 0xbe0b1010L,
+0xc90c2086L, 0x5768b525L, 0x206f85b3L, 0xb966d409L, 0xce61e49fL,
+0x5edef90eL, 0x29d9c998L, 0xb0d09822L, 0xc7d7a8b4L, 0x59b33d17L,
+0x2eb40d81L, 0xb7bd5c3bL, 0xc0ba6cadL, 0xedb88320L, 0x9abfb3b6L,
+0x03b6e20cL, 0x74b1d29aL, 0xead54739L, 0x9dd277afL, 0x04db2615L,
+0x73dc1683L, 0xe3630b12L, 0x94643b84L, 0x0d6d6a3eL, 0x7a6a5aa8L,
+0xe40ecf0bL, 0x9309ff9dL, 0x0a00ae27L, 0x7d079eb1L, 0xf00f9344L,
+0x8708a3d2L, 0x1e01f268L, 0x6906c2feL, 0xf762575dL, 0x806567cbL,
+0x196c3671L, 0x6e6b06e7L, 0xfed41b76L, 0x89d32be0L, 0x10da7a5aL,
+0x67dd4accL, 0xf9b9df6fL, 0x8ebeeff9L, 0x17b7be43L, 0x60b08ed5L,
+0xd6d6a3e8L, 0xa1d1937eL, 0x38d8c2c4L, 0x4fdff252L, 0xd1bb67f1L,
+0xa6bc5767L, 0x3fb506ddL, 0x48b2364bL, 0xd80d2bdaL, 0xaf0a1b4cL,
+0x36034af6L, 0x41047a60L, 0xdf60efc3L, 0xa867df55L, 0x316e8eefL,
+0x4669be79L, 0xcb61b38cL, 0xbc66831aL, 0x256fd2a0L, 0x5268e236L,
+0xcc0c7795L, 0xbb0b4703L, 0x220216b9L, 0x5505262fL, 0xc5ba3bbeL,
+0xb2bd0b28L, 0x2bb45a92L, 0x5cb36a04L, 0xc2d7ffa7L, 0xb5d0cf31L,
+0x2cd99e8bL, 0x5bdeae1dL, 0x9b64c2b0L, 0xec63f226L, 0x756aa39cL,
+0x026d930aL, 0x9c0906a9L, 0xeb0e363fL, 0x72076785L, 0x05005713L,
+0x95bf4a82L, 0xe2b87a14L, 0x7bb12baeL, 0x0cb61b38L, 0x92d28e9bL,
+0xe5d5be0dL, 0x7cdcefb7L, 0x0bdbdf21L, 0x86d3d2d4L, 0xf1d4e242L,
+0x68ddb3f8L, 0x1fda836eL, 0x81be16cdL, 0xf6b9265bL, 0x6fb077e1L,
+0x18b74777L, 0x88085ae6L, 0xff0f6a70L, 0x66063bcaL, 0x11010b5cL,
+0x8f659effL, 0xf862ae69L, 0x616bffd3L, 0x166ccf45L, 0xa00ae278L,
+0xd70dd2eeL, 0x4e048354L, 0x3903b3c2L, 0xa7672661L, 0xd06016f7L,
+0x4969474dL, 0x3e6e77dbL, 0xaed16a4aL, 0xd9d65adcL, 0x40df0b66L,
+0x37d83bf0L, 0xa9bcae53L, 0xdebb9ec5L, 0x47b2cf7fL, 0x30b5ffe9L,
+0xbdbdf21cL, 0xcabac28aL, 0x53b39330L, 0x24b4a3a6L, 0xbad03605L,
+0xcdd70693L, 0x54de5729L, 0x23d967bfL, 0xb3667a2eL, 0xc4614ab8L,
+0x5d681b02L, 0x2a6f2b94L, 0xb40bbe37L, 0xc30c8ea1L, 0x5a05df1bL,
+0x2d02ef8dL
+};
+
+    public static int crc32(String bin_data) {
+        return crc32(bin_data, 0);
+    }
+
+    public static int crc32(String bin_data, long crc) {
+	int len = bin_data.length();
+
+	crc = crc ^ 0xFFFFFFFFL;
+	for (int i = 0; i < len; i++) {
+            char ch = bin_data.charAt(i);
+            crc = (int)crc_32_tab[(int) ((crc ^ ch) & 0xffL)] ^ (crc >> 8);
+            /* Note:  (crc >> 8) MUST zero fill on left */
+            crc &= 0xFFFFFFFFL;
+        }
+        if (crc >= 0x80000000)
+            return -(int)(crc+1 & 0xFFFFFFFF);
+        else
+            return (int)(crc & 0xFFFFFFFF);
+    }
+
+
+    private static char[] hexdigit = "0123456789abcdef".toCharArray();
+
+
+    public static PyString __doc__b2a_hex = new PyString(
+        "b2a_hex(data) -> s; Hexadecimal representation of binary data.\n" + 
+        "\n" + 
+        "This function is also available as \"hexlify()\"."
+    );
+
+    public static String b2a_hex(String argbuf) {
+        int arglen = argbuf.length();
+
+        StringBuffer retbuf = new StringBuffer(arglen*2);
+
+        /* make hex version of string, taken from shamodule.c */
+	for (int i = 0; i < arglen; i++) {
+	    char ch = argbuf.charAt(i);
+            retbuf.append(hexdigit[(ch >>> 4) & 0xF]);
+            retbuf.append(hexdigit[ch & 0xF]);
+        }
+	return retbuf.toString();
+
+    }
+
+
+    public static String hexlify(String argbuf) {
+        return b2a_hex(argbuf);
+    }
+
+
+    public static PyString a2b_hex$doc = new PyString(
+        "a2b_hex(hexstr) -> s; Binary data of hexadecimal representation.\n" +
+        "\n" +
+        "hexstr must contain an even number of hex digits (upper or lower case).\n"+
+        "This function is also available as \"unhexlify()\""
+    );
+
+
+    public static String a2b_hex(String argbuf) {
+	int arglen = argbuf.length();
+
+        /* XXX What should we do about strings with an odd length?  Should
+         * we add an implicit leading zero, or a trailing zero?  For now,
+         * raise an exception.
+         */
+        if (arglen % 2 != 0)
+            throw Py.TypeError("Odd-length string");
+
+        StringBuffer retbuf = new StringBuffer(arglen/2);
+
+        for (int i = 0; i < arglen; i += 2) {
+            int top = Character.digit(argbuf.charAt(i), 16);
+            int bot = Character.digit(argbuf.charAt(i+1), 16);
+            if (top == -1 || bot == -1)
+                throw Py.TypeError("Non-hexadecimal digit found");
+            retbuf.append((char) ((top << 4) + bot));
+	}
+	return retbuf.toString();
+    }
+
+   
+    public static String unhexlify(String argbuf) {
+        return a2b_hex(argbuf);
+    }
+
 
 
 /*
