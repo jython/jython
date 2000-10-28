9982073bde04670fe36d1e19c29b04b379b18cd8
Finn Bock <bckfnn@worldonline.dk>
2000-10-28 11:55:10 +0000

    Use the ArgParser from org.python.core.*.

--- org/python/modules/sha.java
+++ org/python/modules/sha.java
@@ -42,8 +42,8 @@ public class sha {
 
 
     public static SHA1 new$(PyObject[] args, String[] kws) {
-        ArgParser ap = new ArgParser(args, kws, "string");
-        String cp = ap.getString(0);
+        ArgParser ap = new ArgParser("sha", args, kws, "string");
+        String cp = ap.getString(0, null);
 
         SHA1 n = new SHA1();
         if (cp != null)
@@ -56,89 +56,3 @@ public class sha {
         return new$(args, kws);
     }
 }
-
-
-
-// This is the moral equivalent of PyArg_ParseTupleAndKeywords. If it
-// turns out to work as I hope, it will become a utility class in core.
-// In any case, it will soon be removed from this source.
-
-class ArgParser {
-    PyObject[] args;
-    String[] kws;
-    String[] params = null;
-
-    public ArgParser(PyObject[] args, String[] kws) {
-        this.args = args;
-        this.kws = kws;
-    }
-
-    public ArgParser(PyObject[] args, String[] kws, String p0) {
-        this(args, kws);
-        this.params = new String[] { p0 };
-        check();
-    }
-
-    public ArgParser(PyObject[] args, String[] kws, String p0, String p1) {
-        this(args, kws);
-        this.params = new String[] { p0, p1 };
-        check();
-    }
-
-    public ArgParser(PyObject[] args, String[] kws, String p0, String p1, String p2) {
-        this(args, kws);
-        this.params = new String[] { p0, p1, p2 };
-        check();
-    }
-
-    public ArgParser(PyObject[] args, String[] kws, String[] paramnames) {
-        this(args, kws);
-        this.params = paramnames;
-        check();
-    }
-
-
-    public String getString(int pos) {
-         return (String) getArg(pos, String.class, "string");
-    }
-
-    public int getInt(int pos) {
-         return ((Integer) getArg(pos, Integer.class, "int")).intValue();
-    }
-
-
-    private void check() {
-        l1:
-        for (int i = 0; i < kws.length; i++) {
-            for (int j = 0; j < params.length; j++) {
-                if (kws[i].equals(params[j]))
-                    continue l1;
-            }
-            throw Py.TypeError(
-                  kws[i] +  " is an invalid keyword argument for this function");
-        }
-    }
-
-    private PyObject getArg(int pos) {
-        int kws_start = args.length - kws.length;
-        if (pos < kws_start)
-            return args[pos];
-        for (int i = 0; i < kws.length; i++) {
-            if (kws[i].equals(params[pos]))
-                return args[kws_start + i];
-        }
-        return null;
-    }
-
-    private Object getArg(int pos, Class clss, String classname) {
-        PyObject pyret = getArg(pos);
-        if (pyret == null)
-            return null;
-        Object ret = pyret.__tojava__(clss);
-        if (ret == Py.NoConversion) 
-            throw Py.TypeError("argument " + (pos+1) + ": expected " +
-                               classname + ", " + Py.safeRepr(pyret) + " found");
-        return ret;
-    }
-}
-            
