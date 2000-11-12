0c3ceccbe8ca155f6e3eb4fe29cc40832e468b31
Finn Bock <bckfnn@worldonline.dk>
2000-11-12 22:22:06 +0000

    Updated to Jython.

--- Doc/links.h
+++ Doc/links.h
@@ -2,21 +2,21 @@
 <h3>Documentation</h3>
 <li><a href="./index.html">Overview</a>
 <li><a href="whatis.html">Executive Summary</a>
-<li><a href="interpreter.html">Invoking JPython</a>
-<li><a href="registry.html">JPython Registry</a>
+<li><a href="interpreter.html">Invoking Jython</a>
+<li><a href="registry.html">Jython Registry</a>
 <li><a href="embedding.html">Embedding</a>
-<li><a href="compile.html">Compiling JPython from source</a>
+<li><a href="compile.html">Compiling Jython from source</a>
 <h3>Working with Java</h3>
 <li><a href="usejava.html">The Basics</a>
 <li><a href="properties.html">JavaBean Properties</a>
 <li><a href="jarray.html">Java arrays</a>
 <li><a href="subclassing.html">Subclassing</a>
-<li><a href="jpythonc.html">Building applets, servlets, beans...</a>
+<li><a href="jythonc.html">Building applets, servlets, beans...</a>
 <h3>Python Docs (exits)</h3>
 <li><a href="http://www.python.org/doc/tut/tut.html">Python Tutorial</a>
 <li><a href="http://www.python.org/doc/lib/lib.html">Library Reference</a>
 <h3>Other</h3>
-<li><a href="differences.html">JPython vs. CPython</a>
-<li><a href="faq.html">JPython FAQ</a>
-<li><a href="http://www.python.org/pipermail/jpython-interest/">List Archives</a> (exit)
+<li><a href="differences.html">Jython vs. CPython</a>
+<li><a href="faq.html">Jython FAQ</a>
+<li><a href="http://www.geocrawler.com/archives/3/7017/2000/">List Archives</a> (exit)
 <LI><A HREF="http://www.python.org/workshops/1997-10/proceedings/hugunin.html">JPython paper</A> (exit)
