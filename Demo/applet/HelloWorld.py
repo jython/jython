adba3faa9b50330506d71d83395261e18ca5ace8
Finn Bock <bckfnn@worldonline.dk>
2000-11-12 22:09:10 +0000

    Changed text to Jython.

--- Demo/applet/HelloWorld.py
+++ Demo/applet/HelloWorld.py
@@ -2,7 +2,7 @@ from java.applet import Applet
 
 class HelloWorld(Applet):
     def paint(self, g):
-	g.drawString("Hello from JPython!", 20, 30)
+	g.drawString("Hello from Jython!", 20, 30)
 
 
 if __name__ == '__main__':
