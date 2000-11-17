e522444d80b840c4b067c9fae7af93539ceddaa9
Samuele Pedronis <pedronis@openend.se>
2000-11-17 12:41:09 +0000

    Loading logic fixes:   additions and deletions. For issues involved see the jython-dev list archives.

---
+++ org/python/core/PathPackageManager.java
@@ -0,0 +1,153 @@
+
+package org.python.core;
+
+import java.io.*;
+import java.lang.reflect.Modifier;
+
+/** Path package manager. Gathering classes info dynamically from a set of directories
+ * in path {@link #searchPath}, and statically from a set of jars, like {@link CachedJarsPackageManager}.
+ */
+public abstract class PathPackageManager extends CachedJarsPackageManager {
+
+    public PyList searchPath;
+
+    public PathPackageManager() {
+        searchPath = new PyList();
+    }
+
+    /** Helper for {@link #packageExists(java.lang.String,java.lang.String)}.
+     * Scans for package pkg.name the directories in path.
+     *
+     */
+    protected boolean packageExists(PyList path,String pkg,String name) {
+        String child = pkg.replace('.',File.separatorChar) + File.separator + name;
+
+        for (int i=0; i < path.__len__(); i++) {
+            String dir = path.get(i).__str__().toString();
+            if(dir.length() == 0) dir = null;
+
+            if (new File(dir,child).isDirectory()) return true;
+        }
+        return false;
+    }
+
+    /** Helper for {@link #doDir(PyJavaPackage,boolean,boolean)}.
+     * Scans for package jpkg content over the directories in path.
+     * Add to ret the founded classes/pkgs.
+     * Filter out classes using {@link #filterByName},{@link #filterByAccess}.
+     */
+    protected void doDir(PyList path,PyList ret, PyJavaPackage jpkg,boolean instantiate,boolean exclpkgs) {
+        String child=jpkg.__name__.replace('.',File.separatorChar);
+
+        for (int i=0; i < path.__len__(); i++) {
+            String dir = path.get(i).__str__().toString();
+            if(dir.length() == 0) dir = null;
+
+            File childFile = new File(dir,child);
+
+            String[] list=childFile.list();
+            if(list == null) continue;
+
+            doList:
+            for (int j=0; j < list.length; j++) {
+                String jname = list[j];
+
+                File cand = new File(childFile, jname);
+
+                int jlen = jname.length();
+
+                boolean pkgCand=false;
+
+                if (cand.isDirectory()) {
+                    if (!instantiate && exclpkgs) continue;
+                    pkgCand = true;
+                } else {
+                    if(!jname.endsWith(".class")) continue;
+                    jlen-=6;
+                }
+
+                jname = jname.substring(0,jlen);
+                PyString name = new PyString(jname);
+
+                if (filterByName(jname,pkgCand)) continue;
+
+                // for opt maybe we should some hash-set for ret
+                if (jpkg.__dict__.has_key(name) || jpkg.clsSet.has_key(name) || ret.__contains__(name)) {
+                    continue;
+                }
+
+                if(!Character.isJavaIdentifierStart(jname.charAt(0))) continue;
+                for(int k = 1; k < jlen; k++) {
+                    if(!Character.isJavaIdentifierPart(jname.charAt(k))) continue doList;
+                }
+
+                if(!pkgCand) {
+                    try {
+                        int acc = checkAccess(new BufferedInputStream(new FileInputStream(cand)));
+                        if ((acc == -1) || filterByAccess(jname, acc) ) continue;
+                    } catch(IOException e) { 
+                        continue; 
+                    }
+                }
+
+                if(instantiate) {
+                    if(pkgCand) jpkg.addPackage(jname);
+                    else jpkg.addLazyClass(jname);
+                }
+
+                ret.append(name);
+
+            }
+        }
+
+    }
+
+    /** Add directory dir (if exists) to {@link #searchPath}.
+     */
+    public void addDirectory(File dir) {
+        try {
+            if (dir.getPath().length() == 0) searchPath.append(Py.EmptyString);
+            else searchPath.append(new PyString(dir.getCanonicalPath()));
+        } catch(IOException e) {
+            warning("skipping bad directory, '" +dir+ "'");
+        }
+    }
+
+
+    // ??pending: 
+    // Uses simply split and not a StringTokenizer+trim to adhere to sun jvm parsing of classpath.
+    // E.g. "a;" is parsed by sun jvm as a, ""; the latter is interpreted as cwd.
+    // jview trims and cwd is per default in classpath. The logic here should work
+    // for both(...). Need to distinguish?
+    // This code does not avoid duplicates in searchPath. Should cause no problem (?).
+    /** Adds "classpath" entry. Calls {@link #addDirectory} if path refers to a dir, {@link #addJarToPackages(java.io.File, boolean)}
+      with param cache true if path refers to a jar.
+    */
+    public void addClassPath(String path) {
+        PyList paths = new PyString(path).split(java.io.File.pathSeparator);
+
+        for (int i = 0; i < paths.__len__(); i++) {
+            String entry = paths.get(i).toString();
+            if (entry.endsWith(".jar") || entry.endsWith(".zip")) {
+                addJarToPackages(new File(entry),true);
+            } else {
+                File dir = new File(entry);
+                if (entry.length() == 0 || dir.isDirectory()) addDirectory(dir);
+            }
+        }
+    }            
+
+    public PyList doDir(PyJavaPackage jpkg,boolean instantiate,boolean exclpkgs) {
+        PyList basic = basicDoDir(jpkg,instantiate,exclpkgs);
+        PyList ret = new PyList();
+
+        doDir(searchPath,ret,jpkg,instantiate,exclpkgs);
+
+        return merge(basic,ret);
+    }
+
+    public boolean packageExists(String pkg,String name) {
+        return packageExists(searchPath,pkg,name);
+    }
+
+}
---
+++ org/python/core/SysPackageManager.java
@@ -0,0 +1,108 @@
+
+package org.python.core;
+
+import java.util.Properties;
+import java.util.StringTokenizer;
+import java.io.*;
+
+/** System package manager.
+ * Used by org.python.core.PySystemState.
+ */
+public class SysPackageManager extends PathPackageManager {
+
+protected void message(String msg) { Py.writeMessage("*sys-package-mgr*",msg); }
+protected void warning(String warn) { Py.writeWarning("*sys-package-mgr*",warn); }
+protected void comment(String msg) { Py.writeComment("*sys-package-mgr*",msg); }
+protected void debug(String msg) {Py.writeDebug("*sys-package-mgr*",msg); }
+
+    public SysPackageManager(File cachedir, Properties registry) {
+        if(useCacheDir(cachedir)) {
+            initCache();
+            findAllPackages(registry);
+            saveCache();
+        }
+    }
+
+    private void addJarDir(String jdir) {
+        File file = new File(jdir);
+        if (!file.isDirectory()) return;
+        String[] files = file.list();
+        for(int i=0; i<files.length; i++) {
+            String entry = files[i];
+            if (entry.endsWith(".jar") || entry.endsWith(".zip")) {
+                addJarToPackages(new File(jdir,entry),true);
+            }
+        }
+    }
+
+    private void addJarPath(String path) {
+        StringTokenizer tok =
+        new StringTokenizer(path, java.io.File.pathSeparator);
+        while  (tok.hasMoreTokens())  {
+            // ??pending: do jvms trim? how is interpreted entry=""?
+            String entry = tok.nextToken();            
+            addJarDir(entry);
+        }
+    }
+
+    private void findAllPackages(Properties registry) {
+        String paths = registry.getProperty(
+        "python.packages.paths",
+        "java.class.path,sun.boot.class.path");
+        String directories = registry.getProperty(
+        "python.packages.directories",
+        "java.ext.dirs");
+        String fakepath = registry.getProperty("python.packages.fakepath", null);
+        StringTokenizer tok = new StringTokenizer(paths, ",");
+        while  (tok.hasMoreTokens())  {
+            String entry = tok.nextToken().trim();
+            String tmp = registry.getProperty(entry);
+            if (tmp == null) continue;
+            addClassPath(tmp);
+        }
+
+        tok = new StringTokenizer(directories, ",");
+        while  (tok.hasMoreTokens())  {
+            String entry = tok.nextToken().trim();
+            String tmp = registry.getProperty(entry);
+            if (tmp == null) continue;
+            addJarPath(tmp);
+        }
+
+        if (fakepath != null) addClassPath(fakepath);
+    }
+
+    public Class findClass(String pkg,String name) {
+        Class c;
+        if (pkg.length()>0) c = Py.findClassEx(pkg+'.'+name);
+        else c = Py.findClassEx(name);
+
+        return c;
+    }
+
+    public PyList doDir(PyJavaPackage jpkg,boolean instantiate,boolean exclpkgs) {
+        PyList basic = basicDoDir(jpkg,instantiate,exclpkgs);
+        PyList ret = new PyList();
+
+        doDir(searchPath,ret,jpkg,instantiate,exclpkgs);
+
+        PySystemState system = Py.getSystemState();
+
+        if (system.getClassLoader() == null) doDir(system.path,ret,jpkg,instantiate,exclpkgs);
+
+        return merge(basic,ret);
+    }
+
+    public boolean packageExists(String pkg,String name) {
+        if (packageExists(searchPath,pkg,name)) return true;
+
+        PySystemState system = Py.getSystemState();
+
+        if (system.getClassLoader() == null &&  packageExists(Py.getSystemState().path,pkg,name)) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
