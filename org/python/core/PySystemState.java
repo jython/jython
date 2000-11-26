dabbe5ed306a609fc6fb73f9a4a2c4eb80269e8c
Finn Bock <bckfnn@worldonline.dk>
2000-11-26 16:44:11 +0000

    Changed version to 2.0alpha1

--- org/python/core/PySystemState.java
+++ org/python/core/PySystemState.java
@@ -13,7 +13,7 @@ public class PySystemState extends PyObject
     /**
      * The current version of JPython.
      */
-    public static String version = "2.0 pre-alpha";
+    public static String version = "2.0alpha1";
 
     /**
      * The copyright notice for this release.
