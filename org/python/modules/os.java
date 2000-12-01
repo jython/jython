31e67345afcc3f8d612a1967072e2967be7414f0
Finn Bock <bckfnn@worldonline.dk>
2000-12-01 19:23:43 +0000

    Add a __file__ attribute to the "os" module. This is mostly a hack, but it allow the site.py module from CPython2.0 to be used with jython.

--- org/python/modules/os.java
+++ org/python/modules/os.java
@@ -5,6 +5,11 @@ import org.python.core.*;
 
 public class os implements ClassDictInit {
     public static String[] __depends__ = new String[] {"javaos", };
+
+    // An ugly hack, but it keeps the site.py from CPython2.0 happy
+
+    public static String __file__ = 
+          Py.getSystemState().prefix.toString() + "/Lib/javaos.py";
     
     public static void classDictInit(PyObject dict) {
         // Fake from javaos import *
