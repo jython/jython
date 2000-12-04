6bbafe739a014e1776536eac81278388b8a40046
Finn Bock <bckfnn@worldonline.dk>
2000-12-04 21:27:02 +0000

    Moved here from the modules package.

---
+++ org/python/core/ucnhashAPI.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2000 Finn Bock
+ *
+ * This program contains material copyrighted by:
+ * Copyright (c) Corporation for National Research Initiatives.
+ * Originally written by Marc-Andre Lemburg (mal@lemburg.com).
+ */
+
+package org.python.core;
+
+public interface ucnhashAPI {
+    public int getCchMax();
+    public int getValue(String s, int start, int end);
+}
