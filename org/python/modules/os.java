658d1ac48c874448b9dd8c9cff1de4ba812a97c8
Finn Bock <bckfnn@worldonline.dk>
2001-02-07 09:26:19 +0000

    Check for a non-null prefix before using it.

--- org/python/modules/os.java
+++ org/python/modules/os.java
@@ -8,10 +8,13 @@ public class os implements ClassDictInit {
 
     // An ugly hack, but it keeps the site.py from CPython2.0 happy
 
-    public static String __file__ =
-          Py.getSystemState().prefix.toString() + "/Lib/javaos.py";
+    public static String __file__;
 
     public static void classDictInit(PyObject dict) {
+        String prefix = Py.getSystemState().prefix;
+        if (prefix != null)
+            __file__ = prefix + "/Lib/javaos.py";
+
         // Fake from javaos import *
         PyFrame frame = new PyFrame(null, dict, dict, null);
         org.python.core.imp.importAll("javaos", frame);
