c332ce166695d05e26985686dd8e70e586c55f1f
Finn Bock <bckfnn@worldonline.dk>
2001-03-13 20:32:47 +0000

    Added docstring from cpython.

--- Lib/javaos.py
+++ Lib/javaos.py
@@ -1,3 +1,24 @@
+r"""OS routines for Mac, DOS, NT, or Posix depending on what system we're on.
+
+This exports:
+  - all functions from posix, nt, dos, os2, mac, or ce, e.g. unlink, stat, etc.
+  - os.path is one of the modules posixpath, ntpath, macpath, or dospath
+  - os.name is 'posix', 'nt', 'dos', 'os2', 'mac', 'ce' or 'riscos'
+  - os.curdir is a string representing the current directory ('.' or ':')
+  - os.pardir is a string representing the parent directory ('..' or '::')
+  - os.sep is the (or a most common) pathname separator ('/' or ':' or '\\')
+  - os.altsep is the alternate pathname separator (None or '/')
+  - os.pathsep is the component separator used in $PATH etc
+  - os.linesep is the line separator in text files ('\r' or '\n' or '\r\n')
+  - os.defpath is the default search path for executables
+
+Programs that import and use 'os' stand a better chance of being
+portable between different platforms.  Of course, they must then
+only use functions that are defined by all platforms (e.g., unlink
+and opendir), and leave all pathname manipulation to os.path
+(e.g., split and join).
+"""
+
 import java
 from java.io import File
 import javapath
