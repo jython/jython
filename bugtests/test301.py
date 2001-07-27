63704c8d82a46dc3d13d7364db6d8f1906c15933
Finn Bock <bckfnn@worldonline.dk>
2001-07-27 19:51:32 +0000

    Make failure a hard error now the bug is fixed.

--- bugtests/test301.py
+++ bugtests/test301.py
@@ -1,5 +1,5 @@
 """
-Basic test, just raises an TestError
+[ #434324 ] jythonc -A and dead java vars
 """
 
 import support
@@ -10,8 +10,5 @@ support.compileJPythonc("test301c.py", jar="test301.jar", core=1,
                                        addpackages="test301p",
                                        output="test301.err")
 
-ret = support.runJava("test301c", cp="test301.jar", output="test301c.err",
-                                  expectError=1)
-if ret != 0:
-    raise support.TestWarning('This should work. See test301c.err for details')
+support.runJava("test301c", cp="test301.jar", output="test301c.err")
 
