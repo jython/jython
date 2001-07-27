c74e26ada1adab393c94f365666c375877f3e2e8
Finn Bock <bckfnn@worldonline.dk>
2001-07-27 14:56:10 +0000

    Updated to a3.

--- org/python/core/PySystemState.java
+++ org/python/core/PySystemState.java
@@ -17,13 +17,13 @@ public class PySystemState extends PyObject
     /**
      * The current version of JPython.
      */
-    public static String version = "2.1a2";
+    public static String version = "2.1a3";
 
     private static int PY_MAJOR_VERSION = 2;
     private static int PY_MINOR_VERSION = 1;
     private static int PY_MICRO_VERSION = 0;
     private static int PY_RELEASE_LEVEL = 0xA;
-    private static int PY_RELEASE_SERIAL = 2;
+    private static int PY_RELEASE_SERIAL = 3;
 
     public static int hexversion = ((PY_MAJOR_VERSION << 24) | 
                                     (PY_MINOR_VERSION << 16) |
