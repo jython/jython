fdba4e886f7dae6a6834352219a547f55f37ef25
Finn Bock <bckfnn@worldonline.dk>
2001-07-29 20:33:43 +0000

    Show version number in hello world demo.

--- Demo/applet/HelloWorld.py
+++ Demo/applet/HelloWorld.py
@@ -1,8 +1,9 @@
 from java.applet import Applet
+import sys
 
 class HelloWorld(Applet):
     def paint(self, g):
-	g.drawString("Hello from Jython!", 20, 30)
+	g.drawString("Hello from Jython %s!" % sys.version, 20, 30)
 
 
 if __name__ == '__main__':
