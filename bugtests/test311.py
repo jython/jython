a79406592e49f50e1d0760d1498660687e918f19
Finn Bock <bckfnn@worldonline.dk>
2001-08-05 10:00:52 +0000

    A test for the DST flag in time.mktime()

---
+++ bugtests/test311.py
@@ -0,0 +1,19 @@
+"""
+Test DST for time.mktime
+"""
+
+import support
+import time
+
+def check(tup, facit):
+   #print tup, long(time.mktime(tup)), facit
+   assert time.mktime(tup) == facit
+
+check((1998, 6, 13, 0, 0, 0, 0, 0, 0), 897692400)
+check((1998, 6, 13, 0, 0, 0, 0, 0, -1), 897688800)
+check((1998, 6, 13, 0, 0, 0, 0, 0, 0), 897692400)
+check((1998, 6, 13, 0, 0, 0, 0, 0, 1), 897688800)
+check((1998, 1, 13, 0, 0, 0, 0, 0, -1), 884646000)
+check((1998, 1, 13, 0, 0, 0, 0, 0, 0), 884646000)
+check((1998, 1, 13, 0, 0, 0, 0, 0, 1), 884642400)
+
