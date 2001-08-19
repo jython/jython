56c0319a19e98df2787da810cf4884e7952b7969
Finn Bock <bckfnn@worldonline.dk>
2001-08-19 14:40:13 +0000

    Bumped test count to 340.

--- bugtests/driver.py
+++ bugtests/driver.py
@@ -16,7 +16,7 @@ def reportError():
 opts, argv = getopt.getopt(sys.argv[1:], 'w')
 warnings = "-w" in opts
 
-for i in range(320):
+for i in range(340):
     m = None
     n = "test%3.3d" % i
     try:
