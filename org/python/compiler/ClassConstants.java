eb9f697415f024725e08e78a91c6b18a7359009f
Finn Bock <bckfnn@worldonline.dk>
2001-11-27 19:12:16 +0000

    Initial version.

---
+++ org/python/compiler/ClassConstants.java
@@ -0,0 +1,26 @@
+package org.python.compiler;
+
+public interface ClassConstants {
+
+    final static String $pyObj      = "Lorg/python/core/PyObject;";
+    final static String $pyObjArr   = "[Lorg/python/core/PyObject;";
+    final static String $pyStr      = "Lorg/python/core/PyString;";
+    final static String $pyExc      = "Lorg/python/core/PyException;";
+    final static String $pyFrame    = "Lorg/python/core/PyFrame;";
+    final static String $pyCode     = "Lorg/python/core/PyCode;";
+    final static String $pyInteger  = "Lorg/python/core/PyInteger;";
+    final static String $pyLong     = "Lorg/python/core/PyLong;";
+    final static String $pyFloat    = "Lorg/python/core/PyFloat;";
+    final static String $pyComplex  = "Lorg/python/core/PyComplex;";
+    final static String $pyRunnable = "Lorg/python/core/PyRunnable;";
+    final static String $pyFuncTbl  = "Lorg/python/core/PyFunctionTable;";
+    final static String $pyProxy    = "Lorg/python/core/PyProxy;";
+
+    final static String $obj       = "Ljava/lang/Object;";
+    final static String $objArr    = "[Ljava/lang/Object;";
+    final static String $clss      = "Ljava/lang/Class;";
+    final static String $str       = "Ljava/lang/String;";
+    final static String $strArr    = "[Ljava/lang/String;";
+    final static String $throwable = "Ljava/lang/Throwable;";
+
+}
