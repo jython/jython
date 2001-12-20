243319d3d93f47b4dae3f23d034c26bf52205b39
Finn Bock <bckfnn@worldonline.dk>
2001-12-20 16:41:42 +0000

    Dump the test counter.

--- bugtests/driver.py
+++ bugtests/driver.py
@@ -16,7 +16,7 @@ def reportError():
 opts, argv = getopt.getopt(sys.argv[1:], 'w')
 warnings = "-w" in opts
 
-for i in range(350):
+for i in range(360):
     m = None
     n = "test%3.3d" % i
     try:
