e864003c53d3daee9b6fcd4c25ca557f178cb520
Finn Bock <bckfnn@worldonline.dk>
2002-05-30 19:34:22 +0000

    Updated range to 370.

--- bugtests/driver.py
+++ bugtests/driver.py
@@ -16,7 +16,7 @@ def reportError():
 opts, argv = getopt.getopt(sys.argv[1:], 'w')
 warnings = "-w" in opts
 
-for i in range(360):
+for i in range(370):
     m = None
     n = "test%3.3d" % i
     try:
