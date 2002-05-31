4585d4dab2f405c54eb3a46241d762c14e020a44
Finn Bock <bckfnn@worldonline.dk>
2002-05-31 12:45:05 +0000

    Test for "[ 508111 ] jythonc generates invalid statements"

---
+++ bugtests/test365.py
@@ -0,0 +1,9 @@
+"""
+[ 508111 ] jythonc generates invalid statements
+"""
+
+import support
+
+support.compileJPythonc("test365c.py")
+
+#raise support.TestWarning('A test of TestWarning. It is not an error')
---
+++ bugtests/test365c.py
@@ -0,0 +1,8 @@
+
+bar = 1
+def foo():
+    return 2
+
+bar and foo()
+
+
