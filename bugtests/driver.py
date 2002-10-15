4d8543840b90054f9dba949355a693e40a90fd2d
Finn Bock <bckfnn@worldonline.dk>
2002-10-15 13:38:18 +0000

    Increase the number of tests.

--- bugtests/driver.py
+++ bugtests/driver.py
@@ -16,7 +16,7 @@ def reportError():
 opts, argv = getopt.getopt(sys.argv[1:], 'w')
 warnings = "-w" in opts
 
-for i in range(370):
+for i in range(380):
     m = None
     n = "test%3.3d" % i
     try:
