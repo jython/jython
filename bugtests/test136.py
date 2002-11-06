3c451c370bf9c136b9798235fffa3f37d3af39cf
Finn Bock <bckfnn@worldonline.dk>
2002-11-06 14:49:23 +0000

    Accept a TypeError as a valid result. I think that is acceptable.

--- bugtests/test136.py
+++ bugtests/test136.py
@@ -12,5 +12,8 @@ try:
     list(x)
 except AttributeError, e:
     support.compare(e, "__len__")
+except TypeError, e:
+    # Changed to a TypeError in 2.2
+    support.compare(e, "iteration over non-sequence")
 else:
     raise support.TestError("Should raise a AttributeError on __len__")
