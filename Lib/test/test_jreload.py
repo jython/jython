f17a4f21cc2992ea8c471d79abc65460b49e2e8b
Kevin J. Butler <jython-kbutler@sabaydi.com>
2003-04-18 05:47:52 +0000

    Add test for jreload of large files

--- Lib/test/test_jreload.py
+++ Lib/test/test_jreload.py
@@ -4,30 +4,37 @@ We do this by loading a large class file from tools.jar.  If the load failes wit
 the bug is present, if the load succeeds, the bug is fixed, if something else occurs, we don't know.
 """
 
-from test_support import *
-
-print_test( 'jreload module (test_jreload.py)', 1)
+import unittest
+import test_support
 
 from jreload import makeLoadSet
 from java.lang import System, ClassFormatError
 
 import os
 
-# attempt to find tools.jar
-toolsjar = System.getProperty( "toolsjar" )
-if not toolsjar:
-    extdirs = System.getProperty( "java.ext.dirs" ) #JDK_DIR/jre/lib/ext
-    jdkdir = os.path.dirname( os.path.dirname( os.path.dirname( extdirs )))
-    toolsjar = os.path.join( os.path.join( jdkdir, "lib" ), "tools.jar" )
+class JreloadTestCase(unittest.TestCase):
+    # attempt to find tools.jar
+    toolsjar = System.getProperty( "toolsjar" )
+    if not toolsjar:
+        extdirs = System.getProperty( "java.ext.dirs" ) #JDK_DIR/jre/lib/ext
+        jdkdir = os.path.dirname( os.path.dirname( os.path.dirname( extdirs )))
+        toolsjar = os.path.join( os.path.join( jdkdir, "lib" ), "tools.jar" )
 
-if not os.path.exists( toolsjar ):
-    print "Unable to find tools.jar, unable to perform test"
-else:
-    myls = makeLoadSet('myls', [toolsjar])
+    if not os.path.exists( toolsjar ):
+        raise test_support.TestSkipped( "Unable to find tools.jar" )
 
-    try:
-        from myls.com.sun.tools.corba.se.idl import Parser
-    except ClassFormatError:
-        print "Reload Error is present"
-        raise
-    print "Success"
+    def test( self ):
+        myls = makeLoadSet('myls', [self.toolsjar])
+
+        try:
+            from myls.com.sun.tools.corba.se.idl import Parser
+        except ClassFormatError:
+            print "Reload Error is present"
+            raise
+        
+def test_main():
+    test_support.run_unittest(JreloadTestCase)
+
+if __name__ == "__main__":
+    test_main()
+
