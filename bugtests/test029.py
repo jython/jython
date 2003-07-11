29868e754b669274c15ba9df871d36c3db076b19
Samuele Pedronis <pedronis@openend.se>
2003-07-11 22:07:28 +0000

    added tests for #654863 string.split behaves differently.

--- bugtests/test029.py
+++ bugtests/test029.py
@@ -1,8 +1,45 @@
 """
-Check split error.
+Check split errors.
 """
 
 import support
 
 import string
-string.split("aa::bb::cc:", "::")
+if string.split("aa::bb::cc:", "::") != ["aa","bb","cc:"]:
+    raise support.TestError("split misbehavior")
+
+if 'a'.split(None) != ['a']:
+    raise support.TestError("split misbehavior")
+
+if 'a'.split(None,1) != ['a']:
+    raise support.TestError("split misbehavior")
+
+if 'a'.split(' ') != ['a']:
+    raise support.TestError("split misbehavior")
+
+if 'a'.split(' ',1) != ['a']:
+    raise support.TestError("split misbehavior")
+
+if 'a '.split(None) != ['a']:
+    raise support.TestError("split misbehavior")
+
+if 'a b '.split(None) != ['a','b']:
+    raise support.TestError("split misbehavior")
+
+if 'a '.split(' ') != ['a','']:
+    raise support.TestError("split misbehavior")
+
+if 'a b '.split(' ') != ['a','b','']:
+    raise support.TestError("split misbehavior")
+
+if 'a '.split(None,1) != ['a']:
+    raise support.TestError("split misbehavior")
+
+if 'a b '.split(None,1) != ['a','b ']:
+    raise support.TestError("split misbehavior")
+
+if 'a '.split(' ',1) != ['a','']:
+    raise support.TestError("split misbehavior")
+
+if 'a b '.split(' ',1) != ['a','b ']:
+    raise support.TestError("split misbehavior")
