2080887e1cd0d3f0f91801198b25f99359b4a2f3
Brian Zimmer <bzimmer@ziclix.com>
2005-06-03 03:23:39 +0000

    reverting to old version scheme

--- org/python/util/InteractiveConsole.java
+++ org/python/util/InteractiveConsole.java
@@ -34,7 +34,7 @@ public class InteractiveConsole extends InteractiveInterpreter {
     public static String getDefaultBanner() {
         String compiler = System.getProperty("java.compiler");
 
-        return "Jython " + PySystemState.VERSION + " on " +
+        return "Jython " + PySystemState.version + " on " +
             PySystemState.platform + " (JIT: " +
             ((compiler == null) ? "null" : compiler) +  ")";
     }
