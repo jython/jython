c3ade75ef24e96cebe68df82751306761056ba8b
Brian Zimmer <bzimmer@ziclix.com>
2005-06-03 02:44:23 +0000

    new-style class; patch #1094757

---
+++ Lib/test/test_enumerate.py
@@ -0,0 +1,151 @@
+# Taken from Python 2.3.4 test suite.
+
+from __future__ import generators
+import unittest
+
+#from sets import Set
+
+from test import test_support
+
+class G:
+    'Sequence using __getitem__'
+    def __init__(self, seqn):
+        self.seqn = seqn
+    def __getitem__(self, i):
+        return self.seqn[i]
+
+class I:
+    'Sequence using iterator protocol'
+    def __init__(self, seqn):
+        self.seqn = seqn
+        self.i = 0
+    def __iter__(self):
+        return self
+    def next(self):
+        if self.i >= len(self.seqn): raise StopIteration
+        v = self.seqn[self.i]
+        self.i += 1
+        return v
+
+class Ig:
+    'Sequence using iterator protocol defined with a generator'
+    def __init__(self, seqn):
+        self.seqn = seqn
+        self.i = 0
+    def __iter__(self):
+        for val in self.seqn:
+            yield val
+
+class X:
+    'Missing __getitem__ and __iter__'
+    def __init__(self, seqn):
+        self.seqn = seqn
+        self.i = 0
+    def next(self):
+        if self.i >= len(self.seqn): raise StopIteration
+        v = self.seqn[self.i]
+        self.i += 1
+        return v
+
+class E:
+    'Test propagation of exceptions'
+    def __init__(self, seqn):
+        self.seqn = seqn
+        self.i = 0
+    def __iter__(self):
+        return self
+    def next(self):
+        3/0
+
+class N:
+    'Iterator missing next()'
+    def __init__(self, seqn):
+        self.seqn = seqn
+        self.i = 0
+    def __iter__(self):
+        return self
+
+class EnumerateTestCase(unittest.TestCase):
+
+    enum = enumerate
+    seq, res = 'abc', [(0,'a'), (1,'b'), (2,'c')]
+
+    def test_basicfunction(self):
+        #
+        #self.assertEqual(type(self.enum(self.seq)), self.enum)
+        #
+        e = self.enum(self.seq)
+        self.assertEqual(iter(e), e)
+        self.assertEqual(list(self.enum(self.seq)), self.res)
+        self.enum.__doc__
+
+    def test_getitemseqn(self):
+        self.assertEqual(list(self.enum(G(self.seq))), self.res)
+        e = self.enum(G(''))
+        self.assertRaises(StopIteration, e.next)
+
+    def test_iteratorseqn(self):
+        self.assertEqual(list(self.enum(I(self.seq))), self.res)
+        e = self.enum(I(''))
+        self.assertRaises(StopIteration, e.next)
+
+    def test_iteratorgenerator(self):
+        self.assertEqual(list(self.enum(Ig(self.seq))), self.res)
+        e = self.enum(Ig(''))
+        self.assertRaises(StopIteration, e.next)
+
+    def test_noniterable(self):
+        self.assertRaises(TypeError, self.enum, X(self.seq))
+
+    def test_illformediterable(self):
+        self.assertRaises(TypeError, list, self.enum(N(self.seq)))
+
+    def test_exception_propagation(self):
+        self.assertRaises(ZeroDivisionError, list, self.enum(E(self.seq)))
+
+    def test_argumentcheck(self):
+        self.assertRaises(TypeError, self.enum) # no arguments
+        self.assertRaises(TypeError, self.enum, 1) # wrong type (not iterable)
+        self.assertRaises(TypeError, self.enum, 'abc', 2) # too many arguments
+
+    #def test_tuple_reuse(self):
+    #    # Tests an implementation detail where tuple is reused
+    #    # whenever nothing else holds a reference to it
+    #    self.assertEqual(len(Set(map(id, list(enumerate(self.seq))))), len(self.seq))
+    #    self.assertEqual(len(Set(map(id, enumerate(self.seq)))), min(1,len(self.seq)))
+
+#class MyEnum(enumerate):
+#    pass
+#
+#class SubclassTestCase(EnumerateTestCase):
+#
+#    enum = MyEnum
+
+class TestEmpty(EnumerateTestCase):
+
+    seq, res = '', []
+
+class TestBig(EnumerateTestCase):
+
+    seq = range(10,20000,2)
+    res = zip(range(20000), seq)
+
+
+def test_main(verbose=None):
+    #testclasses = (EnumerateTestCase, SubclassTestCase, TestEmpty, TestBig)
+    #test_support.run_unittest(*testclasses)
+    test_support.run_unittest(EnumerateTestCase)
+    test_support.run_unittest(TestEmpty)
+    test_support.run_unittest(TestBig)
+
+    # verify reference counting
+    import sys
+    if verbose and hasattr(sys, "gettotalrefcount"):
+        counts = [None] * 5
+        for i in xrange(len(counts)):
+            test_support.run_unittest(*testclasses)
+            counts[i] = sys.gettotalrefcount()
+        print counts
+
+if __name__ == "__main__":
+    test_main(verbose=True)
--- org/python/core/PyEnumerate.java
+++ org/python/core/PyEnumerate.java
@@ -6,19 +6,98 @@ public class PyEnumerate extends PyIterator {
     private PyObject en_sit;        /* secondary iterator of enumeration */
     private PyTuple en_result;      /* result tuple  */
     protected static PyObject __methods__;
+    
+    /* type info */
 
-    static {
-        PyList list = new PyList();
-        String[] methods = {"next"};
-        list.append(new PyString(methods[0]));
-        __methods__ = list;
+    public static final String exposed_name="enumerate";
+
+    public static final Class exposed_base=PyObject.class;
+
+    public static void typeSetup(PyObject dict,PyType.Newstyle marker) {
+        class exposed_next extends PyBuiltinFunctionNarrow {
+
+            private PyEnumerate self;
+
+            public PyObject getSelf() {
+                return self;
+            }
+
+            exposed_next(PyEnumerate self,PyBuiltinFunction.Info info) {
+                super(info);
+                this.self=self;
+            }
+
+            public PyBuiltinFunction makeBound(PyObject self) {
+                return new exposed_next((PyEnumerate)self,info);
+            }
+
+            public PyObject __call__() {
+                return self.enumerate_next();
+            }
+
+            public PyObject inst_call(PyObject gself) {
+                PyEnumerate self=(PyEnumerate)gself;
+                return self.enumerate_next();
+            }
+
+        }
+        dict.__setitem__("next",new PyMethodDescr("next",PyEnumerate.class,0,0,new exposed_next(null,null)));
+        class exposed___iter__ extends PyBuiltinFunctionNarrow {
+
+            private PyEnumerate self;
+
+            public PyObject getSelf() {
+                return self;
+            }
+
+            exposed___iter__(PyEnumerate self,PyBuiltinFunction.Info info) {
+                super(info);
+                this.self=self;
+            }
+
+            public PyBuiltinFunction makeBound(PyObject self) {
+                return new exposed___iter__((PyEnumerate)self,info);
+            }
+
+            public PyObject __call__() {
+                return self.enumerate___iter__();
+            }
+
+            public PyObject inst_call(PyObject gself) {
+                PyEnumerate self=(PyEnumerate)gself;
+                return self.enumerate___iter__();
+            }
+
+        }
+        dict.__setitem__("__iter__",new PyMethodDescr("__iter__",PyEnumerate.class,0,0,new exposed___iter__(null,null)));
+        dict.__setitem__("__new__",new PyNewWrapper(PyEnumerate.class,"__new__",-1,-1) {
+            public PyObject new_impl(boolean init,PyType subtype,PyObject[]args,String[]keywords) {
+                return enumerate_new(this,init,subtype,args,keywords);
+            }
+       });
+    }
+    
+    public PyObject enumerate_next() {
+        return next();
+    }
+    
+    public PyObject enumerate___iter__() {
+        return __iter__();
     }
 
+    public static PyEnumerate enumerate_new(PyObject new_, boolean init, PyType subtype,
+                                            PyObject[] args, String[] keywords) {
+        if (args.length != 1) {
+            throw PyBuiltinFunction.DefaultInfo.unexpectedCall(args.length,false,exposed_name,0,1);
+        }
+        return new PyEnumerate(args[0]);        
+    }
+    
     public PyEnumerate(PyObject seq) {
         en_index = 0;
         en_sit = seq.__iter__();
     }
-
+    
     public PyObject __iternext__() {
         PyObject next_item;
         PyObject next_index;
@@ -31,16 +110,5 @@ public class PyEnumerate extends PyIterator {
 
         en_result = new PyTuple(new PyObject[] {next_index, next_item});
         return en_result;
-    }
-
-    public PyObject __findattr__(String name) {
-        if (name.equals("__methods__")) {
-            return __methods__;
-        }
-        return super.__findattr__(name);
-    }
-
-    public String toString() {
-        return "<enumerate object "+Py.idstr(this)+">";
     }
 }
