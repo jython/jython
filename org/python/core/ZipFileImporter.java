3a7f4e89cffeb5f349a08e347fd0f72957afd5ca
Frank Wierzbicki <fwierzbicki@gmail.com>
2005-11-06 02:06:36 +0000

    refromat

--- org/python/core/ZipFileImporter.java
+++ org/python/core/ZipFileImporter.java
@@ -12,22 +12,24 @@ public class ZipFileImporter extends PyObject {
     private SyspathArchive archive;
 
     /**
-     * If this path is not an archive (.zip or .jar) then raise
-     * an ImportError, otherwise this instance will handle this
-     * path.
+     * If this path is not an archive (.zip or .jar) then raise an ImportError,
+     * otherwise this instance will handle this path.
+     * 
      * @param path the path to check for modules
      */
     public ZipFileImporter(PyObject path) {
-        if(!(path instanceof SyspathArchive)) {
+        if (!(path instanceof SyspathArchive)) {
             throw Py.ImportError(path.toString());
         }
-        this.archive = (SyspathArchive)path;
+        this.archive = (SyspathArchive) path;
     }
 
     /**
      * Find the module for the fully qualified name.
+     * 
      * @param name the fully qualified name of the module
-     * @return a loader instance if this importer can load the module, None otherwise
+     * @return a loader instance if this importer can load the module, None
+     *         otherwise
      */
     public PyObject find_module(String name) {
         return find_module(name, Py.None);
@@ -35,9 +37,11 @@ public class ZipFileImporter extends PyObject {
 
     /**
      * Find the module for the fully qualified name.
+     * 
      * @param name the fully qualified name of the module
      * @param path if installed on the meta-path None or a module path
-     * @return a loader instance if this importer can load the module, None otherwise
+     * @return a loader instance if this importer can load the module, None
+     *         otherwise
      */
     public PyObject find_module(String name, PyObject path) {
         ZipModuleInfo zip = getModuleInfo(name, this.archive);
@@ -46,7 +50,7 @@ public class ZipFileImporter extends PyObject {
 
     /**
      * Returns a string representation of the object.
-     *
+     * 
      * @return a string representation of the object.
      */
     public String toString() {
@@ -54,31 +58,34 @@ public class ZipFileImporter extends PyObject {
     }
 
     /**
-     * Returns the last part of a fully qualified name.  For example, the name
+     * Returns the last part of a fully qualified name. For example, the name
      * <p>
      * <code>
      * a.b.c
      * </code>
      * </p>
      * would return <code>c</code>.
+     * 
      * @param name a fully qualified name
      * @return the last part of a fully qualified name
      */
     private String getSubName(String name) {
         int x = name.lastIndexOf(".");
         if (x >= 0) {
-            return name.substring(x+1);
+            return name.substring(x + 1);
         }
         return name;
     }
 
     /**
-     * Find the module name starting at the zipArchive root.  This method will
-     * look for both package and non-package names in the archive.  If the name
+     * Find the module name starting at the zipArchive root. This method will
+     * look for both package and non-package names in the archive. If the name
      * is not found null will be returned.
+     * 
      * @param name the fully qualified module name
      * @param zipArchive the root of the path to begin looking
-     * @return null if the module is not found, a ZipModuleInfo instance otherwise
+     * @return null if the module is not found, a ZipModuleInfo instance
+     *         otherwise
      */
     private ZipModuleInfo getModuleInfo(String name, SyspathArchive zipArchive) {
         String entryName = getSubName(name);
@@ -89,19 +96,19 @@ public class ZipFileImporter extends PyObject {
         ZipEntry compiledEntry = zipArchive.getEntry(compiledName);
 
         boolean pkg = (sourceEntry != null || compiledEntry != null);
-        if(!pkg) {
+        if (!pkg) {
             sourceName = entryName + ".py";
             compiledName = entryName + "$py.class";
             sourceEntry = zipArchive.getEntry(sourceName);
             compiledEntry = zipArchive.getEntry(compiledName);
         } else {
-           zipArchive = zipArchive.makeSubfolder(entryName);
+            zipArchive = zipArchive.makeSubfolder(entryName);
         }
 
         ZipModuleInfo info = null;
         if (sourceEntry != null) {
             Py.writeDebug("import", "trying source entry: " + sourceName
-              + " from jar/zip file " + zipArchive);
+                    + " from jar/zip file " + zipArchive);
             if (compiledEntry != null) {
                 Py.writeDebug("import", "trying precompiled entry "
                         + compiledName + " from jar/zip file " + zipArchive);
@@ -114,8 +121,8 @@ public class ZipFileImporter extends PyObject {
             info = new ZipModuleInfo(zipArchive, sourceEntry, false);
         }
 
-        if(pkg && info != null) {
-            info.path = new PyList(new PyObject[]{zipArchive});
+        if (pkg && info != null) {
+            info.path = new PyList(new PyObject[] { zipArchive });
         }
 
         return info;
@@ -134,12 +141,13 @@ public class ZipFileImporter extends PyObject {
 
         /**
          * A loaded module for the fully qualified name.
+         * 
          * @param moduleName the fully qualified name
          * @return a loaded module (added to sys.path)
          */
         public PyObject load_module(String moduleName) {
             PyModule m = null;
-            if(this._info.path != null) {
+            if (this._info.path != null) {
                 m = imp.addModule(moduleName);
                 m.__dict__.__setitem__("__path__", this._info.path);
                 m.__dict__.__setitem__("__loader__", this);
@@ -150,21 +158,24 @@ public class ZipFileImporter extends PyObject {
             try {
                 is = this._info.archive.getInputStream(entry);
             } catch (IOException e) {
-                Py.writeDebug("import", "loadFromZipFile exception: " + e.toString());
+                Py.writeDebug("import", "loadFromZipFile exception: "
+                        + e.toString());
                 throw Py.ImportError("error loading from zipfile");
             }
             if (this._info.compiled) {
-                PyObject o = imp.createFromPyClass(moduleName, is, true, entry.getName());
+                PyObject o = imp.createFromPyClass(moduleName, is, true, entry
+                        .getName());
                 return (m == null) ? o : m;
             } else {
-                PyObject o = imp.createFromSource(moduleName, is, entry.getName(), null);
+                PyObject o = imp.createFromSource(moduleName, is, entry
+                        .getName(), null);
                 return (m == null) ? o : m;
             }
         }
 
         /**
          * Returns a string representation of the object.
-         *
+         * 
          * @return a string representation of the object.
          */
         public String toString() {
@@ -175,18 +186,23 @@ public class ZipFileImporter extends PyObject {
     private class ZipModuleInfo {
         /** The path of the package if it is a package. */
         public PyObject path;
+
         /** Whether the code is already compiled. */
         public boolean compiled;
+
         /** The zip entry for the file to load. */
         public ZipEntry zipEntry;
+
         /** The archive in which the zip entry resides. */
         public SyspathArchive archive;
 
-        public ZipModuleInfo(SyspathArchive archive, ZipEntry zipEntry, boolean compiled) {
+        public ZipModuleInfo(SyspathArchive archive, ZipEntry zipEntry,
+                boolean compiled) {
             this(archive, zipEntry, compiled, null);
         }
 
-        public ZipModuleInfo(SyspathArchive archive, ZipEntry zipEntry, boolean compiled, PyObject path) {
+        public ZipModuleInfo(SyspathArchive archive, ZipEntry zipEntry,
+                boolean compiled, PyObject path) {
             this.path = path;
             this.archive = archive;
             this.zipEntry = zipEntry;
@@ -194,4 +210,3 @@ public class ZipFileImporter extends PyObject {
         }
     }
 }
-
