9bd695fe90cfbf8ddbe0930fea1d0ddf7cbb38b5
Frank Wierzbicki <fwierzbicki@gmail.com>
2006-04-09 16:14:16 +0000

    First pass to get ant working again.

--- Lib/test/javatests/AnonInner.java
+++ Lib/test/javatests/AnonInner.java
@@ -9,6 +9,11 @@ public class AnonInner
                 public int hashCode() {
                     return 2000;
                 }
+		//XXX: stuck compareTo to make the compiler happier.
+		//     hopefully this doesn't mess up the test.
+		public int compareTo(Object x) {
+		    return 0;
+		}
             };
         return d.hashCode();
     }
