5d0358eac1d34df652465b350d3942e08362591b
Charlie Groves <charlie.groves@gmail.com>
2006-08-12 19:24:36 +0000

    reenabled now that bug #480017 is fixed

--- bugtests/test338.py
+++ bugtests/test338.py
@@ -1,5 +1,7 @@
 """
 [ #480017 ] Proxy supers are loaded from syspath
+Running test338j will throw a ClassCastException if a proxy's superclass is loaded
+by the syspath classloader.
 """
 
 import support
@@ -8,6 +10,5 @@ support.compileJava("test338cl.java", classpath=".")
 support.compileJava("test338j1.java", classpath=".")
 support.compileJava("test338j.java", classpath=".")
 
-#support.runJava("test338j", classpath=".")
-raise support.TestWarning("Should (maybe) not fail.")
+support.runJava("test338j", classpath=".")
 
