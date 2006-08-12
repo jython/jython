808e05d902d85c9368780f484254060c800c5efd
Charlie Groves <charlie.groves@gmail.com>
2006-08-12 18:44:40 +0000

    move java importation to a __classpath__ entry on sys.path from a metapath hook.  This fixes bug #1246413 and bug #1421812

--- src/org/python/core/PySystemState.java
+++ src/org/python/core/PySystemState.java
@@ -232,11 +232,12 @@ public class PySystemState extends PyObject
 
         argv = (PyList)defaultArgv.repeat(1);
         path = (PyList)defaultPath.repeat(1);
+        path.append(Py.newString("__classpath__"));
 
         meta_path = new PyList();
         meta_path.append(new PrecompiledImporter());
-        meta_path.append(new JavaImporter());
         path_hooks = new PyList();
+        path_hooks.append(new JavaImporter());
         path_hooks.append(PyJavaClass.lookup(ZipFileImporter.class));
         path_importer_cache = new PyDictionary();
 
