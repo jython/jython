ce4b09219acc73a2d9455748189d9771739b8caa
Charlie Groves <charlie.groves@gmail.com>
2006-08-26 07:33:21 +0000

    merge in updates from Python-2.3

--- Lib/test/test_support.py
+++ Lib/test/test_support.py
@@ -1,4 +1,7 @@
-"""Supporting definitions for the Python regression test."""
+"""Supporting definitions for the Python regression tests."""
+
+#if __name__ != 'test.test_support':
+#    raise ImportError, 'test_support must be imported from the test package'
 
 import sys
 
@@ -16,6 +19,14 @@ class TestSkipped(Error):
     example, if some resource can't be used, such as the network
     appears to be unavailable, this should be raised instead of
     TestFailed.
+    """
+
+class ResourceDenied(TestSkipped):
+    """Test skipped because it requested a disallowed resource.
+
+    This is raised when a test calls requires() for a resource that
+    has not be enabled.  It is used to distinguish between expected
+    and unexpected skips.
     """
 
 verbose = 1              # Flag set to 0 by regrtest.py
@@ -39,19 +50,41 @@ def unload(name):
         pass
 
 def forget(modname):
+    '''"Forget" a module was ever imported by removing it from sys.modules and
+    deleting any .pyc and .pyo files.'''
     unload(modname)
     import os
     for dirname in sys.path:
         try:
-            os.unlink(os.path.join(dirname, modname + '.pyc'))
+            os.unlink(os.path.join(dirname, modname + os.extsep + 'pyc'))
+        except os.error:
+            pass
+        # Deleting the .pyo file cannot be within the 'try' for the .pyc since
+        # the chance exists that there is no .pyc (and thus the 'try' statement
+        # is exited) but there is a .pyo file.
+        try:
+            os.unlink(os.path.join(dirname, modname + os.extsep + 'pyo'))
         except os.error:
             pass
 
+def is_resource_enabled(resource):
+    """Test whether a resource is enabled.  Known resources are set by
+    regrtest.py."""
+    return use_resources is not None and resource in use_resources
+
 def requires(resource, msg=None):
-    if use_resources is not None and resource not in use_resources:
+    """Raise ResourceDenied if the specified resource is not available.
+
+    If the caller's module is __main__ then automatically return True.  The
+    possibility of False being returned occurs when regrtest.py is executing."""
+    # see if the caller's module is __main__ - if so, treat as if
+    # the resource was set
+    if sys._getframe().f_back.f_globals.get("__name__") == "__main__":
+        return
+    if not is_resource_enabled(resource):
         if msg is None:
             msg = "Use of the `%s' resource not enabled" % resource
-        raise TestSkipped(msg)
+        raise ResourceDenied(msg)
 
 FUZZ = 1e-6
 
@@ -85,20 +118,47 @@ import os
 if os.name == 'java':
     # Jython disallows @ in module names
     TESTFN = '__test' # xxx mmh, something good and that works on unix too
-elif os.name != 'riscos':
+elif os.name == 'riscos':
+    TESTFN = 'testfile'
+else:
     TESTFN = '@test'
     # Unicode name only used if TEST_FN_ENCODING exists for the platform.
     if have_unicode:
-        TESTFN_UNICODE=unicode("@test-\xe0\xf2", "latin-1") # 2 latin characters.
-        if os.name=="nt":
-            TESTFN_ENCODING="mbcs"
-else:
-    TESTFN = 'test'
-del os
+        if isinstance('', unicode):
+            # python -U
+            # XXX perhaps unicode() should accept Unicode strings?
+            TESTFN_UNICODE="@test-\xe0\xf2"
+        else:
+            TESTFN_UNICODE=unicode("@test-\xe0\xf2", "latin-1") # 2 latin characters.
+        TESTFN_ENCODING=sys.getfilesystemencoding()
+
+# Make sure we can write to TESTFN, try in /tmp if we can't
+fp = None
+try:
+    fp = open(TESTFN, 'w+')
+except IOError:
+    TMP_TESTFN = os.path.join('/tmp', TESTFN)
+    try:
+        fp = open(TMP_TESTFN, 'w+')
+        TESTFN = TMP_TESTFN
+        del TMP_TESTFN
+    except IOError:
+        print ('WARNING: tests will fail, unable to write to: %s or %s' %
+                (TESTFN, TMP_TESTFN))
+if fp is not None:
+    fp.close()
+    try:
+        os.unlink(TESTFN)
+    except:
+        pass
+del os, fp
 
 from os import unlink
 
 def findfile(file, here=__file__):
+    """Try to find a file on sys.path and the working directory.  If it is not
+    found the argument passed to the function is returned (this does not
+    necessarily signal failure; could still be the legitimate path)."""
     import os
     if os.path.isabs(file):
         return file
@@ -120,6 +180,16 @@ def verify(condition, reason='test failed'):
         raise TestFailed(reason)
 
 def vereq(a, b):
+    """Raise TestFailed if a == b is false.
+
+    This is better than verify(a == b) because, in case of failure, the
+    error message incorporates repr(a) and repr(b) so you can see the
+    inputs.
+
+    Note that "not (a == b)" isn't necessarily the same as "a != b"; the
+    former is tested.
+    """
+
     if not (a == b):
         raise TestFailed, "%r == %r" % (a, b)
 
@@ -177,9 +247,19 @@ def run_suite(suite, testclass=None):
         raise TestFailed(err)
 
 
-def run_unittest(testclass):
-    """Run tests from a unittest.TestCase-derived class."""
-    run_suite(unittest.makeSuite(testclass), testclass)
+def run_unittest(*classes):
+    """Run tests from unittest.TestCase-derived classes."""
+    suite = unittest.TestSuite()
+    for cls in classes:
+        if isinstance(cls, (unittest.TestSuite, unittest.TestCase)):
+            suite.addTest(cls)
+        else:
+            suite.addTest(unittest.makeSuite(cls))
+    if len(classes)==1:
+        testclass = classes[0]
+    else:
+        testclass = None
+    run_suite(suite, testclass)
 
 
 #=======================================================================
@@ -208,9 +288,11 @@ def run_doctest(module, verbosity=None):
         f, t = doctest.testmod(module, verbose=verbosity)
         if f:
             raise TestFailed("%d of %d doctests failed" % (f, t))
-        return f, t
     finally:
         sys.stdout = save_stdout
+    if verbose:
+        print 'doctest (%s) ... %d tests with zero failures' % (module.__name__, t)
+    return f, t
 
 #=======================================================================
 # Old Jython test functions
