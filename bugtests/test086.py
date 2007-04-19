8a4deeb8deef2e3696e8dbb0cee3f3c346f2dbd1
Charlie Groves <charlie.groves@gmail.com>
2007-04-19 04:54:34 +0000

    reduce long result waits

--- bugtests/test086.py
+++ bugtests/test086.py
@@ -10,10 +10,10 @@ class mythread(java.lang.Thread):
     def __init__( self, name="bla" ):
         self.name = name
     def run( self ):
+        print self.name
         pass
 
 mythread().start()
 mythread("pipo 1").start()
 mythread("pipo 2").start()
-java.lang.Thread.sleep(15000)
 
