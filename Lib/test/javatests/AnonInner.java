cb5cf056c748b9b7b5caf55fe87f8435c63ed3d4
Charlie Groves <charlie.groves@gmail.com>
2007-07-15 17:40:58 +0000

    I can't see how a lack of compareTo would upset the compiler and its absence doesn't cause any issues under 1.4 or greater compilers for me, but having it in this form causes the compiler to issue a warning about hiding a bridge method.

--- Lib/test/javatests/AnonInner.java
+++ Lib/test/javatests/AnonInner.java
@@ -9,11 +9,6 @@ public class AnonInner
                 public int hashCode() {
                     return 2000;
                 }
-		//XXX: stuck compareTo to make the compiler happier.
-		//     hopefully this doesn't mess up the test.
-		public int compareTo(Object x) {
-		    return 0;
-		}
             };
         return d.hashCode();
     }
