bc2a944889a0d6402e418e55fc2b6899b69553c9
Charlie Groves <charlie.groves@gmail.com>
2007-08-01 04:55:13 +0000

    Fix the test on both input and output so it attempts to read the amount written from the client on the server, and the amount written on the server from the client instead of vice versa.
    Restructure the tests as a more traditional set of unit tests on client
    writing, server writing and simultaneous writing instead of one long set of
    dependent tests.

--- Lib/test/test_select_new.py
+++ Lib/test/test_select_new.py
@@ -9,8 +9,6 @@ import unittest
 import socket
 import select
 
-NOT_READY, READY = 0, 1
-
 SERVER_ADDRESS = ("localhost", 54321)
 
 DATA_CHUNK_SIZE = 1000
@@ -24,7 +22,7 @@ DATA_CHUNK = "." * DATA_CHUNK_SIZE
 #
 
 if test_support.is_jython:
-    SELECT_TIMEOUT = .2
+    SELECT_TIMEOUT = 0
 else:
     # zero select timeout fails these tests on cpython (on windows 2003 anyway)
     SELECT_TIMEOUT = 0.001
@@ -34,9 +32,6 @@ READ_TIMEOUT = 5
 class AsynchronousServer:
 
     def __init__(self):
-        self.server_socket = None
-
-    def create_socket(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setblocking(0)
         self.server_socket.bind(SERVER_ADDRESS)
@@ -46,30 +41,35 @@ class AsynchronousServer:
         except socket.error:
             pass
 
-    def verify_acceptable_status(self, expected):
-        rfds, wfds, xfds = select.select([self.server_socket], [], [], SELECT_TIMEOUT)
-        if self.server_socket in rfds:
-            actual = READY
-        else:
-            actual = NOT_READY
-        assert actual == expected, \
-            "Server socket should %sbe acceptable" % {NOT_READY:'not ',READY:''}[expected]
+    def select_acceptable(self):
+        return select.select([self.server_socket], [], [], SELECT_TIMEOUT)[0]
 
-    def accept_connection(self):
-        rfds, wfds, xfds = select.select([self.server_socket], [], [], SELECT_TIMEOUT)
-        assert self.server_socket in rfds, "Server socket had no pending connections"
+    def verify_acceptable(self):
+        assert self.select_acceptable(), "Server socket should be acceptable"
+
+    def verify_not_acceptable(self):
+        assert not self.select_acceptable(), "Server socket should not be acceptable"
+
+    def accept(self):
+        self.verify_acceptable()
         new_socket, address = self.server_socket.accept()
         return AsynchronousHandler(new_socket)
 
     def close(self):
         self.server_socket.close()
 
-class PeerImpl:
 
-    def fill_outchannel(self):
+class AsynchronousHandler:
+
+    def __init__(self, new_socket):
+        self.socket = new_socket
+        self.socket.setblocking(0)
+
+    def write(self):
         """
-            This implementation is sub-optimal.
-            It is reliant on how the OS handles the socket buffers.
+        Writes on this channel until select no longer reports it as writable.
+
+        Returns the number of bytes written
         """
         total_bytes = 0
         while 1:
@@ -84,7 +84,13 @@ class PeerImpl:
                     continue
                 raise se
 
-    def read_inchannel(self, expected):
+    def read(self, expected):
+        """
+        Reads expected bytes from this socket
+
+        An Exception is raised if expected bytes aren't read before READ_TIMEOUT
+        is reached.
+        """
         results = ""
         start = time.time()
         while 1:
@@ -122,27 +128,14 @@ class PeerImpl:
         self.verify_writable()
         self.verify_not_readable()
 
-    def fileno(self):
-        return self.socket.fileno()
-
     def close(self):
         self.socket.close()
 
-class AsynchronousHandler(PeerImpl):
-
-    def __init__(self, new_socket):
-        self.socket = new_socket
-        self.socket.setblocking(0)
-
-class AsynchronousClient(PeerImpl):
+class AsynchronousClient(AsynchronousHandler):
 
     def __init__(self):
-        self.socket = None
+        AsynchronousHandler.__init__(self, socket.socket(socket.AF_INET, socket.SOCK_STREAM))
         self.connected = 0
-
-    def create_socket(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setblocking(0)
 
     def start_connect(self):
         result = self.socket.connect_ex(SERVER_ADDRESS)
@@ -155,132 +148,82 @@ class AsynchronousClient(PeerImpl):
         rfds, wfds, xfds = select.select([], [self.socket], [], SELECT_TIMEOUT)
         assert self.socket in wfds, "Client socket incomplete connect"
 
-def log(message):
-    print message
+class TestSelectOnAccept(unittest.TestCase):
+    def setUp(self):
+        self.server = AsynchronousServer()
+        self.client = AsynchronousClient()
+        self.handler = None
+
+    def testSelectOnAccept(self):
+        self.server.verify_not_acceptable()
+        self.client.start_connect()
+        self.server.verify_acceptable()
+        self.handler = self.server.accept()
+        self.client.finish_connect()
+        self.server.verify_not_acceptable()
+
+    def tearDown(self):
+        self.client.close()
+        if self.handler:
+            self.handler.close()
+        self.server.close()
 
 class TestSelect(unittest.TestCase):
+    def setUp(self):
+        self.server = AsynchronousServer()
+        self.client = AsynchronousClient()
+        self.client.start_connect()
+        self.handler = self.server.accept()
+        self.client.finish_connect()
 
-    def test000_CreateSockets(self):
-        # Create the server
-        TestSelect.server_socket = AsynchronousServer()
-        TestSelect.server_socket.create_socket()
+    def tearDown(self):
+        self.client.close()
+        self.handler.close()
+        self.server.close()
 
-        # Create the client
-        TestSelect.client_socket = AsynchronousClient()
-        TestSelect.client_socket.create_socket()
+    def testClientOut(self):
+        self.client.verify_only_writable()
+        self.handler.verify_only_writable()
 
-    def test100_ServerSocketNoPendingConnections(self):
-        # Check the server is not marked "acceptable"
-        TestSelect.server_socket.verify_acceptable_status(NOT_READY)
+        written = self.client.write()
+        self.handler.verify_readable()
 
-    def test110_ServerSocketPendingConnections(self):
-        # Start the client connection process
-        TestSelect.client_socket.start_connect()
-        # Check the server is now acceptable
-        TestSelect.server_socket.verify_acceptable_status(READY)
+        self.handler.read(written/2)
+        self.handler.verify_readable()
 
-    def test120_ServerSocketNoPendingConnection(self):
-        TestSelect.handler_socket = TestSelect.server_socket.accept_connection()
-        TestSelect.server_socket.verify_acceptable_status(NOT_READY)
+        self.handler.read(written/2)
+        self.handler.verify_not_readable()
 
-    def test130_EmptyChannel(self):
-        # Finish the connection
-        TestSelect.client_socket.finish_connect()
+    def testHandlerOut(self):
+        written = self.handler.write()
+        self.client.verify_readable()
 
-    #
-    # Test the client-out -> handler-in channel on its own
-    #
+        self.client.read(written/2)
+        self.client.verify_readable()
 
-    def test200_EmptyChannel(self):
-        # And now test the status of both end of the socket
-        TestSelect.client_socket.verify_only_writable()
-        TestSelect.handler_socket.verify_only_writable()
+        self.client.read(written/2)
+        self.client.verify_not_readable()
 
-    def test210_FullChannel(self):
-        TestSelect.num_bytes_outstanding = TestSelect.client_socket.fill_outchannel()
-        TestSelect.handler_socket.verify_readable()
+    def testBothOut(self):
+        client_written = self.client.write()
+        handler_written = self.handler.write()
+        self.client.verify_readable()
+        self.handler.verify_readable()
 
-    def test220_PartiallyFullChannel(self):
-        # Half empty the channel
-        num_bytes_to_retrieve = TestSelect.num_bytes_outstanding / 2
-        bytes_retrieved = TestSelect.handler_socket.read_inchannel(num_bytes_to_retrieve)
-        TestSelect.num_bytes_outstanding -= len(bytes_retrieved)
-        TestSelect.handler_socket.verify_readable()
+        self.client.read(handler_written/2)
+        self.handler.read(client_written/2)
+        self.client.verify_readable()
+        self.handler.verify_readable()
 
-    def test230_EmptyChannel(self):
-        # Empty the channel
-        bytes_retrieved = TestSelect.handler_socket.read_inchannel(TestSelect.num_bytes_outstanding)
-        TestSelect.handler_socket.verify_not_readable()
-
-    #
-    # Test the handler-out -> client-in channel on its own
-    #
-
-    def test300_EmptyChannel(self):
-        # And now test the status of both end of the socket
-        TestSelect.client_socket.verify_only_writable()
-        TestSelect.handler_socket.verify_only_writable()
-
-    def test310_FullChannel(self):
-        TestSelect.num_bytes_outstanding = TestSelect.handler_socket.fill_outchannel()
-        TestSelect.client_socket.verify_readable()
-
-    def test320_PartiallyFullChannel(self):
-        # Half empty the channel
-        num_bytes_to_retrieve = TestSelect.num_bytes_outstanding / 2
-        bytes_retrieved = TestSelect.client_socket.read_inchannel(num_bytes_to_retrieve)
-        TestSelect.num_bytes_outstanding -= len(bytes_retrieved)
-        TestSelect.client_socket.verify_readable()
-
-    def test330_EmptyChannel(self):
-        # Empty the channel
-        TestSelect.client_socket.read_inchannel(TestSelect.num_bytes_outstanding)
-        TestSelect.client_socket.verify_not_readable()
-
-    #
-    # Test both channels active at the same time
-    #
-
-    def test400_EmptyChannels(self):
-        # And now test the status of both end of the socket
-        TestSelect.client_socket.verify_only_writable()
-        TestSelect.handler_socket.verify_only_writable()
-
-    def test410_FullChannels(self):
-        TestSelect.num_bytes_outstanding_c = TestSelect.client_socket.fill_outchannel()
-        TestSelect.num_bytes_outstanding_h = TestSelect.handler_socket.fill_outchannel()
-        TestSelect.client_socket.verify_readable()
-        TestSelect.handler_socket.verify_readable()
-
-    def test420_PartiallyFullChannels(self):
-        # Half empty the channel
-        num_bytes_to_retrieve_c = TestSelect.num_bytes_outstanding_c / 2
-        num_bytes_to_retrieve_h = TestSelect.num_bytes_outstanding_h / 2
-        bytes_retrieved_c = TestSelect.client_socket.read_inchannel(num_bytes_to_retrieve_c)
-        bytes_retrieved_h = TestSelect.handler_socket.read_inchannel(num_bytes_to_retrieve_h)
-        TestSelect.num_bytes_outstanding_c -= len(bytes_retrieved_c)
-        TestSelect.num_bytes_outstanding_h -= len(bytes_retrieved_h)
-        TestSelect.client_socket.verify_readable()
-        TestSelect.handler_socket.verify_readable()
-
-    def test430_EmptyChannels(self):
-        # Empty the channel
-        TestSelect.client_socket.read_inchannel(TestSelect.num_bytes_outstanding_c)
-        TestSelect.handler_socket.read_inchannel(TestSelect.num_bytes_outstanding_h)
-        TestSelect.client_socket.verify_only_writable()
-        TestSelect.handler_socket.verify_only_writable()
-
-    #
-    # Now close the whole lot down
-    #
-
-    def test99999_CloseSockets(self):
-        TestSelect.client_socket.close()
-        TestSelect.handler_socket.close()
-        TestSelect.server_socket.close()
+        self.client.read(handler_written/2)
+        self.handler.read(client_written/2)
+        self.client.verify_only_writable()
+        self.handler.verify_only_writable()
 
 def test_main():
-    test_support.run_unittest(TestSelect)    
+    tests = [TestSelect, TestSelectOnAccept]
+    suites = [unittest.makeSuite(klass, 'test') for klass in tests]
+    test_support.run_suite(unittest.TestSuite(suites))    
 
 if __name__ == "__main__":
     test_main()
