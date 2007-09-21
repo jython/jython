a12f187239e47422a7808ba6cdfa9c7268bb7b8c
Charlie Groves <charlie.groves@gmail.com>
2007-09-21 08:17:32 +0000

    Fix bug #1767194 with the patch attached to it.  This keeps xmllib from incorrectly applying namespaces to attributes

--- Lib/xmllib.py
+++ Lib/xmllib.py
@@ -637,20 +637,17 @@ class XMLParser:
                     aprefix, key = res.group('prefix', 'local')
                     if self.__map_case:
                         key = key.lower()
-                    if aprefix is None:
-                        aprefix = ''
-                    ans = None
-                    for t, d, nst in self.stack:
-                        if d.has_key(aprefix):
-                            ans = d[aprefix]
-                    if ans is None and aprefix != '':
-                        ans = self.__namespaces.get(aprefix)
-                    if ans is not None:
-                        key = ans + ' ' + key
-                    elif aprefix != '':
-                        key = aprefix + ':' + key
-                    elif ns is not None:
-                        key = ns + ' ' + key
+                    if aprefix is not None:
+                        ans = None
+                        for t, d, nst in self.stack:
+                            if d.has_key(aprefix):
+                                ans = d[aprefix]
+                        if ans is None:
+                            ans = self.__namespaces.get(aprefix)
+                        if ans is not None:
+                            key = ans + ' ' + key
+                        else:
+                            key = aprefix + ':' + key
                 nattrdict[key] = val
                 attrnamemap[key] = okey
             attrdict = nattrdict
