jython/jython

commit c46667663f45115e7b1ec27d777e4d034dcc80cc

o add universal newline support (PEP 278) o fix file text mode bugs:   - text mode reading has historically acted like universal mode (with a couple   bugs), it now acts like CPython's (adapting to the platform's newline)   - text mode writing wasn't correctly identifying LF newline platforms   - add test_file_newlines.py for testing text mode newline behavior o cleanup and pull file mode parsing out into its own sanitizeMode method o remove the test_uu workaround for lack of universal newline support and test_univnewlines from expected failures fixes #1755361
git-svn-id: https://jython.svn.sourceforge.net/svnroot/jython/trunk/jython@3494 79b11f02-f210-0410-99aa-fb189b3d1ac4

src/org/python/core/PyFile.java
@@ -11,6 +11,8 @@ import java.io.PushbackInputStream;
 import java.io.RandomAccessFile;
 import java.io.Writer;
 
+import java.util.EnumSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 // To do:
@@ -36,6 +38,7 @@ public class PyFile extends PyObject
         dict.__setitem__("mode",new PyGetSetDescr("mode",PyFile.class,"getMode",null,null));
         dict.__setitem__("name",new PyGetSetDescr("name",PyFile.class,"getName",null,null));
         dict.__setitem__("closed",new PyGetSetDescr("closed",PyFile.class,"getClosed",null,null));
+        dict.__setitem__("newlines",new PyGetSetDescr("newlines",PyFile.class,"getNewlines",null,null));
         class exposed___cmp__ extends PyBuiltinMethodNarrow {
 
             exposed___cmp__(PyObject self,PyBuiltinFunction.Info info) {
@@ -507,6 +510,9 @@ public class PyFile extends PyObject
 
     private FileWrapper file;
 
+    private boolean universal = false;
+    private boolean binary = false;
+
     private Closer closer;
     private static LinkedList closers = new LinkedList();
     static {
@@ -519,7 +525,7 @@ public class PyFile extends PyObject
 
     private static InputStream _pb(InputStream s, String mode)
     {
-        if (mode.indexOf('b') < 0) {
+        if (mode.contains("b")) {
             if (s instanceof PushbackInputStream) {
                 return s;
             }
@@ -546,19 +552,17 @@ public class PyFile extends PyObject
         super(subType);
     }
 
-    public PyFile(FileWrapper file, String name, String mode) {
-        file_init(file, name, mode);
-    }
-    
     private void file_init(FileWrapper file, String name, String mode) {
         file.setMode(mode);
         this.name = name;
         this.mode = mode;
         this.softspace = false;
         this.closed = false;
-        if (mode.indexOf('b') < 0) {
+        if (binary) {
             this.file = new TextWrapper(file);
-        }else{
+        } else if (universal) {
+            this.file = new UniversalWrapper(file);
+        } else {
             this.file = file;
         }
     }
@@ -566,7 +570,7 @@ public class PyFile extends PyObject
     public PyFile(InputStream istream, OutputStream ostream, String name,
                   String mode)
     {
-        this(new IOStreamWrapper(_pb(istream, mode), ostream), name, mode);
+        file_init(new IOStreamWrapper(_pb(istream, mode), ostream), name, mode);
     }
 
     public PyFile(InputStream istream, OutputStream ostream, String name)
@@ -579,7 +583,7 @@ public class PyFile extends PyObject
     }
 
     public PyFile(InputStream istream, String name, String mode) {
-        this(new InputStreamWrapper(_pb(istream, mode)), name, mode);
+        file_init(new InputStreamWrapper(_pb(istream, mode)), name, mode);
     }
 
     public PyFile(InputStream istream, String name) {
@@ -591,7 +595,7 @@ public class PyFile extends PyObject
     }
 
     public PyFile(OutputStream ostream, String name, String mode) {
-        this(new OutputStreamWrapper(ostream), name, mode);
+        file_init(new OutputStreamWrapper(ostream), name, mode);
     }
 
     public PyFile(OutputStream ostream, String name) {
@@ -603,7 +607,7 @@ public class PyFile extends PyObject
     }
 
     public PyFile(Writer ostream, String name, String mode) {
-        this(new WriterWrapper(ostream), name, mode);
+        file_init(new WriterWrapper(ostream), name, mode);
     }
 
     public PyFile(Writer ostream, String name) {
@@ -615,7 +619,7 @@ public class PyFile extends PyObject
     }
 
     public PyFile(RandomAccessFile file, String name, String mode) {
-        this(new RFileWrapper(file), name, mode);
+        file_init(new RFileWrapper(file), name, mode);
     }
 
     public PyFile(RandomAccessFile file, String name) {
@@ -627,7 +631,7 @@ public class PyFile extends PyObject
     }
 
     public PyFile(String name, String mode, int bufsize) {
-        this(_setup(name, mode, bufsize), name, mode);
+        file_init(_setup(name, mode, bufsize), name, mode);
     }
 
     public void __setattr__(String name, PyObject value) {
@@ -650,34 +654,14 @@ public class PyFile extends PyObject
         return o;
     }
 
-    private static FileWrapper _setup(String name, String mode, int bufsize) {
-        char c1 = ' ';
-        char c2 = ' ';
-        char c3 = ' ';
-        int n = mode.length();
-        for (int i = 0; i < n; i++) {
-            if ("awrtb+".indexOf(mode.charAt(i)) < 0)
-                throw Py.IOError("Unknown open mode:" + mode);
-        }
-        if (n > 0) {
-            c1 = mode.charAt(0);
-            if (n > 1) {
-                c2 = mode.charAt(1);
-                if (n > 2)
-                    c3 = mode.charAt(2);
-            }
-        }
-        String jmode = "r";
-        if (c1 == 'r') {
-            if (c2 == '+' || c3 == '+') jmode = "rw";
-            else jmode = "r";
-        }
-        else if (c1 == 'w' || c1 == 'a') jmode = "rw";
+    private FileWrapper _setup(String name, String mode, int bufsize) {
+        String jmode = sanitizeMode(mode);
+        char c1 = mode.charAt(0);
         try {
             File f = new File(name);
             if (c1 == 'r') {
                 if (!f.exists()) {
-                    throw new IOException("No such file or directory: " + name);
+                    throw Py.IOError("No such file or directory: '" + name + "'");
                 }
             }
             if (c1 == 'w') {
@@ -696,6 +680,46 @@ public class PyFile extends PyObject
         } catch (IOException e) {
             throw Py.IOError(e);
         }
+    }
+
+    /**
+     * Parse and validate the python file mode, returning a cleaned
+     * file mode suitable for RandomAccessFile
+     *
+     * @param mode a python file mode String
+     * @return a RandomAccessFile mode String
+     */
+    private String sanitizeMode(String mode) {
+        if (mode.length() == 0) {
+            throw Py.IOError("invalid mode: ");
+        }
+
+        String origMode = mode;
+        if (mode.contains("U")) {
+            universal = true;
+            mode = mode.replace("U", "");
+            if (mode.length() == 0) {
+                mode = "r";
+            } else if ("wa+".indexOf(mode.charAt(0)) > -1) {
+                throw Py.ValueError("universal newline mode can only be used with " +
+                                    "modes starting with 'r'");
+            }
+        }
+        if ("rwa".indexOf(mode.charAt(0)) == -1) {
+            throw Py.ValueError("mode string must begin with one of 'r', 'w', 'a' or " +
+                                "'U', not '" + origMode + "'");
+        }
+
+        binary = mode.indexOf('b') > -1;
+
+        String cleanMode;
+        if ("wa".indexOf(mode.charAt(0)) > -1 || (mode.length() > 1 &&
+                                                  mode.charAt(1) == '+')) {
+            cleanMode = "rw";
+        } else {
+            cleanMode = "r";
+        }
+        return cleanMode;
     }
 
     final String file_read(int n) {
@@ -1007,6 +1031,29 @@ public class PyFile extends PyObject
 
     public boolean getClosed() {
         return closed;
+    }
+
+    public PyObject getNewlines() {
+        if (!universal) {
+            return Py.None;
+        }
+        EnumSet newlineTypes = ((UniversalWrapper)file).getNewlineTypes();
+        int size = newlineTypes.size();
+        if (size == 0) {
+            return Py.None;
+        } else if (size == 1) {
+            String newline = ((Newline)newlineTypes.iterator().next()).getValue();
+            return new PyString(newline);
+        }
+
+        PyObject[] newlines = new PyObject[size];
+        int i = 0;
+        for (Iterator newlineIter = newlineTypes.iterator(); newlineIter.hasNext();) {
+            String newline = ((Newline)newlineIter.next()).getValue();
+            newlines[i] = new PyString(newline);
+            i++;
+        }
+        return new PyTuple(newlines);
     }
 
     protected void finalize() throws Throwable {
@@ -1499,10 +1546,14 @@ public class PyFile extends PyObject
         public TextWrapper(FileWrapper file) {
             this.file = file;
             sep = System.getProperty("line.separator");
-            sep_is_nl = (sep == "\n");
+            sep_is_nl = sep.equals("\n");
         }
 
         public String read(int n) throws IOException {
+            if (sep_is_nl) {
+                return this.file.read(n);
+            }
+            // Convert CRLF to LF
             String s = this.file.read(n);
             int index = s.indexOf('\r');
             if (index < 0)
@@ -1512,10 +1563,13 @@ public class PyFile extends PyObject
             int end = s.length();
             do {
                 buf.append(s.substring(start, index));
-                buf.append('\n');
-                start = index + 1;
-                if (start < end && s.charAt(start) == '\n')
-                    start++;
+                if (index < end - 1 && s.charAt(index + 1) == '\n') {
+                    buf.append('\n');
+                    start = index + 2;
+                } else {
+                    buf.append('\r');
+                    start = index + 1;
+                }
                 index = s.indexOf('\r', start);
             } while (index >= 0);
             buf.append(s.substring(start));
@@ -1523,18 +1577,23 @@ public class PyFile extends PyObject
                 int c = file.read();
                 if (c != -1 && c != '\n')
                     file.unread(c);
+                else if (c == '\n')
+                    buf.setCharAt(buf.length() - 1, '\n');
             }
             return buf.toString();
         }
 
         public int read() throws IOException {
             int c = file.read();
-            if (c != '\r')
+            if (sep_is_nl || c != '\r' || file.available() == 0) {
                 return c;
-            if (file.available() > 0) {
-                c = file.read();
-                if (c != -1 && c != '\n')
-                    file.unread(c);
+            }
+            c = file.read();
+            if (c == -1) {
+                return '\r';
+            } else if (c != '\n') {
+                file.unread(c);
+                return '\r';
             }
             return '\n';
         }
@@ -1582,6 +1641,122 @@ public class PyFile extends PyObject
             return file.__tojava__(cls);
         }
     }
+
+    private static class UniversalWrapper extends FileWrapper {
+        /** The wrapped FileWrapper */
+        private FileWrapper file;
+
+        /** Whether the next character, if it's a LF, should be
+         * skipped (the previous character was a CR) */
+        private boolean skipNextLF = false;
+
+        /** The Newlines encountered in the current file */
+        private EnumSet newlineTypes = EnumSet.noneOf(Newline.class);
+
+        public UniversalWrapper(FileWrapper file) {
+            this.file = file;
+        }
+
+        public String read(int n) throws IOException {
+            boolean all = n < 0;
+            StringBuffer sb = new StringBuffer(all ? 8192 : n);
+            while (all || n-- > 0) {
+                int next = read();
+                if (next == -1) {
+                    break;
+                }
+                sb.append((char)next);
+            }
+            return sb.toString();
+        }
+
+        public int read() throws IOException {
+            int c = file.read();
+            switch (c) {
+            case '\r':
+                skipNextLF = true;
+                c = '\n';
+                break;
+            case '\n':
+                if (skipNextLF) {
+                    skipNextLF = false;
+                    newlineTypes.add(Newline.CRLF);
+                    c = read();
+                    break;
+                }
+                newlineTypes.add(Newline.LF);
+                break;
+            default:
+                if (skipNextLF) {
+                    skipNextLF = false;
+                    newlineTypes.add(Newline.CR);
+                }
+            }
+            return c;
+        }
+
+        public void write(String s) throws IOException {
+            file.write(s);
+        }
+
+        public long tell() throws IOException {
+            long pos = file.tell();
+            if (skipNextLF) {
+                // Attempt to consume the next LF so the file position
+                // begins on the actual next line
+                int next = file.read();
+                if ((char)next == '\n') {
+                    pos++;
+                    skipNextLF = false;
+                    newlineTypes.add(Newline.CRLF);
+                } else if (next != -1) {
+                    // Not a CRLF; rewind unless EOF
+                    file.seek(file.tell() -1, 0);
+                }
+            }
+            return pos;
+        }
+
+        public void seek(long pos, int how) throws IOException {
+            file.seek(pos, how);
+        }
+
+        public void flush() throws IOException {
+            file.flush();
+        }
+
+        public void close() throws IOException {
+            file.close();
+        }
+
+        public void truncate(long position) throws IOException {
+            file.truncate(position);
+        }
+
+        public Object __tojava__(Class cls) throws IOException {
+            return file.__tojava__(cls);
+        }
+
+        public EnumSet getNewlineTypes() {
+            return newlineTypes;
+        }
+    }
+
+    /**
+     * Newline types
+     */
+    public enum Newline {
+        CR ("\r"),
+        LF ("\n"),
+        CRLF ("\r\n");
+
+        private final String value;
+        public String getValue() { return value; }
+
+        Newline(String value) {
+            this.value = value;
+        }
+    };
 
     /**
      * A mechanism to make sure PyFiles are closed on exit. On
@@ -1645,4 +1820,5 @@ public class PyFile extends PyObject
             }
         }
     }
+
 }

Lib/test/regrtest.py
@@ -1014,7 +1014,6 @@ _skips = {
         test_timing
         test_unicode_file
         test_unicodedata
-        test_univnewlines
         test_urllibnet
         test_wave
         test_weakref

Lib/test/test_file_newlines.py
@@ -1,0 +1,176 @@
+"""Test handling of newlines via file's read and readline
+
+Made for Jython.
+"""
+import os
+import tempfile
+import test.test_support as test_support
+import unittest
+
+assert not os.linesep == '\r', ('os.linesep of  %r is not supported' %
+                                os.linesep)
+
+LF = os.linesep == '\n'
+CRLF = os.linesep == '\r\n'
+
+CRLF_TEST = 'CR\rLF\nCRLF\r\nEOF'
+
+class BaseTestCase(unittest.TestCase):
+
+    data = CRLF_TEST
+    write_mode = 'wb'
+    mode = 'r'
+
+    def setUp(self):
+        self.filename = tempfile.mktemp()
+        fp = open(self.filename, self.write_mode)
+        fp.write(self.data)
+        fp.close()
+        self.fp = open(self.filename, self.mode)
+
+    def tearDown(self):
+        if self.fp:
+            self.fp.close()
+        os.remove(self.filename)
+
+
+class BinaryNewlinesTestCase(BaseTestCase):
+
+    mode = 'rb'
+
+    def test_binary_read(self):
+        read(self.fp, CRLF_TEST)
+
+        self.fp.seek(0)
+        read(self.fp, CRLF_TEST, len(CRLF_TEST))
+
+    def test_binary_readline(self):
+        readline(self.fp, 'CR\rLF\n')
+        readline(self.fp, 'CRLF\r\n')
+        readline(self.fp, 'EOF')
+
+
+class ReadTextNewlinesTestCase(BaseTestCase):
+
+    def test_text_read(self):
+        if LF:
+            read(self.fp, 'CR\rLF\nCRLF\r\nEOF')
+        elif CRLF:
+            read(self.fp, 'CR\rLF\nCRLF\nEOF')
+
+        self.fp.seek(0)
+        read(self.fp, 'CR\r', 3)
+        read(self.fp, 'LF\n', 3)
+        if LF:
+            read(self.fp, 'CRLF\r\n', 6)
+        elif CRLF:
+            read(self.fp, 'CRLF\n', 5)
+        read(self.fp, 'EOF', 3)
+
+    def _test_text_readline(self):
+        readline(self.fp, 'CR\rLF\n')
+        if LF:
+            readline(self.fp, 'CRLF\r\n')
+        elif CRLF:
+            readline(self.fp, 'CRLF\n')
+        readline(self.fp, 'EOF')
+
+        self.fp.seek(0)
+        readline(self.fp, 'CR\rLF\n')
+        if LF:
+            readline(self.fp, 'CRLF\r', 5)
+            readline(self.fp, '\n')
+        elif CRLF:
+            readline(self.fp, 'CRLF\n', 5)
+            readline(self.fp, 'EOF')
+
+
+class ReadTextBoundaryTestCase(BaseTestCase):
+
+    data = 'CR\r'
+
+    def test_read_boundary(self):
+        read(self.fp, 'CR\r')
+        self.fp.seek(0)
+        read(self.fp, 'CR\r', 3)
+
+    def test_readline_boundary(self):
+        readline(self.fp, 'CR\r')
+        self.fp.seek(0)
+        readline(self.fp, 'CR\r', 3)
+
+
+class WriteTextNewlinesTestCase(BaseTestCase):
+
+    write_mode = 'w'
+    mode = 'rb'
+
+    def test_text_written(self):
+        if LF:
+            readline(self.fp, 'CR\rLF\n')
+            readline(self.fp, 'CRLF\r\n')
+        elif CRLF:
+            readline(self.fp, 'CR\rLF\r\n')
+            readline(self.fp, 'CRLF\r\r\n')
+        readline(self.fp, 'EOF')
+
+
+class ReadUniversalNewlinesTestCase(BaseTestCase):
+
+    mode = 'rU'
+
+    def test_read(self):
+        read(self.fp, 'CR\nLF\nCRLF\nEOF')
+
+        self.fp.seek(0)
+        read(self.fp, 'CR\nLF\nCRLF\nEOF', 14)
+
+    def test_readline(self):
+        readline(self.fp, 'CR\n')
+        assert self.fp.newlines == None, repr(self.fp.newlines)
+        readline(self.fp, 'LF\n')
+        assert self.fp.newlines == ('\r', '\n'), repr(self.fp.newlines)
+        readline(self.fp, 'CRLF\n')
+        assert self.fp.newlines == ('\r', '\n'), repr(self.fp.newlines)
+        readline(self.fp, 'EOF')
+        assert self.fp.newlines == ('\r', '\n', '\r\n'), repr(self.fp.newlines)
+
+        self.fp.seek(0)
+        readline(self.fp, 'CR\n', 3)
+        readline(self.fp, 'LF\n', 3)
+        readline(self.fp, 'CRLF\n', 5)
+        readline(self.fp, 'EOF', 3)
+
+
+class WriteUniversalNewlinesTestCase(unittest.TestCase):
+
+    def test_fails(self):
+        try:
+            open(tempfile.mktemp(), 'wU')
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("file mode 'wU' did not raise a "
+                                 "ValueError")
+    
+
+def read(fp, data, size=-1):
+    line = fp.read(size)
+    assert line == data, 'read: %r expected: %r' % (line, data)
+
+
+def readline(fp, data, size=-1):
+    line = fp.readline(size)
+    assert line == data, 'readline: %r expected: %r' % (line, data)
+
+
+def test_main():
+    test_support.run_unittest(BinaryNewlinesTestCase,
+                              ReadTextNewlinesTestCase,
+                              ReadTextBoundaryTestCase,
+                              WriteTextNewlinesTestCase,
+                              ReadUniversalNewlinesTestCase,
+                              WriteUniversalNewlinesTestCase)
+
+if __name__ == '__main__':
+    test_main()

Lib/test/test_uu.py
@@ -1,173 +1,0 @@
-"""
-Tests for uu module.
-Nick Mathewson
-"""
-
-import unittest
-from test import test_support
-
-import sys, os, uu, cStringIO
-import uu
-from StringIO import StringIO
-
-plaintext = "The smooth-scaled python crept over the sleeping dog\n"
-
-encodedtext = """\
-M5&AE('-M;V]T:\"US8V%L960@<'ET:&]N(&-R97!T(&]V97(@=&AE('-L965P
-(:6YG(&1O9PH """
-
-encodedtextwrapped = "begin %03o %s\n" + encodedtext.replace("%", "%%") + "\n \nend\n"
-
-class UUTest(unittest.TestCase):
-
-    def test_encode(self):
-        inp = cStringIO.StringIO(plaintext)
-        out = cStringIO.StringIO()
-        uu.encode(inp, out, "t1")
-        self.assertEqual(out.getvalue(), encodedtextwrapped % (0666, "t1"))
-        inp = cStringIO.StringIO(plaintext)
-        out = cStringIO.StringIO()
-        uu.encode(inp, out, "t1", 0644)
-        self.assertEqual(out.getvalue(), encodedtextwrapped % (0644, "t1"))
-
-    def test_decode(self):
-        inp = cStringIO.StringIO(encodedtextwrapped % (0666, "t1"))
-        out = cStringIO.StringIO()
-        uu.decode(inp, out)
-        self.assertEqual(out.getvalue(), plaintext)
-        inp = cStringIO.StringIO(
-            "UUencoded files may contain many lines,\n" +
-            "even some that have 'begin' in them.\n" +
-            encodedtextwrapped % (0666, "t1")
-        )
-        out = cStringIO.StringIO()
-        uu.decode(inp, out)
-        self.assertEqual(out.getvalue(), plaintext)
-
-    def test_truncatedinput(self):
-        inp = cStringIO.StringIO("begin 644 t1\n" + encodedtext)
-        out = cStringIO.StringIO()
-        try:
-            uu.decode(inp, out)
-            self.fail("No exception thrown")
-        except uu.Error, e:
-            self.assertEqual(str(e), "Truncated input file")
-
-    def test_missingbegin(self):
-        inp = cStringIO.StringIO("")
-        out = cStringIO.StringIO()
-        try:
-            uu.decode(inp, out)
-            self.fail("No exception thrown")
-        except uu.Error, e:
-            self.assertEqual(str(e), "No valid begin line found in input file")
-
-class UUStdIOTest(unittest.TestCase):
-
-    def setUp(self):
-        self.stdin = sys.stdin
-        self.stdout = sys.stdout
-
-    def tearDown(self):
-        sys.stdin = self.stdin
-        sys.stdout = self.stdout
-
-    def test_encode(self):
-        sys.stdin = cStringIO.StringIO(plaintext)
-        sys.stdout = cStringIO.StringIO()
-        uu.encode("-", "-", "t1", 0666)
-        self.assertEqual(
-            sys.stdout.getvalue(),
-            encodedtextwrapped % (0666, "t1")
-        )
-
-    def test_decode(self):
-        sys.stdin = cStringIO.StringIO(encodedtextwrapped % (0666, "t1"))
-        sys.stdout = cStringIO.StringIO()
-        uu.decode("-", "-")
-        self.assertEqual(sys.stdout.getvalue(), plaintext)
-
-class UUFileTest(unittest.TestCase):
-
-    def _kill(self, f):
-        # close and remove file
-        try:
-            f.close()
-        except (SystemExit, KeyboardInterrupt):
-            raise
-        except:
-            pass
-        try:
-            os.unlink(f.name)
-        except (SystemExit, KeyboardInterrupt):
-            raise
-        except:
-            pass
-
-    def setUp(self):
-        self.tmpin  = test_support.TESTFN + "i"
-        self.tmpout = test_support.TESTFN + "o"
-
-    def tearDown(self):
-        del self.tmpin
-        del self.tmpout
-
-    def test_encode(self):
-        try:
-            fin = open(self.tmpin, 'wb')
-            fin.write(plaintext)
-            fin.close()
-
-            fin = open(self.tmpin, 'rb')
-            fout = open(self.tmpout, 'w')
-            uu.encode(fin, fout, self.tmpin, mode=0644)
-            fin.close()
-            fout.close()
-
-            fout = open(self.tmpout, 'r')
-            s = fout.read()
-            fout.close()
-            self.assertEqual(s, encodedtextwrapped % (0644, self.tmpin))
-        finally:
-            self._kill(fin)
-            self._kill(fout)
-
-    def test_decode(self):
-        try:
-            f = open(self.tmpin, 'wb')
-            f.write(encodedtextwrapped % (0644, self.tmpout))
-            f.close()
-
-            f = open(self.tmpin, 'rb')
-            uu.decode(f)
-            f.close()
-            #This was using rU but Jython lacks universal newline support
-            #See http://jython.org/bugs/1755361
-            f = open(self.tmpout, 'r')
-            s = f.read()
-            f.close()
-            self.assertEqual(s, plaintext)
-            # XXX is there an xp way to verify the mode?
-        finally:
-            self._kill(f)
-
-    def test_decodetwice(self):
-        # Verify that decode() will refuse to overwrite an existing file
-        try:
-            f = cStringIO.StringIO(encodedtextwrapped % (0644, self.tmpout))
-
-            f = open(self.tmpin, 'rb')
-            uu.decode(f)
-            f.close()
-
-            f = open(self.tmpin, 'rb')
-            self.assertRaises(uu.Error, uu.decode, f)
-            f.close()
-        finally:
-            self._kill(f)
-
-def test_main():
-    test_support.run_unittest(UUTest, UUStdIOTest, UUFileTest)
-
-if __name__=="__main__":
-    test_main()

src/templates/file.expose
@@ -6,6 +6,7 @@ type_base_class: PyObject
 expose_getset: mode getMode
 expose_getset: name getName
 expose_getset: closed getClosed
+expose_getset: newlines getNewlines
 # exposed methods
 expose_vanilla_cmp
 expose_meth: __iter__

commit 416a7f97eba5c4e2ddf69ee83cd4bc64d43d34a4

fix file mode parsing bug introduced in r3494: identify '+' when it's the third character in the mode string. cleanup

src/org/python/core/PyFile.java
@@ -698,16 +698,15 @@ public class PyFile extends PyObject
                                 "'U', not '" + origMode + "'");
         }
 
-        binary = mode.indexOf('b') > -1;
+        binary = mode.contains("b");
 
-        String cleanMode;
-        if ("wa".indexOf(mode.charAt(0)) > -1 || (mode.length() > 1 &&
-                                                  mode.charAt(1) == '+')) {
-            cleanMode = "rw";
+        int maxSearch = mode.length() < 3 ? mode.length() : 3;
+        if ("wa".indexOf(mode.charAt(0)) > -1 ||
+            mode.substring(0, maxSearch).contains("+")) {
+            return "rw";
         } else {
-            cleanMode = "r";
+            return "r";
         }
-        return cleanMode;
     }
 
     final String file_read(int n) {