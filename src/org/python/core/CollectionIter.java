f9ca59336e4a4eb3f5039042f42c9aeb3035b049
Charlie Groves <charlie.groves@gmail.com>
2007-09-22 22:33:59 +0000

    Have IteratorIter and EnumerationIter extend PyIterator so they pick up an __iter__ method and can be used by the iter builtin.  Rip out the reflection lookup of Iterable since we're on Java 5 and check on that instead of Collection and Vector since both of those implement Iterable

--- src/org/python/core/CollectionIter.java
+++ src/org/python/core/CollectionIter.java
@@ -10,31 +10,16 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Vector;
 
-class CollectionIter extends PyObject {
+class CollectionIter{
     PyObject findCollection(Object object) {
         if (object instanceof Map) {
             return new IteratorIter(((Map) object).keySet().iterator());
         }
-        if (object instanceof Collection) {
-            return new IteratorIter(((Collection) object).iterator());
+        if (object instanceof Iterable) {
+            return new IteratorIter(((Iterable)object).iterator());
         }
         if (object instanceof Iterator) {
             return new IteratorIter(((Iterator) object));
-        }
-        try {
-            // TODO - Once we depend on Java 5 we can replace this with a check
-            // for the Iterable interface
-            Method m = object.getClass().getMethod("iterator", new Class[0]);
-            if (Iterator.class.isAssignableFrom(m.getReturnType())) {
-                return new IteratorIter((Iterator) m.invoke(object,
-                        new Object[0]));
-            }
-        } catch (Exception e) {
-            // Looks like one of the many reflection based exceptions ocurred so
-            // we won't get an Iterator this way
-        }
-        if (object instanceof Vector) {
-            return new EnumerationIter(((Vector) object).elements());
         }
         if (object instanceof Enumeration) {
             return new EnumerationIter(((Enumeration) object));
@@ -45,18 +30,10 @@ class CollectionIter extends PyObject {
 
         return null;
     }
-
-    public PyObject next() {
-        PyObject ret = __iternext__();
-        if (ret == null) {
-            throw Py.StopIteration(null);
-        }
-        return ret;
-    }
-
+    
 }
 
-class EnumerationIter extends CollectionIter {
+class EnumerationIter extends PyIterator {
     private Enumeration proxy;
 
     public EnumerationIter(Enumeration proxy) {
@@ -70,7 +47,7 @@ class EnumerationIter extends CollectionIter {
     }
 }
 
-class IteratorIter extends CollectionIter {
+class IteratorIter extends PyIterator {
     private Iterator proxy;
 
     public IteratorIter(Iterator proxy) {
