01cb1d52b477f0fc52f6613d2dc1a26285709e87
Charlie Groves <charlie.groves@gmail.com>
2007-11-26 19:59:12 +0000

    Move common functionality from PyFieldDescr and PyGetSetDescr into PyDataDescr

---
+++ src/org/python/core/PyDataDescr.java
@@ -0,0 +1,87 @@
+package org.python.core;
+
+/**
+ * Implements type checking and retury type coercion for a data descriptor. A
+ * subclass must at least implement invokeGet which is called in __get__
+ * operations. If the descriptor supports setting and deleting, it must also
+ * override invokeSet and invokeDel respectively. When implementing those
+ * methods, their respective implementsDescr* methods should be overriden to
+ * return true well.
+ */
+public class PyDataDescr extends PyDescriptor {
+
+    /**
+     * @param onType -
+     *            the type the descriptor belongs to
+     * @param name -
+     *            the name of the descriptor on descriptor type
+     * @param ofType -
+     *            the type returned by the descriptor
+     */
+    public PyDataDescr(PyType onType, String name, Class ofType) {
+        this.dtype = onType;
+        this.name = name;
+        this.ofType = ofType;
+    }
+
+    @Override
+    public PyObject __get__(PyObject obj, PyObject type) {
+        if(obj != null) {
+            PyType objtype = obj.getType();
+            if(objtype != dtype && !objtype.isSubType(dtype))
+                throw get_wrongtype(objtype);
+            Object v = invokeGet(obj);
+            if(v == null) {
+                obj.noAttributeError(name);
+            }
+            return Py.java2py(v);
+        }
+        return this;
+    }
+
+    public Object invokeGet(PyObject obj) {
+        throw new UnsupportedOperationException("Must be overriden by a subclass");
+    }
+
+    @Override
+    public void __set__(PyObject obj, PyObject value) {
+        PyType objtype = obj.getType();
+        if(objtype != dtype && !objtype.isSubType(dtype))
+            throw get_wrongtype(objtype);
+        Object converted = value.__tojava__(ofType);
+        if(converted == Py.NoConversion) {
+            throw Py.TypeError(""); // xxx
+        }
+        invokeSet(obj, converted);
+    }
+
+    public void invokeSet(PyObject obj, Object converted) {
+        throw new UnsupportedOperationException("Must be overriden by a subclass");
+    }
+
+    @Override
+    public void __delete__(PyObject obj) {
+        if(obj != null) {
+            PyType objtype = obj.getType();
+            if(objtype != dtype && !objtype.isSubType(dtype))
+                throw get_wrongtype(objtype);
+            invokeDel(obj);
+        }
+    }
+
+    public void invokeDel(PyObject obj) {
+        throw new UnsupportedOperationException("Must be overriden by a subclass");
+    }
+
+    @Override
+    public boolean isDataDescr() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "<member '" + name + "' of '" + dtype.fastGetName() + "' objects>";
+    }
+
+    protected Class ofType;
+}
--- src/org/python/core/PyFieldDescr.java
+++ src/org/python/core/PyFieldDescr.java
@@ -3,94 +3,58 @@ package org.python.core;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
-public class PyFieldDescr extends PyDescriptor {
-
-    private Field field;
-    private Class field_type;
-    private boolean readonly;
+public class PyFieldDescr extends PyDataDescr {
 
     public PyFieldDescr(String name, Class c, String field_name) {
         this(name, c, field_name, false);
     }
 
-    public PyFieldDescr(
-        String name,
-        Class c,
-        String field_name,
-        boolean readonly) {
-        this.name = name;
-        this.dtype = PyType.fromClass(c);
+    public PyFieldDescr(String name, Class c, String field_name, boolean readonly) {
+        super(PyType.fromClass(c), name, get(c, field_name).getType());
+        field = get(c, field_name);
+        this.readonly = readonly || Modifier.isFinal(field.getModifiers());
+    }
+
+    private static Field get(Class onClass, String name) {
+        Field f;
         try {
-            field = c.getField(field_name);
-        } catch (NoSuchFieldException e) {
+            f = onClass.getField(name);
+        } catch(NoSuchFieldException e) {
             throw Py.SystemError("bogus attribute spec");
         }
-        int modifiers = field.getModifiers();
-        if (Modifier.isStatic(modifiers)) {
+        if(Modifier.isStatic(f.getModifiers())) {
             throw Py.SystemError("static attributes not supported");
         }
-        this.readonly = readonly || Modifier.isFinal(modifiers);
-        field_type = field.getType();
-
+        return f;
     }
 
-    public String toString() {
-        return "<member '" + name + "' of '"+dtype.fastGetName()+"' objects>";
-    }
-
-    /**
-     * @see org.python.core.PyObject#__get__(org.python.core.PyObject, org.python.core.PyObject)
-     */
-    public PyObject __get__(PyObject obj, PyObject type) {
+    @Override
+    public Object invokeGet(PyObject obj) {
         try {
-            if (obj != null) {
-                PyType objtype = obj.getType();
-                if (objtype != dtype && !objtype.isSubType(dtype))
-                    throw get_wrongtype(objtype);
-                return Py.java2py(field.get(obj));
-            }
-            return this;
-        } catch (IllegalArgumentException e) {
+            return field.get(obj);
+        } catch(IllegalArgumentException e) {
             throw Py.JavaError(e);
-
-        } catch (IllegalAccessException e) {
-            throw Py.JavaError(e); // unexpected
+        } catch(IllegalAccessException e) {
+            throw Py.JavaError(e);
         }
     }
 
-    /**
-     * @see org.python.core.PyObject#__set__(org.python.core.PyObject, org.python.core.PyObject)
-     */
-    public void __set__(PyObject obj, PyObject value) {
+    @Override
+    public void invokeSet(PyObject obj, Object converted) {
         try {
-            // obj != null
-            PyType objtype = obj.getType();
-            if (objtype != dtype && !objtype.isSubType(dtype))
-                throw get_wrongtype(objtype);
-            Object converted = value.__tojava__(field_type);
-            if (converted == Py.NoConversion) {
-                throw Py.TypeError(""); // xxx
-            }
             field.set(obj, converted);
-        } catch (IllegalArgumentException e) {
+        } catch(IllegalArgumentException e) {
             throw Py.JavaError(e);
-        } catch (IllegalAccessException e) {
-            throw Py.JavaError(e); // unexpected
+        } catch(IllegalAccessException e) {
+            throw Py.JavaError(e);
         }
     }
 
-    /**
-     * @see org.python.core.PyObject#implementsDescrSet()
-     */
     public boolean implementsDescrSet() {
         return !readonly;
     }
 
-    /**
-     * @see org.python.core.PyObject#isDataDescr()
-     */
-    public boolean isDataDescr() {
-        return true;
-    }
+    private Field field;
 
+    private boolean readonly;
 }
