76878fd45a1f8aa3072094cd403d514888f5ee7f
Philip Jenvey <pjenvey@underboss.org>
2007-12-17 00:28:36 +0000

    woops, revert r3823 as it breaks tracebacks. this will need more work

--- src/org/python/core/imp.java
+++ src/org/python/core/imp.java
@@ -468,7 +468,7 @@ public class imp {
                     PyObject ret = createFromPyClass(modName,
                                                      makeStream(compiledFile),
                                                      true,
-                                                     compiledFile.getAbsolutePath());
+                                                     sourceFile.getAbsolutePath());
                     if(ret != null) {
                         return ret;
                     }
