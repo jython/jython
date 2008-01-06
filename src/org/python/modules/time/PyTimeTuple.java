261a7d6632fb46be6b1470f90dd0e431aefdd78f
Charlie Groves <charlie.groves@gmail.com>
2008-01-06 22:29:09 +0000

    This commit was really to change struct_time_new from private to package protected since @Exposed doesn't do the necessary contortions to make private methods accessible to inner classes yet.  However, pjenvey beat me to that, so just kill the PyTuple specific constructor, and use the results of eq to implement ne to give myself a feeling of accomplishment.

--- src/org/python/modules/time/PyTimeTuple.java
+++ src/org/python/modules/time/PyTimeTuple.java
@@ -22,32 +22,9 @@ import org.python.expose.MethodType;
  */
 @ExposedType(name = "struct_time")
 public class PyTimeTuple extends PyTuple {
-    @ExposedGet
-    public PyInteger tm_year;
 
     @ExposedGet
-    public PyInteger tm_mon;
-
-    @ExposedGet
-    public PyInteger tm_mday;
-
-    @ExposedGet
-    public PyInteger tm_hour;
-
-    @ExposedGet
-    public PyInteger tm_min;
-
-    @ExposedGet
-    public PyInteger tm_sec;
-
-    @ExposedGet
-    public PyInteger tm_wday;
-
-    @ExposedGet
-    public PyInteger tm_yday;
-
-    @ExposedGet
-    public PyInteger tm_isdst;
+    public PyInteger tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst;
 
     public static final PyType TYPE = PyType.fromClass(PyTimeTuple.class);
 
@@ -64,39 +41,20 @@ public class PyTimeTuple extends PyTuple {
         tm_isdst = (PyInteger)vals[8];
     }
 
-    PyTimeTuple(PyTuple vals) {
-        super(TYPE, new PyObject[] {
-                vals.pyget(0),
-                vals.pyget(1),
-                vals.pyget(2),
-                vals.pyget(3),
-                vals.pyget(4),
-                vals.pyget(5),
-                vals.pyget(6),
-                vals.pyget(7),
-                vals.pyget(8)
-            });
-        tm_year = (PyInteger)vals.pyget(0);
-        tm_mon = (PyInteger)vals.pyget(1);
-        tm_mday = (PyInteger)vals.pyget(2);
-        tm_hour = (PyInteger)vals.pyget(3);
-        tm_min = (PyInteger)vals.pyget(4);
-        tm_sec = (PyInteger)vals.pyget(5);
-        tm_wday = (PyInteger)vals.pyget(6);
-        tm_yday = (PyInteger)vals.pyget(7);
-        tm_isdst = (PyInteger)vals.pyget(8);
-    }
-
     @ExposedNew
-    static PyObject struct_time_new(PyNewWrapper wrapper, boolean init, PyType subtype,
-                                    PyObject[] args, String[] keywords) {
+    static PyObject struct_time_new(PyNewWrapper wrapper,
+                                    boolean init,
+                                    PyType subtype,
+                                    PyObject[] args,
+                                    String[] keywords) {
         ArgParser ap = new ArgParser("struct_time", args, keywords, new String[] {"tuple"}, 1);
         PyObject obj = ap.getPyObject(0);
         if (obj instanceof PyTuple) {
             if (obj.__len__() != 9) {
                 throw Py.TypeError("time.struct_time() takes a 9-sequence (1-sequence given)");
             }
-            return new PyTimeTuple((PyTuple)obj);
+            // tuples are immutable, so we can just use its underlying array
+            return new PyTimeTuple(((PyTuple)obj).getArray());
         } else if (obj instanceof PySequence) {
             PySequence seq = (PySequence)obj;
             if (seq.__len__() != 9) {
@@ -131,16 +89,11 @@ public class PyTimeTuple extends PyTuple {
 
     @ExposedMethod(type = MethodType.BINARY)
     final synchronized PyObject struct_time___ne__(PyObject o) {
-        if (!(getType() == o.getType()) && !(getType().isSubType(o.getType()))) {
+        PyObject eq = struct_time___eq__(o);
+        if(eq == null) {
             return null;
         }
-        int tl = __len__();
-        int ol = o.__len__();
-        if (tl != ol) {
-            return Py.True;
-        }
-        int i = cmp(this, tl, o, ol);
-        return (i < 0) ? Py.False : Py.True;
+        return eq.__not__();
     }
 
     /**
