5f18b43bb16b6c4dbef2df9200f565fe7fbd68c4
Nicholas Riley <njriley@illinois.edu>
2008-03-19 14:21:53 +0000

    Remove -m from usage message because it's not yet supported by Jython.

--- src/org/python/util/jython.java
+++ src/org/python/util/jython.java
@@ -22,7 +22,8 @@ import org.python.core.imp;
 public class jython
 {
     private static String usage =
-        "usage: jython [option] ... [-c cmd | -m mod | file | -] [arg] ...\n" +
+        // "usage: jython [option] ... [-c cmd | -m mod | file | -] [arg] ...\n" +
+        "usage: jython [option] ... [-c cmd | file | -] [arg] ...\n" +
         "Options and arguments:\n" + //(and corresponding environment variables):\n" +
         "-c cmd   : program passed in as string (terminates option list)\n" +
         //"-d       : debug output from parser (also PYTHONDEBUG=x)\n" +
