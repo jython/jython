790537e188e4782818dadbfb06eb00ec85d1ea20
Philip Jenvey <pjenvey@underboss.org>
2008-05-25 23:21:36 +0000

    add CPython 2.3's special case for KeyError__str__ patch from lsoto, fixes #1040

---
+++ Lib/test/test_exceptions_jy.py
@@ -0,0 +1,21 @@
+"""Misc. exception related tests
+
+Made for Jython.
+"""
+import test_support
+import unittest
+
+class ExceptionsTestCase(unittest.TestCase):
+
+    def test_keyerror_str(self):
+        self.assertEquals(str(KeyError()), '')
+        # Is actually repr(args[0])
+        self.assertEquals(str(KeyError('')), "''")
+        self.assertEquals(str(KeyError('', '')), "('', '')")
+
+
+def test_main():
+    test_support.run_unittest(ExceptionsTestCase)
+
+if __name__ == '__main__':
+    test_main()
