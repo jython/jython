7339ec472f7cd7645f0b60d779558ff19733a7b0
Philip Jenvey <pjenvey@underboss.org>
2008-06-13 04:47:04 +0000

    fix test_descr.test_dir dir(None) == dir(Ellipsis)

--- src/org/python/core/PyEllipsis.java
+++ src/org/python/core/PyEllipsis.java
@@ -3,19 +3,22 @@ package org.python.core;
 
 import java.io.Serializable;
 
+import org.python.expose.ExposedType;
+
 /**
- * A class representing the singleton Ellipsis <code>...</code>
- * object.
+ * A class representing the singleton Ellipsis <code>...</code> object.
  */
+// XXX: not subclassable
+@ExposedType(name = "ellipsis", base = PyObject.class)
 public class PyEllipsis extends PySingleton implements Serializable {
+
+    public static final PyType TYPE = PyType.fromClass(PyEllipsis.class);
+
     PyEllipsis() {
         super("Ellipsis");
     }
 
-
     private Object writeReplace() {
         return new Py.SingletonResolver("Ellipsis");
-    } 
-    
-    
+    }
 }
