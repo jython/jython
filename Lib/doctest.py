5e426aed30fba84cde5745ad5a28fe5a70421f8e
Philip Jenvey <pjenvey@underboss.org>
2008-06-25 00:11:28 +0000

    o bring in decimal (test_decimal passes). also hashlib and platform which don't quite work yet o Jython changes for doctest, though test_doctest still fails. bring in trace for test_doctest

--- Lib/doctest.py
+++ Lib/doctest.py
@@ -854,6 +854,12 @@ class DocTestFinder:
         elif inspect.isfunction(object):
             return module.__dict__ is object.func_globals
         elif inspect.isclass(object):
+            # XXX: Jython transition 2.5
+            # Java classes appear as Python classes to inspect, but they
+            # have no __module__ http://jython.org/bugs/1758279
+            # org.python.modules uses Java classes to masq
+            if not hasattr(object, '__module__'):
+                return False
             return module.__name__ == object.__module__
         elif inspect.getmodule(object) is not None:
             return module is inspect.getmodule(object)
@@ -961,6 +967,8 @@ class DocTestFinder:
             filename = getattr(module, '__file__', module.__name__)
             if filename[-4:] in (".pyc", ".pyo"):
                 filename = filename[:-1]
+            elif filename.endswith('$py.class'):
+                filename = '%s.py' % filename[:-9]
         return self._parser.get_doctest(docstring, globs, name,
                                         filename, lineno)
 
@@ -1667,7 +1675,8 @@ class DebugRunner(DocTestRunner):
 
        If a failure or error occurs, the globals are left intact:
 
-         >>> del test.globs['__builtins__']
+         >>> if '__builtins__' in test.globs:
+         ...     del test.globs['__builtins__']
          >>> test.globs
          {'x': 1}
 
@@ -1681,7 +1690,8 @@ class DebugRunner(DocTestRunner):
          ...
          UnexpectedException: <DocTest foo from foo.py:0 (2 examples)>
 
-         >>> del test.globs['__builtins__']
+         >>> if '__builtins__' in test.globs:
+         ...     del test.globs['__builtins__']
          >>> test.globs
          {'x': 2}
 
@@ -2281,6 +2291,8 @@ def DocTestSuite(module=None, globs=None, extraglobs=None, test_finder=None,
             filename = module.__file__
             if filename[-4:] in (".pyc", ".pyo"):
                 filename = filename[:-1]
+            elif filename.endswith('$py.class'):
+                filename = '%s.py' % filename[:-9]
             test.filename = filename
         suite.addTest(DocTestCase(test, **options))
 
