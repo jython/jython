5645001a9ea61af71a18e493f112597d4d4358a9
Jim Baker <jbaker@zyasoft.com>
2008-06-27 00:50:25 +0000

    Removed obsolete test, superseded by 2.5 coercion rules for emptry strings on re.sub

--- Lib/test/test_re_jy.py
+++ Lib/test/test_re_jy.py
@@ -9,16 +9,6 @@ class ReTest(unittest.TestCase):
                           re.match("(?P<int>\d+)\.(\d*)", '3.14').group,
                           "misspelled")
 
-    def test_no_empty_string_no_match(self):
-        # must return pattern[0:0] here
-        result = re.sub('^foo', 'bar', u'')
-        self.assert_(isinstance(result, str))
-        self.assertEqual(result, '')
-
-        result = re.sub(u'^foo', 'bar', '')
-        self.assert_(isinstance(result, unicode))
-        self.assertEqual(result, u'')
-
 def test_main():
         test.test_support.run_unittest(ReTest)
 
