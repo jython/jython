50de7e0f24e136e3a2b5125e89fb0e007defa852
Nicholas Riley <njriley@illinois.edu>
2008-07-18 19:21:37 +0000

    Actually use JYTHON_OPTS; correct error message and properly delete _JYTHON_HOME.

--- src/shell/jython.bat
+++ src/shell/jython.bat
@@ -16,6 +16,11 @@ setlocal enabledelayedexpansion
 
 rem ----- Verify and set required environment variables -----------------------
 
+set _JAVA_CMD=java
+if not "%JAVA_HOME%" == "" (
+   set _JAVA_CMD="%JAVA_HOME%\bin\java"
+)
+
 set _JYTHON_HOME="%JYTHON_HOME%"
 if not "%JYTHON_HOME%" == "" goto gotHome
 pushd "%~dp0%\.."
@@ -23,32 +28,31 @@ set _JYTHON_HOME="%CD%"
 popd
 
 :gotHome
-if exist %_JYTHON_HOME%/jython.jar goto homeOK
-echo Cannot find jython.jar in %_JYTHON_HOME%
-echo Try running this batch file from the 'bin' directory of an installed Jython
-echo or setting JYTHON_HOME.
-goto cleanup
-
-:homeOK
-set _JAVA_CMD=java
-if not "%JAVA_HOME%" == "" (
-   set _JAVA_CMD=%JAVA_HOME%\bin\java
-)
-
-set _CP=%_JYTHON_HOME%\jython-complete.jar
-if not exist %_JYTHON_HOME%\jython.jar goto run
+if not exist %_JYTHON_HOME%\jython.jar goto tryComplete
 rem prefer built version
 set _CP=%_JYTHON_HOME%\jython.jar
 for %%j in (%_JYTHON_HOME%\javalib\*.jar) do (
     set _CP=!_CP!;"%%j"
 )
+goto run
+
+:tryComplete
+set _CP=%_JYTHON_HOME%\jython-complete.jar
+if exist %_JYTHON_HOME%/jython-complete.jar goto run
+
+echo Cannot find jython.jar or jython-complete.jar in %_JYTHON_HOME%
+echo Try running this batch file from the 'bin' directory of an installed Jython
+echo or setting JYTHON_HOME.
+goto cleanup
+
+rem ----- Execute the requested command ----------------------------------------
 
 :run
-%_JAVA_CMD% %JAVA_OPTS% -Xss512k -Xbootclasspath/a:%_CP% -Dpython.home=%_JYTHON_HOME% -Dpython.executable="%~f0" -classpath "%CLASSPATH%" org.python.util.jython %*
+%_JAVA_CMD% %JAVA_OPTS% -Xss512k -Xbootclasspath/a:%_CP% -Dpython.home=%_JYTHON_HOME% -Dpython.executable="%~f0" -classpath "%CLASSPATH%" org.python.util.jython %JYTHON_OPTS% %*
 set E=%ERRORLEVEL%
 
 :cleanup
-set _JYTHON_HOME=%JYTHON_HOME%
+set _JYTHON_HOME=
 set _JAVA_CMD=
 set _CP=
 
