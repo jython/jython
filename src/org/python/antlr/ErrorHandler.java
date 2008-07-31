1102e79b269a47db18d95f8592d0368ccd484a1f
Frank Wierzbicki <fwierzbicki@gmail.com>
2008-07-31 20:50:59 +0000

    Tightened up signature of ErrorHandler and added a ListErrorHandler that mimics Antlr default behavior for error handling.

--- src/org/python/antlr/ErrorHandler.java
+++ src/org/python/antlr/ErrorHandler.java
@@ -2,11 +2,12 @@ package org.python.antlr;
 
 import org.antlr.runtime.BaseRecognizer;
 import org.antlr.runtime.IntStream;
+import org.antlr.runtime.Lexer;
 import org.antlr.runtime.RecognitionException;
 
 interface ErrorHandler {
     void reportError(BaseRecognizer br, RecognitionException re);
-    void recover(BaseRecognizer br, RecognitionException re);
     void recover(BaseRecognizer br, IntStream input, RecognitionException re);
+    void recover(Lexer lex, RecognitionException re);
     boolean isRecoverable();
 }
--- src/org/python/antlr/ListErrorHandler.java
+++ src/org/python/antlr/ListErrorHandler.java
@@ -5,28 +5,23 @@ import org.antlr.runtime.IntStream;
 import org.antlr.runtime.Lexer;
 import org.antlr.runtime.RecognitionException;
 
-public class FailFastHandler implements ErrorHandler {
+public class ListErrorHandler implements ErrorHandler {
     private BaseRecognizer recognizer;
 
     public void reportError(BaseRecognizer br, RecognitionException re) {
-        throw new ParseException(message(br,re), re);
+        br.reportError(re);
     }
 
     public void recover(Lexer lex, RecognitionException re) {
-        throw new ParseException(message(lex,re), re);
+        lex.recover(re);
     }
 
     public void recover(BaseRecognizer br, IntStream input, RecognitionException re) {
-        throw new ParseException(message(br,re), re);
+        br.recover(input, re);
     }
 
     public boolean isRecoverable() {
-        return false;
+        return true;
     }
 
-    private String message(BaseRecognizer br, RecognitionException re) {
-		String hdr = br.getErrorHeader(re);
-		String msg = br.getErrorMessage(re, br.getTokenNames());
-        return hdr+" "+msg;
-    }
 }
