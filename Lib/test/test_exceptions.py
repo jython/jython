fdf083dcc1c4467f0a8496f783582ffab96bab9d
Leo Soto <leo.soto@gmail.com>
2008-09-21 00:20:23 +0000

    importing test_support from the test package on test_exceptions.py

--- Lib/test/test_exceptions.py
+++ Lib/test/test_exceptions.py
@@ -339,7 +339,7 @@ class ExceptionTests(unittest.TestCase):
 
 
 def test_main():
-    import test_support
+    from test import test_support
     if test_support.is_jython:
         # XXX: http://bugs.jython.org/issue1063
         del ExceptionTests.testInfiniteRecursion
