799bab5edbb78014f9d46b900c46ea0fce25031c
Frank Wierzbicki <fwierzbicki@gmail.com>
2008-10-16 14:33:33 +0000

    Basic slice ops that are already well tested.

--- bugtests/test112.py
+++
@@ -1,8 +0,0 @@
-"""
-Check slice [::-1]
-"""
-
-import support
-
-if 'abcdef'[::-1] != 'fedcba':
-    raise support.TestError("slice [::-1] failed")
--- bugtests/test113.py
+++
@@ -1,9 +0,0 @@
-"""
-Check slice [-1:1:-1].
-"""
-
-import support
-
-if 'abcde'[-1:1:-1] != 'edc':
-    raise support.TestError("slice [-1:1:-1] failed %s" % 'abcde'[-1:1:-1])
-
