e290e9f5f9c2a18166be9c0d9b8b08d9f42a3dbe
Charlie Groves <charlie.groves@gmail.com>
2008-10-20 01:01:39 +0000

    Add an ant resource collection that's like union except it combines based on the name of the resource instead of its full path.  Use this to exclude the unexposed version of classes from jython.jar to keep it quiet.
    Resource collections didn't appear until Ant 1.7, but that came out nearly 2
    years ago, so I'm hoping most people already have it.

---
+++ src/org/python/util/NameUnionAntType.java
@@ -0,0 +1,35 @@
+package org.python.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.tools.ant.types.Resource;
+import org.apache.tools.ant.types.ResourceCollection;
+import org.apache.tools.ant.types.resources.BaseResourceCollectionContainer;
+
+/**
+ * Unions several resource collections by the name of their contained resources.
+ */
+public class NameUnionAntType extends BaseResourceCollectionContainer {
+    @SuppressWarnings("unchecked")
+    @Override
+    protected Collection<Resource> getCollection() {
+        List<ResourceCollection> collections = getResourceCollections();
+        // preserve order-encountered using a list; keep track of the items with a set
+        Set<String> seenNames = new HashSet<String>();
+        List<Resource> union = new ArrayList();
+        for (ResourceCollection rc : collections) {
+            for (Iterator<Resource> resources = rc.iterator(); resources.hasNext();) {
+                Resource r = resources.next();
+                if (seenNames.add(r.getName())) {
+                    union.add(r);
+                }
+            }
+        }
+        return union;
+    }
+}
