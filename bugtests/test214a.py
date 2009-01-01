75a90270f0b273bde35ea8b5250de5825da4c5c8
Charlie Groves <charlie.groves@gmail.com>
2009-01-01 01:13:50 +0000

    Should've gone out with r5804

--- bugtests/test214a.py
+++
@@ -1,9 +0,0 @@
-from java.applet import Applet
-
-class test214a(Applet):
-    def paint(self, g):
-        g.drawString("Hello world", 20, 30)
-
-if __name__ == '__main__':
-    import pawt
-    pawt.test(HelloWorld())
--- bugtests/test235p/javaParent.java
+++
@@ -1,6 +0,0 @@
-package test235p;
-public class javaParent {
-    public javaParent(int x) {
-        //System.out.println("constructor "+x);
-    }
-}
