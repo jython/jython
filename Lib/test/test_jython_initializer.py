25261a95af097f5cf6465c8b8f86a7edb698f4ee
Philip Jenvey <pjenvey@underboss.org>
2009-02-03 21:25:23 +0000

    include PATH for platforms lacking bash in /bin

--- Lib/test/test_jython_initializer.py
+++ Lib/test/test_jython_initializer.py
@@ -1,14 +1,16 @@
-import unittest
+import os
 import subprocess
 import sys
+import unittest
 from test import test_support
 
 class TestUsingInitializer(unittest.TestCase):
+
     def test_syspath_initializer(self):
-        fn = test_support.findfile("check_for_initializer_in_syspath.py")
-        ret = subprocess.Popen([sys.executable, fn],
-                env={"CLASSPATH":"tests/data/initializer"}).wait()
-        self.assertEquals(0, ret)
+        fn = test_support.findfile('check_for_initializer_in_syspath.py')
+        env = dict(CLASSPATH='tests/data/initializer',
+                   PATH=os.environ.get('PATH', ''))
+        self.assertEquals(0, subprocess.call([sys.executable, fn], env=env))
 
 def test_main():
     test_support.run_unittest(TestUsingInitializer)
