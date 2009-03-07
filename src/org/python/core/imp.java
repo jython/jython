062938a11fbca345791198439872a3a1be4c272d
Frank Wierzbicki <fwierzbicki@gmail.com>
2009-03-07 04:16:00 +0000

    increment APIVersion, take --verify out of build.xml

--- src/org/python/core/imp.java
+++ src/org/python/core/imp.java
@@ -20,7 +20,7 @@ public class imp {
 
     private static final String UNKNOWN_SOURCEFILE = "<unknown>";
 
-    public static final int APIVersion = 17;
+    public static final int APIVersion = 18;
 
     public static final int NO_MTIME = -1;
 
