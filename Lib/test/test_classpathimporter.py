7c61c5b4aca3caf4f83809275cf964fe1fdd1a19
Philip Jenvey <pjenvey@underboss.org>
2009-03-29 23:21:00 +0000

    fix failing tests: always compile the compiled version, incase the bytecode magic changes

--- Lib/test/test_classpathimporter.py
+++ Lib/test/test_classpathimporter.py
@@ -1,9 +1,15 @@
-import unittest
+import os
+import py_compile
+import shutil
 import sys
+import tempfile
+import unittest
+import zipfile
 from test import test_support
 from java.lang import Thread
 
 class ClasspathImporterTestCase(unittest.TestCase):
+
     def setUp(self):
         self.orig_context = Thread.currentThread().contextClassLoader
         self.orig_path = sys.path
@@ -11,31 +17,6 @@ class ClasspathImporterTestCase(unittest.TestCase):
     def tearDown(self):
         Thread.currentThread().contextClassLoader = self.orig_context
         sys.path = self.orig_path
-        try:
-            del sys.modules['flat_in_jar']
-            del sys.modules['jar_pkg']
-            del sys.modules['jar_pkg.prefer_compiled']
-        except KeyError:
-            pass
-
-    def setClassLoaderAndCheck(self, jar, prefix):
-        Thread.currentThread().contextClassLoader = test_support.make_jar_classloader(jar)
-        import flat_in_jar
-        self.assertEquals(flat_in_jar.value, 7)
-        import jar_pkg
-        self.assertEquals(prefix + '/jar_pkg/__init__$py.class', jar_pkg.__file__)
-        from jar_pkg import prefer_compiled
-        self.assertEquals(prefix + '/jar_pkg/prefer_compiled$py.class', prefer_compiled.__file__)
-        self.assert_(prefer_compiled.compiled)
-        self.assertRaises(NameError, __import__, 'flat_bad')
-        self.assertRaises(NameError, __import__, 'jar_pkg.bad')
-
-    def test_default_pyclasspath(self):
-        self.setClassLoaderAndCheck("classimport.jar", "__pyclasspath__")
-
-    def test_path_in_pyclasspath(self):
-        sys.path = ['__pyclasspath__/Lib']
-        self.setClassLoaderAndCheck("classimport_Lib.jar", "__pyclasspath__/Lib")
 
     # I don't like the checked in jar file bug1239.jar.  The *one* thing I
     # liked about the tests in bugtests/ is that you could create a java file,
@@ -54,8 +35,65 @@ class ClasspathImporterTestCase(unittest.TestCase):
         sys.path.append("Lib/test/bug1126/bug1126.jar")
         import org.subpackage
 
+
+class PyclasspathImporterTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.orig_context = Thread.currentThread().contextClassLoader
+        self.orig_path = sys.path
+        self.temp_dir = tempfile.mkdtemp()
+        self.modules = sys.modules.keys()
+
+    def tearDown(self):
+        Thread.currentThread().contextClassLoader = self.orig_context
+        sys.path = self.orig_path
+        shutil.rmtree(self.temp_dir)
+        for module in sys.modules.keys():
+            if module not in self.modules:
+                del sys.modules[module]
+
+    def setClassLoaderAndCheck(self, orig_jar, prefix, compile_path=''):
+        # Create a new jar and compile prefer_compiled into it
+        orig_jar = test_support.findfile(orig_jar)
+        jar = os.path.join(self.temp_dir, os.path.basename(orig_jar))
+        shutil.copy(orig_jar, jar)
+
+        code = os.path.join(self.temp_dir, 'prefer_compiled.py')
+        fp = open(code, 'w')
+        fp.write('compiled = True')
+        fp.close()
+        py_compile.compile(code)
+        zip = zipfile.ZipFile(jar, 'a')
+        zip.write(os.path.join(self.temp_dir, 'prefer_compiled$py.class'),
+                  os.path.join(compile_path, 'jar_pkg',
+                               'prefer_compiled$py.class'))
+        zip.close()
+        zip = zipfile.ZipFile(jar)
+
+        Thread.currentThread().contextClassLoader = test_support.make_jar_classloader(jar)
+        import flat_in_jar
+        self.assertEquals(flat_in_jar.value, 7)
+        import jar_pkg
+        self.assertEquals(prefix + '/jar_pkg/__init__.py', jar_pkg.__file__)
+        from jar_pkg import prefer_compiled
+        self.assertEquals(prefix + '/jar_pkg/prefer_compiled$py.class', prefer_compiled.__file__)
+        self.assert_(prefer_compiled.compiled)
+        self.assertRaises(NameError, __import__, 'flat_bad')
+        self.assertRaises(NameError, __import__, 'jar_pkg.bad')
+
+    def test_default_pyclasspath(self):
+        self.setClassLoaderAndCheck('classimport.jar', '__pyclasspath__')
+
+    def test_path_in_pyclasspath(self):
+        sys.path = ['__pyclasspath__/Lib']
+        self.setClassLoaderAndCheck('classimport_Lib.jar',
+                                    '__pyclasspath__/Lib', 'Lib')
+
+
 def test_main():
-    test_support.run_unittest(ClasspathImporterTestCase)
+    test_support.run_unittest(ClasspathImporterTestCase,
+                              PyclasspathImporterTestCase)
+
 
 if __name__ == '__main__':
     test_main()
