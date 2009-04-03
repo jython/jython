78dbdfe3ac0d06a85424effd65b1675abfb64d16
Philip Jenvey <pjenvey@underboss.org>
2009-04-03 16:43:24 +0000

    bump magic for r6135/6142 changes

--- src/org/python/core/imp.java
+++ src/org/python/core/imp.java
@@ -20,7 +20,7 @@ public class imp {
 
     private static final String UNKNOWN_SOURCEFILE = "<unknown>";
 
-    public static final int APIVersion = 18;
+    public static final int APIVersion = 19;
 
     public static final int NO_MTIME = -1;
 
