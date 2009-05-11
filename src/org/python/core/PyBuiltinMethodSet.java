cb47c5bdcde0e22aa1b1ff63d0896c85a31e7724
Philip Jenvey <pjenvey@underboss.org>
2009-05-11 00:48:39 +0000

    coding standards/cleanup

--- src/org/python/core/PyBuiltinMethodSet.java
+++ src/org/python/core/PyBuiltinMethodSet.java
@@ -1,54 +1,55 @@
+/* Copyright (c) Jython Developers */
 package org.python.core;
 
-public class PyBuiltinMethodSet extends PyBuiltinFunctionSet implements
-        Cloneable {
+public class PyBuiltinMethodSet extends PyBuiltinFunctionSet implements Cloneable {
 
-    public PyBuiltinMethodSet(String name,
-                              int index,
-                              int minargs,
-                              int maxargs,
-                              String doc,
-                              Class type) {
+    private Class<?> type;
+
+    protected PyObject __self__ = Py.None;
+
+    public PyBuiltinMethodSet(String name, int index, int minargs, int maxargs, String doc,
+                              Class<?> type) {
         super(name, index, minargs, maxargs, doc);
         this.type = type;
     }
 
+    @Override
     public PyObject __get__(PyObject obj, PyObject type) {
-        if(obj != null) {
-            if(this.type.isAssignableFrom(obj.getClass())) {
+        if (obj != null) {
+            if (this.type.isAssignableFrom(obj.getClass())) {
                 return bind(obj);
             } else {
-                throw Py.TypeError("descriptor '" + info.getName() + "' for '" + PyType.fromClass(this.type)
-                        + "' objects doesn't apply to '" + obj.getType() + "' object");
+                throw Py.TypeError(String.format("descriptor '%s' for '%s' objects doesn't apply "
+                                                 + "to '%s' object", info.getName(),
+                                                 PyType.fromClass(this.type), obj.getType()));
             }
         }
         return this;
     }
 
+    @Override
     public PyBuiltinCallable bind(PyObject bindTo) {
-        if(__self__ == Py.None) {
-            PyBuiltinMethodSet bindable;
-            try {
-                bindable = (PyBuiltinMethodSet)clone();
-            } catch(CloneNotSupportedException e) {
-                throw new RuntimeException("Didn't expect PyBuiltinMethodSet to throw CloneNotSupported since it implements Cloneable",
-                                           e);
-            }
-            bindable.__self__ = bindTo;
-            return bindable;
+        if (__self__ != Py.None) {
+            return this;
         }
-        return this;
+        PyBuiltinMethodSet bindable;
+        try {
+            bindable = (PyBuiltinMethodSet)clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Didn't expect PyBuiltinMethodSet to throw "
+                                       + "CloneNotSupported since it implements Cloneable", e);
+        }
+        bindable.__self__ = bindTo;
+        return bindable;
     }
 
+    @Override
     public PyObject getSelf() {
         return __self__;
     }
-    
-    public String toString(){
-        return "<built-in method "+info.getName()+">";
-    }
 
-    private Class type;
-    
-    protected PyObject __self__ = Py.None;
+    @Override
+    public String toString() {
+        return String.format("<built-in method %s>", info.getName());
+    }
 }
