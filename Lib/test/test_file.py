cece680ae7f8672351369c781ef44830b831a53e
Philip Jenvey <pjenvey@underboss.org>
2009-05-15 01:33:01 +0000

    we support isatty now

--- Lib/test/test_file.py
+++ Lib/test/test_file.py
@@ -88,8 +88,7 @@ class AutoFileTests(unittest.TestCase):
     def testErrors(self):
         f = self.f
         self.assertEquals(f.name, TESTFN)
-        # XXX: Jython doesn't support isatty
-        #self.assert_(not f.isatty())
+        self.assert_(not f.isatty())
         self.assert_(not f.closed)
 
         self.assertRaises(TypeError, f.readinto, "")
@@ -103,9 +102,7 @@ class AutoFileTests(unittest.TestCase):
         #           'readline', 'readlines', 'seek', 'tell', 'truncate',
         #           'write', 'xreadlines', '__iter__']
         noarg = object()
-        # XXX: Jython doesn't support isatty
-        #methods = dict(fileno=noarg, flush=noarg, isatty=noarg, next=noarg,
-        methods = dict(fileno=noarg, flush=noarg, next=noarg,
+        methods = dict(fileno=noarg, flush=noarg, isatty=noarg, next=noarg,
                        read=-1, readinto=array('c', 'x'), readline=-1,
                        readlines=noarg, seek=0, tell=noarg, truncate=0,
                        write='x', xreadlines=noarg, __iter__=noarg)
