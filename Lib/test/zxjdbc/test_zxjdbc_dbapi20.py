199d98fb3130d145ab656945e2c9bb74f2790c7e
Frank Wierzbicki <fwierzbicki@gmail.com>
2009-08-10 17:22:46 +0000

    Trivial reindent.

--- Lib/test/zxjdbc/test_zxjdbc_dbapi20.py
+++ Lib/test/zxjdbc/test_zxjdbc_dbapi20.py
@@ -18,7 +18,7 @@ class test_zxjdbc(dbapi20.DatabaseAPI20Test):
     driver = zxjdbc
     connect_args = ("jdbc:postgresql://localhost/ziclix", "bzimmer", "", "org.postgresql.Driver")
     connect_kw_args = {}
-    
+
     def test_nextset(self): pass
     def test_setoutputsize(self): pass
 
