46d2ef368688367e68567386336ee17c3fabdb1f
Frank Wierzbicki <fwierzbicki@gmail.com>
2009-09-09 17:59:05 +0000

    Also test the toArray() bugs fixed by Andrea's patch applied by Tobias.

--- tests/java/org/python/core/PyListTest.java
+++ tests/java/org/python/core/PyListTest.java
@@ -7,21 +7,34 @@ import junit.framework.TestCase;
  */
 public class PyListTest extends TestCase {
 
+    private PyList p = null;
+
     @Override
     protected void setUp() throws Exception {
+        p = new PyList();
+        p.add("foo");
+        p.add("bar");
     }
 
     @Override
     protected void tearDown() throws Exception {
+        p = null;
     }
 
     // Test for http://bugs.jython.org/issue1419
     // "Bug in PyTuple.indexOf and PyList.indexOf"
     public void testIndexOf() {
-        PyList p = new PyList();
-        p.add("foo");
-        p.add("bar");
         assertEquals(0, p.indexOf("foo"));
         assertEquals(1, p.indexOf("bar"));
+    }
+
+    public void testToArray() {
+        // In Jython 2.5.0 if an array was passed into toArray() that was
+        // too short, an Object[] was always returned instead of an array
+        // of the proper type.
+        Object[] test = new String[1];
+        String[] s = (String[])p.toArray(test);
+        assertEquals(s[0], "foo");
+        assertEquals(s[1], "bar");
     }
 }
--- tests/java/org/python/core/PyTupleTest.java
+++ tests/java/org/python/core/PyTupleTest.java
@@ -7,12 +7,16 @@ import junit.framework.TestCase;
  */
 public class PyTupleTest extends TestCase {
 
+    private PyTuple p = null;
+
     @Override
     protected void setUp() throws Exception {
+        p = new PyTuple(new PyString("foo"), new PyString("bar"));
     }
 
     @Override
     protected void tearDown() throws Exception {
+        p = null;
     }
 
     // Test for http://bugs.jython.org/issue1419
@@ -22,5 +26,14 @@ public class PyTupleTest extends TestCase {
         assertEquals(0, p.indexOf("foo"));
         assertEquals(1, p.indexOf("bar"));
     }
-
+    
+    public void testToArray() {
+        // In Jython 2.5.0 if an array was passed into toArray() that was
+        // too short, an Object[] was always returned instead of an array
+        // of the proper type.
+        Object[] test = new String[1];
+        String[] s = (String[])p.toArray(test);
+        assertEquals(s[0], "foo");
+        assertEquals(s[1], "bar");
+    }
 }
