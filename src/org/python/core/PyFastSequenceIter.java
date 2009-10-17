7d9d4059e1d05d718561c5a6bbdcd5a8f13e21df
Philip Jenvey <pjenvey@underboss.org>
2009-10-17 17:07:38 +0000

    expose PyFastSequenceIter and avoid PyType.fromClass during its construction

--- src/org/python/core/PyFastSequenceIter.java
+++ src/org/python/core/PyFastSequenceIter.java
@@ -1,19 +1,32 @@
 /* Copyright (c) Jython Developers */
 package org.python.core;
 
+import org.python.expose.ExposedMethod;
+import org.python.expose.ExposedType;
+
 /**
  * Sequence iterator specialized for accessing the underlying sequence directly.
  */
+@ExposedType(name = "fastsequenceiterator", base = PyObject.class, isBaseType = false)
 public class PyFastSequenceIter extends PyIterator {
+
+    public static final PyType TYPE = PyType.fromClass(PyFastSequenceIter.class);
 
     private PySequence seq;
 
-    private int index = 0;
+    private int index;
 
     public PyFastSequenceIter(PySequence seq) {
+        super(TYPE);
         this.seq = seq;
     }
 
+    @ExposedMethod(doc = "x.next() -> the next value, or raise StopIteration")
+    final PyObject fastsequenceiterator_next() {
+        return super.next();
+    }
+
+    @Override
     public PyObject __iternext__() {
         if (seq == null) {
             return null;
@@ -22,13 +35,14 @@ public class PyFastSequenceIter extends PyIterator {
         PyObject result;
         try {
             result = seq.seq___finditem__(index++);
-        } catch (PyException exc) {
-            if (exc.match(Py.StopIteration)) {
+        } catch (PyException pye) {
+            if (pye.match(Py.StopIteration)) {
                 seq = null;
                 return null;
             }
-            throw exc;
+            throw pye;
         }
+
         if (result == null) {
             seq = null;
         }
