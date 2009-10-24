c5580303f1c3bf63f8d48f335d33bc3a068a6d26
Philip Jenvey <pjenvey@underboss.org>
2009-10-24 04:21:18 +0000

    don't rely on jna directly as we'll likely swap to jffi/jnr-posix eventually

--- src/org/python/core/util/PlatformUtil.java
+++ src/org/python/core/util/PlatformUtil.java
@@ -1,9 +1,7 @@
-// Copyright (c) 2009 Jython project
+/* Copyright (c) 2009 Jython project */
 package org.python.core.util;
 
-// Note: Sun does not sponsor the jna project, even though the package name
-// might imply otherwise.
-import com.sun.jna.Platform;
+import org.jruby.ext.posix.util.Platform;
 
 /**
  * Methods for testing the platform/operating system that we are on.
@@ -15,10 +13,10 @@ public class PlatformUtil {
      *         where case insensitive means that a file that is stored as FOO
      *         can be accessed as (for example) FoO.
      */
-    //Currently we just check to see if we are on windows or macs, which are
-    //commonly (though not always!) case insensitive.  There are certainly cases
-    //where this is not sufficient, like the case of mounted filesystems.
     public static boolean isCaseInsensitive() {
-        return Platform.isMac() || Platform.isWindows();
+        // Currently we just check to see if we are on windows or macs, which are commonly
+        // (though not always!) case insensitive.  There are certainly cases where this is
+        // not sufficient, like the case of mounted filesystems.
+        return Platform.IS_MAC || Platform.IS_WINDOWS;
     }
 }
