bd835bdc175bc80ce1206521a9d296360ebee048
Philip Jenvey <pjenvey@underboss.org>
2010-04-09 02:54:17 +0000

    make this SynchronizedCallable descriptor public so it can be introspected by future descriptor machinery

--- src/org/python/modules/synchronize.java
+++ src/org/python/modules/synchronize.java
@@ -2,61 +2,6 @@
 package org.python.modules;
 import org.python.core.*;
 
-class SynchronizedCallable extends PyObject
-{
-    PyObject callable;
-
-    public SynchronizedCallable(PyObject callable) {
-        this.callable = callable;
-    }
-
-    public PyObject _doget(PyObject container) {
-        // TBD: third arg == null?  Hmm...
-        return new PyMethod(this, container, null);
-    }
-
-    public PyObject __call__() {
-        throw Py.TypeError("synchronized callable called with 0 args");
-    }
-
-    public PyObject __call__(PyObject arg) {
-        synchronized(synchronize._getSync(arg)) {
-            return callable.__call__(arg);
-        }
-    }
-
-    public PyObject __call__(PyObject arg1, PyObject arg2) {
-        synchronized(synchronize._getSync(arg1)) {
-            return callable.__call__(arg1, arg2);
-        }
-    }
-
-    public PyObject __call__(PyObject arg1, PyObject arg2, PyObject arg3) {
-        synchronized(synchronize._getSync(arg1)) {
-            return callable.__call__(arg1, arg2, arg3);
-        }
-    }
-
-    public PyObject __call__(PyObject[] args, String[] keywords) {
-        if (args.length == 0) {
-            throw Py.TypeError("synchronized callable called with 0 args");
-        }
-        synchronized(synchronize._getSync(args[0])) {
-            return callable.__call__(args, keywords);
-        }
-    }
-
-    public PyObject __call__(PyObject arg1, PyObject[] args,
-                             String[] keywords)
-    {
-        synchronized(synchronize._getSync(arg1)) {
-            return callable.__call__(arg1, args, keywords);
-        }
-    }
-
-
-}
-
 public class synchronize
 {
     public static Object _getSync(PyObject obj) {
@@ -83,5 +28,60 @@ public class synchronize
 
     public static PyObject make_synchronized(PyObject callable) {
         return new SynchronizedCallable(callable);
+    }
+
+    public static class SynchronizedCallable extends PyObject
+    {
+        PyObject callable;
+
+        public SynchronizedCallable(PyObject callable) {
+            this.callable = callable;
+        }
+
+        public PyObject _doget(PyObject container) {
+            // TBD: third arg == null?  Hmm...
+            return new PyMethod(this, container, null);
+        }
+
+        public PyObject __call__() {
+            throw Py.TypeError("synchronized callable called with 0 args");
+        }
+
+        public PyObject __call__(PyObject arg) {
+            synchronized(synchronize._getSync(arg)) {
+                return callable.__call__(arg);
+            }
+        }
+
+        public PyObject __call__(PyObject arg1, PyObject arg2) {
+            synchronized(synchronize._getSync(arg1)) {
+                return callable.__call__(arg1, arg2);
+            }
+        }
+
+        public PyObject __call__(PyObject arg1, PyObject arg2, PyObject arg3) {
+            synchronized(synchronize._getSync(arg1)) {
+                return callable.__call__(arg1, arg2, arg3);
+            }
+        }
+
+        public PyObject __call__(PyObject[] args, String[] keywords) {
+            if (args.length == 0) {
+                throw Py.TypeError("synchronized callable called with 0 args");
+            }
+            synchronized(synchronize._getSync(args[0])) {
+                return callable.__call__(args, keywords);
+            }
+        }
+
+        public PyObject __call__(PyObject arg1, PyObject[] args,
+                                 String[] keywords)
+        {
+            synchronized(synchronize._getSync(arg1)) {
+                return callable.__call__(arg1, args, keywords);
+            }
+        }
+
+
     }
 }
