6a945417efde8e1d2c2a3cb2ef6677017d10c398
Philip Jenvey <pjenvey@underboss.org>
2010-04-09 03:01:56 +0000

    cleanup

--- src/org/python/modules/synchronize.java
+++ src/org/python/modules/synchronize.java
@@ -1,27 +1,31 @@
-// Copyright (c) Corporation for National Research Initiatives
+/*
+ * Copyright (c) Corporation for National Research Initiatives
+ * Copyright (c) Jython Developers
+ */
 package org.python.modules;
-import org.python.core.*;
 
-public class synchronize
-{
+import org.python.core.__builtin__;
+import org.python.core.PyDictionary;
+import org.python.core.PyObject;
+import org.python.core.PyMethod;
+import org.python.core.Py;
+
+public class synchronize {
+
     public static Object _getSync(PyObject obj) {
         return Py.tojava(obj, Object.class);
     }
 
-    public static PyObject apply_synchronized(PyObject sync_object,
-                                              PyObject callable,
-                                              PyObject args)
-    {
-        synchronized (_getSync(sync_object)) {
+    public static PyObject apply_synchronized(PyObject syncObject, PyObject callable,
+                                              PyObject args) {
+        synchronized (_getSync(syncObject)) {
+
             return __builtin__.apply(callable, args);
         }
     }
-    public static PyObject apply_synchronized(PyObject sync_object,
-                                              PyObject callable,
-                                              PyObject args,
-                                              PyDictionary kws)
-    {
-        synchronized (_getSync(sync_object)) {
+    public static PyObject apply_synchronized(PyObject syncObject, PyObject callable,
+                                              PyObject args, PyDictionary kws) {
+        synchronized (_getSync(syncObject)) {
             return __builtin__.apply(callable, args, kws);
         }
     }
@@ -30,58 +34,60 @@ public class synchronize
         return new SynchronizedCallable(callable);
     }
 
-    public static class SynchronizedCallable extends PyObject
-    {
+    public static class SynchronizedCallable extends PyObject {
+
         PyObject callable;
 
         public SynchronizedCallable(PyObject callable) {
             this.callable = callable;
         }
 
-        public PyObject _doget(PyObject container) {
-            // TBD: third arg == null?  Hmm...
-            return new PyMethod(this, container, null);
+        @Override
+        public PyObject __get__(PyObject obj, PyObject type) {
+            return new PyMethod(this, obj, type);
         }
 
+        @Override
         public PyObject __call__() {
             throw Py.TypeError("synchronized callable called with 0 args");
         }
 
+        @Override
         public PyObject __call__(PyObject arg) {
-            synchronized(synchronize._getSync(arg)) {
+            synchronized (synchronize._getSync(arg)) {
                 return callable.__call__(arg);
             }
         }
 
+        @Override
         public PyObject __call__(PyObject arg1, PyObject arg2) {
-            synchronized(synchronize._getSync(arg1)) {
+            synchronized (synchronize._getSync(arg1)) {
                 return callable.__call__(arg1, arg2);
             }
         }
 
+        @Override
         public PyObject __call__(PyObject arg1, PyObject arg2, PyObject arg3) {
-            synchronized(synchronize._getSync(arg1)) {
+            synchronized (synchronize._getSync(arg1)) {
                 return callable.__call__(arg1, arg2, arg3);
             }
         }
 
+        @Override
         public PyObject __call__(PyObject[] args, String[] keywords) {
             if (args.length == 0) {
                 throw Py.TypeError("synchronized callable called with 0 args");
             }
-            synchronized(synchronize._getSync(args[0])) {
+            synchronized (synchronize._getSync(args[0])) {
                 return callable.__call__(args, keywords);
             }
         }
 
-        public PyObject __call__(PyObject arg1, PyObject[] args,
-                                 String[] keywords)
-        {
-            synchronized(synchronize._getSync(arg1)) {
+        @Override
+        public PyObject __call__(PyObject arg1, PyObject[] args, String[] keywords) {
+            synchronized (synchronize._getSync(arg1)) {
                 return callable.__call__(arg1, args, keywords);
             }
         }
-
-
     }
 }
