68ed78882aaf0b0b240752e000eb2d8e762865b2
Jim Baker <jbaker@zyasoft.com>
2010-08-09 20:58:24 +0000

    Missing file from the last commit, necessary for test_java_integration copy testing

---
+++ tests/java/javatests/ProxyTests.java
@@ -0,0 +1,38 @@
+package javatests;
+
+public class ProxyTests {
+
+    public static class Person {
+
+        // models conventions of names in certain parts of the world
+
+        private String firstName;
+        private String lastName;
+    
+        public Person(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    
+        public String toString() {
+            return lastName + ", " + firstName;
+        }
+    
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+    
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    }
+
+}
