8c84c1501981092822a306846a8b1d61c4aa6b59
Jim Baker <jbaker@zyasoft.com>
2010-08-14 21:00:19 +0000

    Removed JSR223TestCase.test_eval, it was causing action at a distance issues in the regrtest.

--- Lib/test/test_jsr223.py
+++ Lib/test/test_jsr223.py
@@ -1,30 +1,28 @@
-# test for ScriptEngine, otherwise we are not in Java 6/JSR223 support added
+# XXX Earlier version of this test also tested put, get, eval on the
+# engine, however this introduced action at a distance where aspects
+# of the sys state changed (notably sys.stdin.newlines), which then
+# impacted test_univnewlines later in the regrtest.
+#
+# For now, there may be limits in how much we can test Jython from
+# itself, no matter how attractive from an ouroboros perspective that
+# may be :). Certainly worth revisiting in 2.6.
 
-# test each feature
-
-import sys
 import unittest
-from javax.script import ScriptEngine, ScriptEngineManager
+import sys
 from test import test_support
+from javax.script import ScriptEngine, ScriptEngineManager
 
 
 class JSR223TestCase(unittest.TestCase):
-    def setUp(self):
-        self.engine = ScriptEngineManager().getEngineByName("python")
-
-    def test_eval(self):
-        engine = self.engine
-        engine.put("a", 42)
-        engine.eval("b = a/6")
-        self.assertEqual(engine.get("b"), 7)
 
     def test_factory(self):
-        f = self.engine.factory
+        engine = ScriptEngineManager().getEngineByName("python")
+        f = engine.factory
         language_version = ".".join(str(comp) for comp in sys.version_info[0:2]) # such as "2.5"
         impl_version =  ".".join(str(comp) for comp in sys.version_info[0:3]) # such as "2.5.2"
 
-        self.assertNotEqual(f.scriptEngine, self.engine) # we don't pool engines
-        
+        self.assertNotEqual(f.scriptEngine, engine) # we don't pool engines
+
         self.assertEqual(f.engineName, "jython")
         self.assertEqual(f.engineVersion, impl_version)
         self.assertEqual(set(f.extensions), set(['py']))
@@ -32,7 +30,7 @@ class JSR223TestCase(unittest.TestCase):
         self.assertEqual(f.languageVersion, language_version)
         self.assertEqual(set(f.names), set(["python", "jython"]))
         self.assertEqual(set(f.mimeTypes), set(["text/python", "application/python", "text/x-python", "application/x-python"]))
-        
+
         # variants
         self.assertEqual(f.getParameter(ScriptEngine.ENGINE), "jython")
         self.assertEqual(f.getParameter(ScriptEngine.ENGINE_VERSION), impl_version)
@@ -42,8 +40,6 @@ class JSR223TestCase(unittest.TestCase):
 
         self.assertEqual(f.getOutputStatement("abc"), "print u'abc'")
         self.assertEqual(f.getProgram("x = 42", "y = 'abc'"), "x = 42\ny = 'abc'\n")
-        
-        
 
 
 def test_main():
