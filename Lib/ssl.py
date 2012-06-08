2fba3fb05cba75e97bb79c34dd8dec24f38c5ee3
Alan Kennedy <alan@xhaus.com>
2012-06-08 23:09:16 +0100

    Checking in an extemely limited ssl module. Fixes #1897

---
+++ Lib/ssl.py
@@ -0,0 +1,10 @@
+"""
+This module provides very limited support for the SSL module on jython.
+
+See the jython wiki for more information.
+http://wiki.python.org/jython/SSLModule
+"""
+
+import socket
+
+wrap = socket.ssl
