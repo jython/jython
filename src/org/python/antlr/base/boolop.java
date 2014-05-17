0c90e4e930bd9718958c78a4e1caa116430bd58e
Frank Wierzbicki <fwierzbicki@gmail.com>
2014-05-17 21:54:35 +0000

    Forgot to check these in - unbreak build :(

---
+++ src/org/python/antlr/base/boolop.java
@@ -0,0 +1,58 @@
+// Hand copied from stmt.
+// XXX: autogenerate this.
+package org.python.antlr.base;
+import org.antlr.runtime.CommonToken;
+import org.antlr.runtime.Token;
+import org.python.antlr.AST;
+import org.python.antlr.PythonTree;
+import org.python.antlr.adapter.AstAdapters;
+import org.python.antlr.base.excepthandler;
+import org.python.antlr.base.expr;
+import org.python.antlr.base.mod;
+import org.python.antlr.base.slice;
+import org.python.antlr.base.stmt;
+import org.python.core.ArgParser;
+import org.python.core.AstList;
+import org.python.core.Py;
+import org.python.core.PyObject;
+import org.python.core.PyString;
+import org.python.core.PyStringMap;
+import org.python.core.PyType;
+import org.python.expose.ExposedGet;
+import org.python.expose.ExposedMethod;
+import org.python.expose.ExposedNew;
+import org.python.expose.ExposedSet;
+import org.python.expose.ExposedType;
+
+@ExposedType(name = "_ast.boolop", base = AST.class)
+public abstract class boolop extends PythonTree {
+
+    public static final PyType TYPE = PyType.fromClass(boolop.class);
+    private final static PyString[] fields = new PyString[0];
+    @ExposedGet(name = "_fields")
+    public PyString[] get_fields() { return fields; }
+
+    private final static PyString[] attributes =
+    new PyString[] {new PyString("lineno"), new PyString("col_offset")};
+    @ExposedGet(name = "_attributes")
+    public PyString[] get_attributes() { return attributes; }
+
+    public boolop() {
+    }
+
+    public boolop(PyType subType) {
+    }
+
+    public boolop(int ttype, Token token) {
+        super(ttype, token);
+    }
+
+    public boolop(Token token) {
+        super(token);
+    }
+
+    public boolop(PythonTree node) {
+        super(node);
+    }
+
+}
---
+++ src/org/python/antlr/base/expr_context.java
@@ -0,0 +1,58 @@
+// Hand copied from stmt.
+// XXX: autogenerate this.
+package org.python.antlr.base;
+import org.antlr.runtime.CommonToken;
+import org.antlr.runtime.Token;
+import org.python.antlr.AST;
+import org.python.antlr.PythonTree;
+import org.python.antlr.adapter.AstAdapters;
+import org.python.antlr.base.excepthandler;
+import org.python.antlr.base.expr;
+import org.python.antlr.base.mod;
+import org.python.antlr.base.slice;
+import org.python.antlr.base.stmt;
+import org.python.core.ArgParser;
+import org.python.core.AstList;
+import org.python.core.Py;
+import org.python.core.PyObject;
+import org.python.core.PyString;
+import org.python.core.PyStringMap;
+import org.python.core.PyType;
+import org.python.expose.ExposedGet;
+import org.python.expose.ExposedMethod;
+import org.python.expose.ExposedNew;
+import org.python.expose.ExposedSet;
+import org.python.expose.ExposedType;
+
+@ExposedType(name = "_ast.expr_context", base = AST.class)
+public abstract class expr_context extends PythonTree {
+
+    public static final PyType TYPE = PyType.fromClass(expr_context.class);
+    private final static PyString[] fields = new PyString[0];
+    @ExposedGet(name = "_fields")
+    public PyString[] get_fields() { return fields; }
+
+    private final static PyString[] attributes =
+    new PyString[] {new PyString("lineno"), new PyString("col_offset")};
+    @ExposedGet(name = "_attributes")
+    public PyString[] get_attributes() { return attributes; }
+
+    public expr_context() {
+    }
+
+    public expr_context(PyType subType) {
+    }
+
+    public expr_context(int ttype, Token token) {
+        super(ttype, token);
+    }
+
+    public expr_context(Token token) {
+        super(token);
+    }
+
+    public expr_context(PythonTree node) {
+        super(node);
+    }
+
+}
