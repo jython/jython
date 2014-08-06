40cd2260d9706ac6a924ef374a84ff2a5d4b57a3
Alex Grönholm <alex.gronholm@nextday.fi>
2014-08-06 19:57:33 +0300

    Fixed typo in compiler/pycodegen.py, fixes #2032.

--- Lib/compiler/pycodegen.py
+++ Lib/compiler/pycodegen.py
@@ -8,7 +8,7 @@ is_jython = sys.platform.startswith('java')
 
 from compiler import ast, parse, walk, syntax
 from compiler import misc, future, symbols
-from compiler.consts import SC_LOCAL, SC_GLOBAL_IMPLICIT, SC_GLOBAL_EXPLICT, \
+from compiler.consts import SC_LOCAL, SC_GLOBAL_IMPLICIT, SC_GLOBAL_EXPLICIT, \
      SC_FREE, SC_CELL
 from compiler.consts import (CO_VARARGS, CO_VARKEYWORDS, CO_NEWLOCALS,
      CO_NESTED, CO_GENERATOR, CO_FUTURE_DIVISION,
