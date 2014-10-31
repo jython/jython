5208de2f6e786a8cca16729b78523831744ffd5c
Stefan Richthofer <stefan.richthofer@gmx.de>
2014-10-31 02:46:05 +0100

    Fixed two defective lines in PyJavaType.java These prevented successful building, c.f. Issue 1708. Additionally out-commented code right before the defective lines was removed as it infringes coding style guide.

--- src/org/python/core/PyJavaType.java
+++ src/org/python/core/PyJavaType.java
@@ -1054,19 +1054,8 @@ public class PyJavaType extends PyType {
                 }
             };
             PyBuiltinMethodNarrow mapLeProxy = new MapMethod("__le__", 1) {
-//                if not isinstance(other, Set):
-//                    return NotImplemented
-//                if len(self) > len(other):
-//                    return False
-//                for elem in self:
-//                    if elem not in other:
-//                        return False
-//                return True
-
                 @Override
                 public PyObject __call__(PyObject other) {
-                    PyObject mapping =
-                    if (!Py.isInstance(other, ))
                     if (other.getType().isSubType(PyDictionary.TYPE)) {
                         PyDictionary oDict = (PyDictionary) other;
                         if (asMap().size() != oDict.size()) {
