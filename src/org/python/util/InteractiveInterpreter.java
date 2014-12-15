603c899cb934dc6151a5a6642ff108b9749067a5
Jeff Allen <ja.py@farowl.co.uk>
2014-12-15 22:45:08 +0000

    Javadoc and tests relevant to Unicode in PythonInterpreter.
    Makes trivial (format only) changes to Java code and adds Javadoc clarifying the current behaviour of PythonInterpreter and its subclasses with respect to Unicode programs and Reader/Writer streams used as sys.stdin/out/err. Tests are added to exercise the behaviour, including 3 skipped tests where features may be lacking.

--- src/org/python/util/InteractiveInterpreter.java
+++ src/org/python/util/InteractiveInterpreter.java
@@ -1,51 +1,96 @@
 // Copyright (c) Corporation for National Research Initiatives
 package org.python.util;
+
 import org.python.core.*;
 
+/**
+ * This class provides the interface for compiling and running code that supports an interactive
+ * interpreter.
+ */
 // Based on CPython-1.5.2's code module
-
 public class InteractiveInterpreter extends PythonInterpreter {
+
+    /**
+     * Construct an InteractiveInterpreter with all default characteristics: default state (from
+     * {@link Py#getSystemState()}), and a new empty dictionary of local variables.
+     * */
     public InteractiveInterpreter() {
         this(null);
     }
-    public InteractiveInterpreter(PyObject locals) {
-        this(locals, null);
-
-    }
-        public InteractiveInterpreter(PyObject locals, PySystemState systemState) {
-            super(locals, systemState);
-        }
 
     /**
-     * Compile and run some source in the interpreter.
+     * Construct an InteractiveInterpreter with state (from {@link Py#getSystemState()}), and the
+     * specified dictionary of local variables.
      *
-     * Arguments are as for compile_command().
+     * @param locals dictionary to use, or if <code>null</code>, a new empty one will be created
+     */
+    public InteractiveInterpreter(PyObject locals) {
+        this(locals, null);
+    }
+
+    /**
+     * Construct an InteractiveInterpreter with, and system state the specified dictionary of local
+     * variables.
      *
-     * One several things can happen:
+     * @param locals dictionary to use, or if <code>null</code>, a new empty one will be created
+     * @param systemState interpreter state, or if <code>null</code> use {@link Py#getSystemState()}
+     */
+    public InteractiveInterpreter(PyObject locals, PySystemState systemState) {
+        super(locals, systemState);
+    }
+
+    /**
+     * Compile and run some source in the interpreter, in the mode {@link CompileMode#single} which
+     * is used for incremental compilation at the interactive console, known as "<input>".
      *
-     * 1) The input is incorrect; compile_command() raised an exception
-     * (SyntaxError or OverflowError).  A syntax traceback will be printed
-     * by calling the showsyntaxerror() method.
-     *
-     * 2) The input is incomplete, and more input is required;
-     * compile_command() returned None.  Nothing happens.
-     *
-     * 3) The input is complete; compile_command() returned a code object.
-     * The code is executed by calling self.runcode() (which also handles
-     * run-time exceptions, except for SystemExit).
-     *
-     * The return value is 1 in case 2, 0 in the other cases (unless an
-     * exception is raised).  The return value can be used to decide
-     * whether to use sys.ps1 or sys.ps2 to prompt the next line.
-     **/
+     * @param source Python code
+     * @return <code>true</code> to indicate a partial statement was entered
+     */
     public boolean runsource(String source) {
         return runsource(source, "<input>", CompileMode.single);
     }
 
+    /**
+     * Compile and run some source in the interpreter, in the mode {@link CompileMode#single} which
+     * is used for incremental compilation at the interactive console.
+     *
+     * @param source Python code
+     * @param filename name with which to label this console input (e.g. in error messages).
+     * @return <code>true</code> to indicate a partial statement was entered
+     */
     public boolean runsource(String source, String filename) {
         return runsource(source, filename, CompileMode.single);
     }
 
+    /**
+     * Compile and run some source in the interpreter, according to the {@link CompileMode} given.
+     * This method supports incremental compilation and interpretation through the return value,
+     * where <code>true</code> signifies that more input is expected in order to complete the Python
+     * statement. An interpreter can use this to decide whether to use <code>sys.ps1</code> ("
+     * <code>>>> </code> ") or <code>sys.ps2</code> ("<code>... </code> ") to prompt the next line.
+     * The arguments are the same as the mandatory ones in the Python <code>compile()</code>
+     * command.
+     * <p>
+     * One the following can happen:
+     * <ol>
+     * <li>The input is incorrect; compilation raised an exception (SyntaxError or OverflowError). A
+     * syntax traceback will be printed by calling {@link #showexception(PyException)}. Return is
+     * <code>false</code>.</li>
+     *
+     * <li>The input is incomplete, and more input is required; compilation returned no code.
+     * Nothing happens. Return is <code>true</code>.</li>
+     *
+     * <li>The input is complete; compilation returned a code object. The code is executed by
+     * calling {@link #runcode(PyObject)} (which also handles run-time exceptions, except for
+     * SystemExit). Return is <code>false</code>.</li>
+     * </ol>
+     *
+     * @param source Python code
+     * @param filename name with which to label this console input (e.g. in error messages).
+     * @param kind of compilation required: {@link CompileMode#eval}, {@link CompileMode#exec} or
+     *            {@link CompileMode#single}
+     * @return <code>true</code> to indicate a partial statement was provided
+     */
     public boolean runsource(String source, String filename, CompileMode kind) {
         PyObject code;
         try {
@@ -64,23 +109,20 @@ public class InteractiveInterpreter extends PythonInterpreter {
             }
         }
         // Case 2
-        if (code == Py.None)
+        if (code == Py.None) {
             return true;
+        }
         // Case 3
         runcode(code);
         return false;
     }
 
     /**
-     * execute a code object.
-     *
-     * When an exception occurs, self.showtraceback() is called to display
-     * a traceback.  All exceptions are caught except SystemExit, which is
-     * reraised.
-     *
-     * A note about KeyboardInterrupt: this exception may occur elsewhere
-     * in this code, and may not always be caught.  The caller should be
-     * prepared to deal with it.
+     * Execute a code object. When an exception occurs, {@link #showexception(PyException)} is
+     * called to display a stack trace, except in the case of SystemExit, which is re-raised.
+     * <p>
+     * A note about KeyboardInterrupt: this exception may occur elsewhere in this code, and may not
+     * always be caught. The caller should be prepared to deal with it.
      **/
 
     // Make this run in another thread somehow????
@@ -88,7 +130,9 @@ public class InteractiveInterpreter extends PythonInterpreter {
         try {
             exec(code);
         } catch (PyException exc) {
-            if (exc.match(Py.SystemExit)) throw exc;
+            if (exc.match(Py.SystemExit)) {
+                throw exc;
+            }
             showexception(exc);
         }
     }
@@ -96,7 +140,7 @@ public class InteractiveInterpreter extends PythonInterpreter {
     public void showexception(PyException exc) {
         // Should probably add code to handle skipping top stack frames
         // somehow...
-        Py.printException(exc); 
+        Py.printException(exc);
     }
 
     public void write(String data) {
@@ -104,48 +148,55 @@ public class InteractiveInterpreter extends PythonInterpreter {
     }
 
     public StringBuffer buffer = new StringBuffer();
-    public String filename="<stdin>";
+    public String filename = "<stdin>";
 
     public void resetbuffer() {
         buffer.setLength(0);
     }
 
-    /** Pause the current code, sneak an exception raiser into
-     * sys.trace_func, and then continue the code hoping that Jython will
-     * get control to do the break;
+    /**
+     * Pause the current code, sneak an exception raiser into sys.trace_func, and then continue the
+     * code hoping that Jython will get control to do the break;
      **/
     public void interrupt(ThreadState ts) {
         TraceFunction breaker = new BreakTraceFunction();
         TraceFunction oldTrace = ts.tracefunc;
         ts.tracefunc = breaker;
-        if (ts.frame != null)
+        if (ts.frame != null) {
             ts.frame.tracefunc = breaker;
+        }
         ts.tracefunc = oldTrace;
-        //ts.thread.join();
+        // ts.thread.join();
     }
 }
 
+
 class BreakTraceFunction extends TraceFunction {
+
     private void doBreak() {
         throw new Error("Python interrupt");
-        //Thread.currentThread().interrupt();
+        // Thread.currentThread().interrupt();
     }
 
+    @Override
     public TraceFunction traceCall(PyFrame frame) {
         doBreak();
         return null;
     }
 
+    @Override
     public TraceFunction traceReturn(PyFrame frame, PyObject ret) {
         doBreak();
         return null;
     }
 
+    @Override
     public TraceFunction traceLine(PyFrame frame, int line) {
         doBreak();
         return null;
     }
 
+    @Override
     public TraceFunction traceException(PyFrame frame, PyException exc) {
         doBreak();
         return null;
