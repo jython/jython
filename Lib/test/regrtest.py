2b04e1fd0e85679e30a260f451857bc7f4c10092
Stefan Richthofer <stefan.richthofer@gmx.de>
2015-01-03 21:24:55 +0100

    Reverted accidental changes in regrtest.py; these were ment to be temporary and should have been reverted before the commit.

--- Lib/test/regrtest.py
+++ Lib/test/regrtest.py
@@ -1314,9 +1314,6 @@ _failures = {
         test_peepholer
         test_pyclbr
         test_pyexpat
-        test_select
-        test_select_new
-        test_socket
         test_stringprep
         test_threadsignals
         test_transformer
