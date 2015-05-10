27e191d19b4f55096317b1c741f613aec4ba1298
Jeff Allen <ja.py@farowl.co.uk>
2015-05-10 18:46:03 +0100

    Selectively update test.test_support from CPython 2.7
    Pull in those changes that look like progress, while preserving what
    looks like a Jython-specific addition. In particular, add the
    DirsOnSysPath context manager to support tests of the import mechanism,
    so we can fix the path pollution in test_classpathimporter.

--- Lib/test/test_classpathimporter.py
+++ Lib/test/test_classpathimporter.py
@@ -12,11 +12,9 @@ class ClasspathImporterTestCase(unittest.TestCase):
 
     def setUp(self):
         self.orig_context = Thread.currentThread().contextClassLoader
-        self.orig_path = sys.path
 
     def tearDown(self):
         Thread.currentThread().contextClassLoader = self.orig_context
-        sys.path = self.orig_path
 
     # I don't like the checked in jar file bug1239.jar.  The *one* thing I
     # liked about the tests in bugtests/ is that you could create a java file,
@@ -26,27 +24,25 @@ class ClasspathImporterTestCase(unittest.TestCase):
     # with sys.path.append where not getting scanned if they start with a top
     # level package we already have, like the "org" in org.python.*
     def test_bug1239(self):
-        sys.path.append("Lib/test/bug1239.jar")
-        import org.test403javapackage.test403
+        with test_support.DirsOnSysPath("Lib/test/bug1239.jar"):
+            import org.test403javapackage.test403
 
     # different from test_bug1239 in that only a Java package is imported, not
     # a Java class.  I'd also like to get rid of this checked in test jar.
     def test_bug1126(self):
-        sys.path.append("Lib/test/bug1126/bug1126.jar")
-        import org.subpackage
+        with test_support.DirsOnSysPath("Lib/test/bug1126/bug1126.jar"):
+            import org.subpackage
 
 
 class PyclasspathImporterTestCase(unittest.TestCase):
 
     def setUp(self):
         self.orig_context = Thread.currentThread().contextClassLoader
-        self.orig_path = sys.path
         self.temp_dir = tempfile.mkdtemp()
         self.modules = sys.modules.keys()
 
     def tearDown(self):
         Thread.currentThread().contextClassLoader = self.orig_context
-        sys.path = self.orig_path
         shutil.rmtree(self.temp_dir)
         for module in sys.modules.keys():
             if module not in self.modules:
@@ -84,9 +80,10 @@ class PyclasspathImporterTestCase(unittest.TestCase):
         self.setClassLoaderAndCheck('classimport.jar', '__pyclasspath__')
 
     def test_path_in_pyclasspath(self):
-        sys.path = ['__pyclasspath__/Lib']
-        self.setClassLoaderAndCheck('classimport_Lib.jar',
-                                    '__pyclasspath__/Lib', 'Lib')
+        with test_support.DirsOnSysPath():
+            sys.path = ['__pyclasspath__/Lib']
+            self.setClassLoaderAndCheck('classimport_Lib.jar',
+                                        '__pyclasspath__/Lib', 'Lib')
 
 
 def test_main():
