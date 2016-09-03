8a91664268713490c13ea9e3b8837740b1dcbe10
Jeff Allen <ja.py@farowl.co.uk>
2016-09-03 17:43:02 +0100

    Formatting only (crlf->lf) in test_socketserver_jython

--- Lib/test/test_socketserver_jython.py
+++ Lib/test/test_socketserver_jython.py
@@ -1,15 +1,14 @@
-# -*- coding: windows-1252 -*-
-
 import unittest
-
 import SocketServer
 
 class TestSocketServer(unittest.TestCase):
 
     def testEphemeralPort(self):
         """ Test that an ephemeral port is set correctly """
-        host, port = "localhost", 0 # If we specify 0, system should pick an emphemeral port
-        server = SocketServer.TCPServer( (host, port), None) # Request handler never instantiated
+        # If we specify 0, system should pick an emphemeral port
+        host, port = "localhost", 0
+        # Request handler never instantiated
+        server = SocketServer.TCPServer( (host, port), None)
         server_host, server_port = server.server_address
         self.failIfEqual(server_port, 0, "System assigned ephemeral port should not be zero")
 
