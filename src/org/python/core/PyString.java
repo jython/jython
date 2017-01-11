7515082e0dc5d4e6c971d85ab815774a23e2712f
Stefan Richthofer <stefan.richthofer@gmx.de>
2017-01-11 23:55:15 +0100

    Let PyString implement java.lang.CharSequence for better Java-integration.

--- src/org/python/core/PyString.java
+++ src/org/python/core/PyString.java
@@ -4253,6 +4253,21 @@ public class PyString extends PyBaseString implements BufferProtocol {
         }
         return super.unsupportedopMessage(op, o2);
     }
+
+    @Override
+    public char charAt(int index) {
+        return string.charAt(index);
+    }
+
+    @Override
+    public int length() {
+        return string.length();
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return string.subSequence(start, end);
+    }
 }
 
 
