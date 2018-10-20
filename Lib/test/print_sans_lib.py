6960f3d3fa9e42717d024fc14ca0f13a5557f92a
Jeff Allen <ja.py@farowl.co.uk>
2018-10-20 15:12:51 +0100

    Treat illegal reflective access in test_codecs_jy.
    There seems no good reason not to respect Java accessibility in test_codecs_jy,
    so we shall. We also fix CodecsTestCase.test_print_sans_lib so it does
    what it says. (Presumably changes since it was devised had invalidated it.)

--- Lib/test/print_sans_lib.py
+++ Lib/test/print_sans_lib.py
@@ -1,4 +1,9 @@
+# Used by test_codec_jy::CodecsTestCase.test_print_sans_lib.
+# Run without importing site module so codec registry is not initialised yet.
 import sys
-sys.path = [path for path in sys.path if not path.startswith('/')]
+from os.path import basename
+# Hide standard library from import mechanism so the Python codec cannot be found.
+sys.path = [p for p in sys.path if basename(p).lower() != 'lib']
+# Can we still encode and decode utf-8?
 encoded = u'hi'.encode("utf-8")
 encoded.decode('utf-8')
--- Lib/test/test_codecs_jy.py
+++ Lib/test/test_codecs_jy.py
@@ -6,13 +6,11 @@ from test import test_support
 class CodecsTestCase(unittest.TestCase):
 
     def test_print_sans_lib(self):
-        """Encodes and decodes using utf-8 after in an environment
-        without the standard library
-
-        Checks that the builtin utf-8 codec is always available:
-        http://bugs.jython.org/issue1458"""
+        # Encode and decode using utf-8 in an environment without the standard
+        # library, to check that a utf-8 codec is always available. See:
+        # http://bugs.jython.org/issue1458
         subprocess.call([sys.executable, "-J-Dpython.cachedir.skip=true",
-            "-J-Dpython.security.respectJavaAccessibility=false",
+            "-S", # No site module: avoid codec registry initialised too soon
             test_support.findfile('print_sans_lib.py')])
 
     def test_string_escape_1502(self):
