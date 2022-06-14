43c35b232c878620633e62daa89ba98fb1f95bf0
Jeff Allen <ja.py@farowl.co.uk>
2022-06-14 15:02:43 +0100

    Avoid deploying test classes
    We segregate classes needed only for tests within the main build into a
    testclasses tree. In this change set we also redirect classes generated
    during the ant javatest target (previously into build/classes) into this
    tree.
    
    This tackles the basics of separation requested in #12, but a proportion
    of tests still fail.

--- tests/java/org/python/compiler/custom_proxymaker/ClassAnnotationTest.java
+++ tests/java/org/python/compiler/custom_proxymaker/ClassAnnotationTest.java
@@ -3,29 +3,31 @@ package org.python.compiler.custom_proxymaker;
 /*
  * Test support for Python class annotations
  */
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
-
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
 import org.python.util.ProxyCompiler;
 
 public class ClassAnnotationTest {
-    
+
     Class<?> proxy;
-    
+
     @Before
     public void setUp() throws Exception {
-        ProxyCompiler.compile("tests/python/custom_proxymaker/annotated_class.py", "build/classes");
+        ProxyCompiler.compile("tests/python/custom_proxymaker/annotated_class.py",
+                "build/testclasses");
         proxy = Class.forName("custom_proxymaker.tests.AnnotatedInputStream");
     }
-    
+
     @Test
     public void hasClassAnnotation() {
         // Just by "finding" it we satisfy the test.
         @SuppressWarnings("unused")
         Deprecated deprecatedAnnotation = proxy.getAnnotation(Deprecated.class);
     }
-    
+
     @Test
     public void hasCustomAnnotationWithFields() throws Exception {
         CustomAnnotation customAnnotation = proxy.getAnnotation(CustomAnnotation.class);
--- tests/java/org/python/compiler/custom_proxymaker/ConstructorSignatureTest.java
+++ tests/java/org/python/compiler/custom_proxymaker/ConstructorSignatureTest.java
@@ -1,33 +1,31 @@
 package org.python.compiler.custom_proxymaker;
 
-/* 
- * Tests constructor signatures
- */
-
-import static org.junit.Assert.*;
-
-import java.lang.reflect.*;
-
-import org.junit.*;
-import org.python.util.ProxyCompiler;
-
 import java.awt.Container;
+import java.lang.reflect.Constructor;
+
 import javax.swing.BoxLayout;
 
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.python.util.ProxyCompiler;
+
 public class ConstructorSignatureTest {
+
     Class<?> proxy;
-    
+
     @Before
     public void setUp() throws Exception {
-        ProxyCompiler.compile("tests/python/custom_proxymaker/constructor_signatures.py", "build/classes");
+        ProxyCompiler.compile("tests/python/custom_proxymaker/constructor_signatures.py",
+                "build/testclasses");
         proxy = Class.forName("custom_proxymaker.tests.ConstructorSignatures");
     }
-    
+
     @Ignore // Constructor signatures are not working yet
     @Test
-    @SuppressWarnings("unused")
     public void returnsVoid() throws Exception {
-        Constructor<?> constructor = proxy.getConstructor(new Class<?>[] {Container.class, Integer.TYPE});
+        Constructor<?> constructor =
+                proxy.getConstructor(new Class<?>[] {Container.class, Integer.TYPE});
         constructor.newInstance(new Container(), BoxLayout.X_AXIS);
     }
 }
