c611f52713885fc74d6a6417b24c17541e48842b
Jeff Allen <ja.py@farowl.co.uk>
2022-07-03 08:43:27 +0100

    Validate path before constructing a SyspathArchive
    We repeatedly ask SyspathArchive to wrap things that are not jar/zip
    files, just to see if it can. We refactor this to fail a sooner, saving
    some work.

--- src/org/python/core/SyspathArchive.java
+++ src/org/python/core/SyspathArchive.java
@@ -1,7 +1,10 @@
 package org.python.core;
-import java.io.*;
-import java.util.logging.Logger;
-import java.util.zip.*;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 @Untraversable
 public class SyspathArchive extends PyString {
@@ -20,29 +23,70 @@ public class SyspathArchive extends PyString {
         }
     }
 
-    SyspathArchive(ZipFile zipFile, String archiveName) {
+    SyspathArchive(ZipFile zipFile, String path) {
         // As a string-like object (on sys.path) an FS-encoded bytes object is expected
-        super(Py.fileSystemEncode(archiveName).getString());
+        // Equivalent to Py.fileSystemEncode(path) with super instead of PyString
+        super(TYPE,
+                PyString.charsFitWidth(path, 7) ? path : codecs.PyUnicode_EncodeUTF8(path, null),
+                true);
         this.zipFile = zipFile;
     }
 
+    /**
+     * Return a {@code SyspathArchive} wrapping the FS-encoded path into a ".zip" or ".jar" file
+     * (like "x/y/a.jar/b/c.x") and {@code ZipFile} archive representing the file part ("x/y/a.jar")
+     * of that path. This is guaranteed to be bytes-like (all chars &lt; 256). The method throws if
+     * the argument is malformed, meaning it does not contain ".zip" or ".jar", (terminally or
+     * followed by "/").
+     *
+     * @param path to represent
+     * @return SyspathArchive for the path
+     * @throws IOException if the argument is malformed
+     */
+    static SyspathArchive fromPath(String path) throws IOException {
+        // Check the name is properly formed and reduce it to the file part.
+        String archiveName = getArchiveName(path);
+        if (archiveName == null) {
+            throw new IOException(path);
+        } else {
+            ZipFile zipFile = new ZipFile(new File(archiveName));
+            if (PySystemState.isPackageCacheEnabled()) {
+                PySystemState.packageManager.addJar(archiveName, true);
+            }
+            return new SyspathArchive(zipFile, path);
+        }
+    }
+
+    /**
+     * Return the archive name extracted from a path like "x/y/a.jar/b/c.x" as a String, extracted
+     * from the path by truncation. Here the archive name would be the "x/y/a.jar" part. The method
+     * returns {@code null} if the argument is malformed, meaning it does not contain ".zip" or
+     * ".jar", (terminally or followed by "/").
+     *
+     * @param path to extract from
+     * @return archive name or {@code null} if the argument is does not contain ".zip" or ".jar"
+     */
     static String getArchiveName(String dir) {
         String lowerName = dir.toLowerCase();
         int idx = lowerName.indexOf(".zip");
         if (idx < 0) {
             idx = lowerName.indexOf(".jar");
         }
-        if (idx < 0) {
-            return null;
-        }
 
-        if (idx == dir.length() - 4) {
-            return dir;
+        if (idx >= 0) {
+            if (idx == dir.length() - 4) {
+                // Exactly ends with .zip or .jar
+                return dir;
+            } else {
+                // Continues, but must be /something
+                char ch = dir.charAt(idx + 4);
+                if (ch == File.separatorChar || ch == '/') {
+                    // Return only up toe the .jar/.zip (inclusive)
+                    return dir.substring(0, idx + 4);
+                }
+            }
         }
-        char ch = dir.charAt(idx+4);
-        if (ch == File.separatorChar || ch == '/') {
-            return dir.substring(0, idx+4);
-        }
+        // Malformed after all.
         return null;
     }
 
