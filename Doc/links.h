100b78f8e0bc67e3b8273bc19778d9577eecab8a
Stefan Richthofer <stefan.richthofer@gmx.de>
2024-06-09 13:43:33 +0200

    Cleaned up Doc folder: removed obsolete files and info, updated links.

--- Doc/links.h
+++ Doc/links.h
@@ -5,20 +5,17 @@
 <li><a href="interpreter.html">Invoking Jython</a>
 <li><a href="registry.html">Jython Registry</a>
 <li><a href="embedding.html">Embedding</a>
-<li><a href="compile.html">Compiling Jython from source</a>
 <h3>Working with Java</h3>
 <li><a href="usejava.html">The Basics</a>
 <li><a href="properties.html">JavaBean Properties</a>
 <li><a href="jarray.html">Java arrays</a>
 <li><a href="subclassing.html">Subclassing</a>
-<li><a href="jythonc.html">Building applets, servlets, beans...</a>
-<li><a href="jreload.html">Reloading java classes</a>
 <li><a href="zxjdbc.html">zxJDBC</a>
 <h3>Python Docs (exits)</h3>
-<li><a href="http://www.python.org/doc/tut/tut.html">Python Tutorial</a>
-<li><a href="http://www.python.org/doc/lib/lib.html">Library Reference</a>
+<li><a href="https://docs.python.org/2.7/tutorial/index.html">Python Tutorial (2.7)</a>
+<li><a href="https://docs.python.org/2.7/library/index.html">Library Reference (2.7)</a>
 <h3>Other</h3>
 <li><a href="differences.html">Jython vs. CPython</a>
-<li><a href="http://www.jython.org/cgi-bin/faqw.py?req=index">Jython FAQ (exit)</a>
-<li><a href="http://www.geocrawler.com/archives/3/7017/2000/">List Archives</a> (exit)
-<LI><A HREF="http://www.python.org/workshops/1997-10/proceedings/hugunin.html">JPython paper</A> (exit)
+<li><a href="https://wiki.python.org/jython/JythonFaq">Jython FAQ (exit)</a>
+<li><a href="https://sourceforge.net/p/jython/mailman/">List Archives</a> (exit)
+<LI><A HREF="http://hugunin.net/papers/hugunin97python.pdf">JPython paper</A> (exit)
